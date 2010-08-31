cffd27389bb7bb2172ac508a066b424bf88dcdeb
Alan Knowles <alan@akbkhome.com>
2010-08-31 17:18:30 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -81,7 +81,7 @@ class HTML_FlexyFramework {
     var $nodatabase = false; // set to true to block db config and testing.
     var $fatalAction = false; // page to redirct to on failure. (eg. databse down etc.)
     var $charset = false; // default UTF8
-    var $dataObjectsCache = false;  // use dataobjects ini cache..
+    var $dataObjectsCache = true;  // use dataobjects ini cache..
     var $dataObjectsCacheExpires = 3600; // 1 houre..
     
     
