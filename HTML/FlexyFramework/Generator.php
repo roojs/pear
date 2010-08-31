5b145c580e2bb2b11895b3dcd50049223435178b
Alan Knowles <alan@akbkhome.com>
2010-08-31 17:11:23 +0800

    gen

---
+++ HTML/FlexyFramework/Generator.php
@@ -0,0 +1,16 @@
+<?php
+
+
+require_once 'DB/DataObject/Generator.php';
+
+
+class HTML_FlexyFramework_Generator extends DB_DataObject_Generator 
+{
+    // block class generation.
+    function generateClasses()
+    {
+        return;
+    }
+    
+    
+}
