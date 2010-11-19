f2432f7509808c30484bc56d9aca5bea12ec709f
Alan Knowles <alan@akbkhome.com>
2010-11-19 16:02:47 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -7,7 +7,6 @@ class HTML_FlexyFramework_Cli
     function help($cli)
     {
              echo "
-
     $cli DataObjects
 
     Run Basic DataObjects Generator (used to generate plain HTML_FlexyFramework projects)
