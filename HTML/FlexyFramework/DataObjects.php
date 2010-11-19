edf3a112849a227b0b5a61fadae483bfc0d65c5e
Alan Knowles <alan@akbkhome.com>
2010-11-19 16:09:28 +0800

    HTML/FlexyFramework/DataObjects.php

--- HTML/FlexyFramework/DataObjects.php
+++ HTML/FlexyFramework/DataObjects.php
@@ -3,6 +3,15 @@
 
 class HTML_FlexyFramework_DataObjects {
     
+    function getAuth()
+    {
+        return true; // auth handled by framework..
+    }
+    
+    function get()
+    {
+        die("dataobject genertor");
+    }
     
     
 }
