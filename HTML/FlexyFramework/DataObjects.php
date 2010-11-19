bfe53ec338a2a93f358662ce99171ffe9e0f2fc5
Alan Knowles <alan@akbkhome.com>
2010-11-19 16:30:40 +0800

    HTML/FlexyFramework/DataObjects.php

--- HTML/FlexyFramework/DataObjects.php
+++ HTML/FlexyFramework/DataObjects.php
@@ -13,10 +13,12 @@ class HTML_FlexyFramework_DataObjects extends HTML_FlexyFramework_Page {
         require_once 'HTML/FlexyFramework/Generator.php';
          require_once 'DB/DataObject.php';
         HTML_FlexyFramework_Generator::$generateClasses = true;
-        DB_DataObject::debugLevel(5);
+        $ff = HTML_FlexyFramework::get()
+
+        $ff->DB_DataObject['debug'] = 5;
         // make sure the cache generator is on..
         HTML_FlexyFramework::get()->dataObjectsCache = true;
-        $ff = HTML_FlexyFramework::get()->generateDataobjectsCache(true);
+         HTML_FlexyFramework::get()->generateDataobjectsCache(true);
         die("Generation done..");
     }
     
