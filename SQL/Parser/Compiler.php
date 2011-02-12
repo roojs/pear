88af69749fb90f3120f70b995a1468f8d9b97f07
Alan Knowles <alan@akbkhome.com>
2011-02-12 16:36:45 +0800

    SQL/Parser/Compiler.php

--- SQL/Parser/Compiler.php
+++ SQL/Parser/Compiler.php
@@ -348,7 +348,7 @@ class SQL_Parser_Compiler
         
         
         
-        $sql .= implode(",\n", $body) . ");"
+        $sql .= implode(",\n", $body) . ");";
         return $sql;
     }
     
