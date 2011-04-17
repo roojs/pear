a3e558733d91feebe114515de9a1fe2e7e01d95a
Alan Knowles <alan@akbkhome.com>
2011-04-17 14:12:15 +0800

    HTML/Template/Flexy/Compiler/Flexy.php

--- HTML/Template/Flexy/Compiler/Flexy.php
+++ HTML/Template/Flexy/Compiler/Flexy.php
@@ -247,7 +247,7 @@ class HTML_Template_Flexy_Compiler_Flexy extends HTML_Template_Flexy_Compiler {
         if (is_a($this->options['Translation2'], 'Translation2')) {
             $this->options['Translation2']->setLang($this->options['locale']);
             
-            if(empty($this->options['Translation2']['CommonPageID'])) {
+            if(empty($this->_options['Translation2']['CommonPageID'])) {
 		// fixme - needs to be more specific to which template to use..
     	        foreach ($this->options['templateDir'] as $tt) {
                     $n = basename($this->currentTemplate);
