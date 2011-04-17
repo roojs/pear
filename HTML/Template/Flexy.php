122a6a7c242325ec69d3ee5116d82176e4d56c26
Alan Knowles <alan@akbkhome.com>
2011-04-17 14:09:21 +0800

    HTML/Template/Flexy.php

--- HTML/Template/Flexy.php
+++ HTML/Template/Flexy.php
@@ -244,7 +244,7 @@ class HTML_Template_Flexy
                 $this->options[$key] = $aOption;
             }
         }
-        print_r($options);
+        print_r($this->options);
         foreach( $options as $key=>$aOption)  {
            $this->options[$key] = $aOption;
         }
