62684ddbbde1ba9611aa1ba7e4be27dcd2eb3375
Alan Knowles <alan@akbkhome.com>
2011-08-09 18:45:30 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -1064,7 +1064,7 @@ Available commands:
                 $clp[] = preg_replace('/\.php$/','', $d);
                 
                 print_r("CLP:          " . implode('/', $clp)."\n");
-                $this->cliShortHelp("$pr/".implode('/', $clp ));
+                $this->cliShortHelp(implode('/', $clp ));
                 continue;
             }
             $clp[] = $d;
