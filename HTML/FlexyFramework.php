835fcd64ef1918af0f62405c744010ceec71ee1a
Alan Knowles <alan@akbkhome.com>
2011-08-09 17:35:00 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -698,11 +698,12 @@ class HTML_FlexyFramework {
             if (is_a($newargs, 'PEAR_Error')) {
                 if ($newargs->getCode() === CONSOLE_GETARGS_ERROR_USER) {
 	                // User put illegal values on the command line.
-	                echo Console_Getargs::getHelp($config, NULL, $args->getMessage(), 78, 4)."\n";
+	                echo Console_Getargs::getHelp($classname::$cli_opts, NULL, $args->getMessage(), 78, 4)."\n";
 	            } else if ($args->getCode() === CONSOLE_GETARGS_HELP) {
 	                // User needs help.
-	                echo Console_Getargs::getHelp($config, NULL, NULL, 78, 4)."\n";
+	                echo Console_Getargs::getHelp($classname::$cli_opts, NULL, NULL, 78, 4)."\n";
 	            }
+                exit;
             }
             
             $args = array_merge($newargs, $ar );
