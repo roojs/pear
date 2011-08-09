5b08f2b250f9cadf16c464c21674124e8240a23b
Alan Knowles <alan@akbkhome.com>
2011-08-09 18:18:42 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -1025,9 +1025,10 @@ Available commands:
         
         $p = dirname(realpath($_SERVER["SCRIPT_FILENAME"])); 
         $pr = $this->project;
-        
+        print_r("$p/");        
         foreach(scandir("$p/$pr") as $d) {
-            print_r("$p/$pr/$d");
+            
+            print_r("$pr/$d");
             if (!strlen($d) || $d[0] == '.') {
                 continue;
             }
