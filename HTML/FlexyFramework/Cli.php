d8bc5d4747ca9b747380cbf79c504451dd8e69af
Alan Knowles <alan@akbkhome.com>
2011-08-11 10:50:24 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -1,19 +1,17 @@
 <?php
 
-
+/**
+ *
+ * handles the cli features of Flexy Framework.
+ *
+ *
+ *
+ */
 class HTML_FlexyFramework_Cli
 {
-    var $available = array(
-        'HTML/FlexyFramework/Database'
-    );
-    function help($cli)
-    {
-             echo "
-    $cli DataObjects
-
-    Run Basic DataObjects Generator (used to generate plain HTML_FlexyFramework projects)
-        
-";
-    }
+    
+    
+    
+    
     
 }
