fd48940cf1880ffb119dbe52fa4ba72643b2f58b
Alan Knowles <alan@akbkhome.com>
2011-08-22 15:17:40 +0800

    Mail/mimeDecode.php

--- Mail/mimeDecode.php
+++ Mail/mimeDecode.php
@@ -562,7 +562,9 @@ class Mail_mimeDecode extends PEAR
                     $i++;
                     continue; // skip leading spaces after '=' or after '"'
                 }
-                if (!$escaped && ($c == '"' || $c == "'")) {
+                $key_is_trans = $key[strlen($key)-1] == 0;
+                
+                if (!$key_is_trans && !$escaped && ($c == '"' || $c == "'")) {
                     // start quoted area..
                     $q = $c;
                     // in theory should not happen raw text in value part..
