96f8a65ae980a4902a7734fd09e5e7637efcd702
Alan Knowles <alan@akbkhome.com>
2011-10-24 12:23:03 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -335,7 +335,7 @@ class HTML_FlexyFramework {
             $this->DB_DataObject['quote_identifiers_tableinfo'] = true;
         }
         if (!file_exists(dirname($iniCache))) {
-            mkdir($iniCache,0600, true);
+            mkdir($iniCache,0700, true);
         }
         
         $this->DB_DataObject[$dbini] = $iniCacheTmp;
