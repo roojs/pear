8628c51265479faba66a961c4690a9c35c8d4651
Alan Knowles <alan@akbkhome.com>
2011-11-17 16:38:32 +0800

    HTML/FlexyFramework/JsTemplate.php

--- HTML/FlexyFramework/JsTemplate.php
+++ HTML/FlexyFramework/JsTemplate.php
@@ -62,7 +62,7 @@ class HTML_FlexyFramework_JsTemplate {
             
         }
         $in = str_repeat("    ", $indent);
-        $ret[] = $in + "return ret.join('');\n}\n"'
+        $ret[] = $in + "return ret.join('');\n}\n";
         echo '<PRE>' . htmlspecialchars(implode("\n",$ret));
         
         
