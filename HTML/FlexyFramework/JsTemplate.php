02613b974fe0933f175bfe7614974804c5875ad6
Alan Knowles <alan@akbkhome.com>
2011-11-17 16:15:14 +0800

    HTML/FlexyFramework/JsTemplate.php

---
+++ HTML/FlexyFramework/JsTemplate.php
@@ -0,0 +1,21 @@
+<?php
+
+
+HTML_FlexyFramework_JsTemplate {
+    
+    
+    function HTML_FlexyFramework_JsTemplate($fn)
+    {
+        // cached? - check file see if we have cached contents.
+        
+        
+        $contents = file_get_contents($fn);
+        
+        
+        
+        
+    }
+    
+    
+    
+}
