4e8c1fb5ae2b49b20bcd1f836483dd335bc640bc
Alan Knowles <alan@akbkhome.com>
2011-11-24 16:29:53 +0800

    DB/DataObject/Generator.php

--- DB/DataObject/Generator.php
+++ DB/DataObject/Generator.php
@@ -728,7 +728,7 @@ class DB_DataObject_Generator extends DB_DataObject
             if (preg_match('/(auto_increment|nextval\()/i',rawurldecode($t->flags)) 
                 || (isset($t->autoincrement) && ($t->autoincrement === true))) {
                 
-                var_Dump($t->flags);
+                echo($t->flags);
                 // native sequences = 2
                 if ($write_ini) {
                     $keys_out_primary .= "{$t->name} = N\n";
