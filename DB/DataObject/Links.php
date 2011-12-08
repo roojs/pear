a4d384a65bd01d375c9e59cc4e51a65495058d26
Alan Knowles <alan@akbkhome.com>
2011-12-08 14:43:34 +0800

    DB/DataObject/Links.php

--- DB/DataObject/Links.php
+++ DB/DataObject/Links.php
@@ -309,10 +309,11 @@ class DB_DataObject_Links
     {
          
         // get table will load the options.
-        if (!empty($this->do->_link_loaded)) {
+        if (isset($this->do->_link_loaded)) {
             return true;
         }
-        $this->_link_loaded = false;
+        
+        $this->_do->link_loaded = false;
         $cols  = $this->table();
         $links = $this->links();
          
