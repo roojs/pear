78a0b3f41db07115e9eafab70180c4ed96281c7b
Alan Knowles <alan@akbkhome.com>
2012-02-22 12:52:51 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -31,42 +31,8 @@ class HTML_FlexyFramework_Cli
             //'short' => 'v',
             'min' => 0,
             'max' => 1,
-        ),
-        
-        'source' => array(
-            'desc' => 'Source directory for json files.',
-            'short' => 'f',
-            'default' => '',
-            'min' => 1,
-            'max' => 1,
-        ),
-         'no-cache' => array(
-            'desc' => 'Do not cache.',
-            'short' => 'n',
-            'default' => 0,
-            'min' => 1,
-            'max' => 1,
-        ),
-        'list-order' => array(
-            'desc' => 'List Order.',
-            'short' => 'l',
-            'default' => 0,
-            'min' => 1,
-            'max' => 1,
-        ),
-         'table' => array(
-            'desc' => 'Only a specific table, eg. -t Netsuite_SalesOrder',
-            'short' => 't',
-            
-            'max' => 1,
-        ),
-         'year' => array(
-            'desc' => 'Only a specific year, eg. -y 2009',
-            'short' => 'y',
-            'default' => 2009,
-            'min' => 1,
-            'max' => 1,
-        ),
+        )
+         
     );
     
     
