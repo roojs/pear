829e404b4f36c2eff1d8b4c87a281b75ed0d9f25
Alan Knowles <alan@akbkhome.com>
2012-03-06 23:52:08 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -35,7 +35,9 @@ require_once 'HTML/FlexyFramework/Error.php';
 // remove E_ANAL
  
 error_reporting(E_ALL & ~E_STRICT);
-ini_set('display_errors','off');
+//ini_set('display_errors','off');
+//ini_set('log_errors','off');
+
 //PEAR::setErrorHandling(PEAR_ERROR_TRIGGER, E_USER_ERROR);
 
 
