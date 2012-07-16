7ec4ed0e4e6f49d7bf8522ffeb213135a09d0cf0
Alan Knowles <alan@roojs.com>
2012-07-16 14:02:58 +0800

    DB/pgsql.php

--- DB/pgsql.php
+++ DB/pgsql.php
@@ -610,16 +610,16 @@ class DB_pgsql extends DB_common
      */
     function commit()
     {
-        if ($this->transaction_opcount > 0) {
+        //if ($this->transaction_opcount > 0) {
             // (disabled) hack to shut up error messages from libpq.a
             //@fclose(@fopen("php://stderr", "w"));
-            //$result = @pg_exec($this->connection, 'end;');
+            $result = @pg_exec($this->connection, 'end;');
             $result = @pg_exec($this->connection, 'COMMIT');
             $this->transaction_opcount = 0;
             if (!$result) {
                 return $this->pgsqlRaiseError();
             }
-        }
+        //}
         return DB_OK;
     }
 
