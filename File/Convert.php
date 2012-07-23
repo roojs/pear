a95d140b1a762d6dfe7f4f6ebb7bc1fa402ffccd
Alan Knowles <alan@roojs.com>
2012-07-23 17:11:03 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -383,6 +383,7 @@ class File_Convert
                 array( 
                     'application/rtf',
                     'application/vnd.oasis.opendocument.text',
+                    'application/x-abiword'
                 )
             ),
             
