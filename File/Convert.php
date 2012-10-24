61ccd10f1a4d5099423040c9839d544374c82be8
Alan Knowles <alan@roojs.com>
2012-10-24 12:32:28 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -981,8 +981,9 @@ class File_Convert_Solution
         
         $PDFINFO = System::which("pdfinfo");
         $GREP = System::which("grep");
-        $cmd = $PDFINFO . ' '. escapeshellarg($fn) . " | $GREP 'Page size'";
-        //echo $cmd;
+        $STRINGS= System::which("strings");
+        $cmd = $PDFINFO . ' '. escapeshellarg($fn) . " | $STRINGS | $GREP 'Page size'";
+        
         
          $info = trim( `$cmd`);
         $match = array();
