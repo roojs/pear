b3d70755326f5d84da46187ac5bfaecc8ab6b4e3
Edward <edward@roojs.com>
2012-12-05 14:18:59 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -78,8 +78,8 @@ class File_Convert_AbiToDocx
                     if($cellObj->nodeName === 'cell'){
                         $cellStyle = $this->parseProps($cellObj->getAttribute('props'));
                         
-                        if($cellStyle['left-attach'] == 0) {
-                            $table->addRow();
+                        if($cellStyle['colunmNum'] == 0) {
+                            $table->addRow(preg_replace('/[^0-9]./', '', $tableStyle['height'.$cellStyle['rowNum']]));
                         }   
                         foreach($cellObj->childNodes as $pObj){
                             if($pObj->nodeName === 'p'){
