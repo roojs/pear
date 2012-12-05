e612fb3618f0f04d38f3ca338d8716d7c31f6f95
Edward <edward@roojs.com>
2012-12-05 18:18:59 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -85,7 +85,7 @@ class File_Convert_AbiToDocx
             // Get The Name of image
             $imageId = $xr->getAttribute('dataid');
             $path = '/tmp/'.$imageId.'.jpg';
-            if(file_exists($path)){
+            if(file_exists($path) && $imageId == 67219.0){
                 $imageStyle = $this->parseProps($xr->getAttribute('props'));
                 $width = preg_replace('/[^0-9.]/', '', $imageStyle['width']);
                 $height = preg_replace('/[^0-9.]/', '', $imageStyle['height']);
