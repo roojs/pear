a3e6c91c0c8e5df48a50037e1f416b73e65811ee
Edward <edward@roojs.com>
2012-12-05 14:57:39 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -44,10 +44,9 @@ class File_Convert_AbiToDocx
                         $props = $this->parseProps($xr->getAttribute('props'));
                         
                         $map[$imageId] = $section->addImageDefered('/tmp/'.$imageId.'.jpg');
+                        print_r($map);
                         
-                    }
-                    
-                    elseif($xr->name === 'd'){
+                    }elseif($xr->name === 'd'){
                         $data = base64_decode($xr->readString());
                         $imageId = $xr->getAttribute('name');
                         $path = '/tmp/' . $xr->getAttribute('name') . '.jpg';
