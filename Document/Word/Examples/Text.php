388cc4333c02efb81c8a91412b31c546bd831e4b
Edward <edward@roojs.com>
2012-12-05 17:43:26 +0800

    Document/Word/Examples/Text.php

--- Document/Word/Examples/Text.php
+++ Document/Word/Examples/Text.php
@@ -10,15 +10,7 @@ $section = $PHPWord->createSection();
 
 // Add text elements
 $section->addText('Hello World!设置编码');
-$section->addTextBreak(2);
 
-$section->addText('I am inline styled.', array('name'=>'Verdana', 'color'=>'006699'));
-$section->addTextBreak(2);
-
-$PHPWord->addFontStyle('rStyle', array('bold'=>true, 'italic'=>true, 'size'=>16));
-$PHPWord->addParagraphStyle('pStyle', array('align'=>'center', 'spaceAfter'=>100));
-$section->addText('I am styled by two style definitions.', 'rStyle', 'pStyle');
-$section->addText('I have only a paragraph style definition.', null, 'pStyle');
 
 
 
