b9e823cc7d65e55964259358da8bc6f7c25c631e
Edward <edward@roojs.com>
2012-12-05 15:03:09 +0800

    Document/Word/Examples/Image.php

--- Document/Word/Examples/Image.php
+++ Document/Word/Examples/Image.php
@@ -5,24 +5,28 @@ require_once __DIR__ . '/../Writer.php';
 // New Word Document
 $PHPWord = new Document_Word_Writer();
 
-// New portrait section
-$section = $PHPWord->createSection();
-
+$section = $PHPWord->createSection(array('borderColor'=>'00FF00', 'borderSize'=>12));
+$section->addText('I am placed on a default section.');
+$section->addTextBreak(2);
 // Add image elements
 $section->addImage('_mars.jpg');
 $section->addTextBreak(2);
-
+// New landscape section
+$section = $PHPWord->createSection(array('orientation'=>'landscape'));
+$section->addText('I am placed on a landscape section. Every page starting from this section will be landscape style.');
+$section->addPageBreak();
 $section->addImage('_earth.JPG', array('width'=>210, 'height'=>210, 'align'=>'center'));
 $section->addTextBreak(2);
+$section->addPageBreak();
+
+// New portrait section
+$section = $PHPWord->createSection(array('marginLeft'=>600, 'marginRight'=>600, 'marginTop'=>600, 'marginBottom'=>600));
+$section->addText('This section uses other margins.');
+
 
 $section->addImage('_mars.jpg', array('width'=>100, 'height'=>100, 'align'=>'right'));
 
-// add in /tmp/67219.0.jpg
-$rid = $section->addImageDefered('_earth.JPG', array('width'=>210, 'height'=>210, 'align'=>'center'));
-// store map [67219.0] = $rid
 
-// when get image - > lookup map [67219.0] for rid
-$section->addImageToCollection($rid, '_earth.JPG'); // 67219.0.jpg/:;:
 
 // Save File
 require_once __DIR__ . '/../Writer/IOFactory.php';
