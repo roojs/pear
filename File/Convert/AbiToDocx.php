968c72523dd510ccbdf279d1ea99677eda2a4e61
Edward <edward@roojs.com>
2012-12-05 13:33:26 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -101,6 +101,7 @@ class File_Convert_AbiToDocx
                 return $attribute;
             
             foreach ($data as $attrs){
+                print_r($attr[0] . ' : ' . $attr[1]);
                 $attr = explode(':', $attrs);
                 $attrArray[trim($attr[0])] = trim($attr[1]);
             }
