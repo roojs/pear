a7b184940c0690f1ab22bbce44a66f5b24212906
Edward <edward@roojs.com>
2012-12-05 15:05:59 +0800

    Document/Word/Examples/Image.php

--- Document/Word/Examples/Image.php
+++ Document/Word/Examples/Image.php
@@ -5,14 +5,14 @@ require_once __DIR__ . '/../Writer.php';
 // New Word Document
 $PHPWord = new Document_Word_Writer();
 
-$section = $PHPWord->createSection(array('borderColor'=>'00FF00', 'borderSize'=>12));
+$section = $PHPWord->createSection();
 $section->addText('I am placed on a default section.');
 $section->addTextBreak(2);
 // Add image elements
 $section->addImage('_mars.jpg');
 $section->addTextBreak(2);
 // New landscape section
-$section = $PHPWord->createSection(array('orientation'=>'landscape'));
+$section = $PHPWord->createSection();
 $section->addText('I am placed on a landscape section. Every page starting from this section will be landscape style.');
 $section->addPageBreak();
 $section->addImage('_mars.jpg');
@@ -20,7 +20,7 @@ $section->addTextBreak(2);
 $section->addPageBreak();
 
 // New portrait section
-$section = $PHPWord->createSection(array('marginLeft'=>600, 'marginRight'=>600, 'marginTop'=>600, 'marginBottom'=>600));
+$section = $PHPWord->createSection();
 $section->addText('This section uses other margins.');
 
 
