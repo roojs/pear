7f10e90c1a1d6c8a38dcdc2bbcb3b6fa24ad1c54
Edward <edward@roojs.com>
2012-12-05 17:54:53 +0800

    Document/Word/Writer/Section.php

--- Document/Word/Writer/Section.php
+++ Document/Word/Writer/Section.php
@@ -115,7 +115,8 @@ class Document_Word_Writer_Section
 	public function addText($text, $styleFont = null, $styleParagraph = null) 
         {
                 require_once __DIR__ . '/Section/Text.php';
-		$givenText = utf8_encode($text);
+		//$givenText = utf8_encode($text);
+                $text = iconv(mb_detect_encoding($text), "UTF-8", $text);
 		$text = new Document_Word_Writer_Section_Text($givenText, $styleFont, $styleParagraph);
 		$this->_elementCollection[] = $text;
 		return $text;
