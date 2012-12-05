ec2d5d7dba521fea485a4152a3b0d8f9e3b63a35
Edward <edward@roojs.com>
2012-12-05 14:28:19 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -78,14 +78,12 @@ class File_Convert_AbiToDocx
                 foreach($tableObj->childNodes as $cellObj){
                     if($cellObj->nodeName === 'cell'){
                         $cellStyle = $this->parseProps($cellObj->getAttribute('props'));
-                        print_r($cellStyle.'<br/>');
                         if($cellStyle['colunmNum'] == 0) {
                             $table->addRow(preg_replace('/[^0-9.]/', '', $tableStyle['height'.$cellStyle['rowNum']]));
                         }   
                         foreach($cellObj->childNodes as $pObj){
                             if($pObj->nodeName === 'p'){
                                 $pStyle = $this->parseProps($pObj->getAttribute('style'));
-                                print_r($cellStyle.'<br/>');
                                 $table->addCell(preg_replace('/[^0-9.]/', '', $tableStyle['width'.$cellStyle['colunmNum']]), $cellStyle)->addText($pObj->nodeValue, $pStyle);
                             }
                         }
