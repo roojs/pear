83902c9ded533cd5945bb737b75a0334821810f2
Edward <edward@roojs.com>
2012-12-05 19:28:31 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -51,162 +51,33 @@ class File_Convert_AbiToDocx
                 }
         }
         
-        function handle_Table() {
+        function handle_Table() 
+        {
             if ($this->pass != 2) {
                 return;
             }
             
         }
-        function handle_pbr() {
+        
+        function handle_Table() 
+        {
+            if ($this->pass != 2) {
+                return;
+            }
+            
+        }
+        
+        function handle_pbr() 
+        {
             $this->section = $PHPWord->createSection();
         }
+        
         function parseAbiDom($node)
         {
             
             
         }
-                    // Handle All The Elements
-                    if($xr->name === 'table'){
-                        //New Section
-                        $section = $PHPWord->createSection();
-                        // Draw Table
-                        $this->drawTable($section, $xr);
-                    }elseif($xr->name === 'image'){
-                        //New Section
-                        $section = $PHPWord->createSection();
-                        // Draw Image
-                        $this->drawImage($section,$xr);
-                    }
-                }
-                // Close XML Reader
-                $xr->close();
-                // Save File
-                $this->saveDocx($PHPWord);
-	}
         
-        
-        
-        public function drawTable($section, $xr){
-                // Define table style arrays
-                $tableStyle = $this->parseProps($xr->getAttribute('props'));
-                // Add table
-                $table = $section->addTable();
-                // Convert xr Element to DOM Object
-                $tableObj = $xr->expand();
-                // Draw The Table
-                foreach($tableObj->childNodes as $cellObj){
-                    if($cellObj->nodeName === 'cell'){
-                        $cellStyle = $this->parseProps($cellObj->getAttribute('props'));
-                        if($cellStyle['colunmNum'] == 0) {
-                            $height = array_key_exists('height'.$cellStyle['rowNum'], $tableStyle) ? $tableStyle['height'.$cellStyle['rowNum']] : '';
-                            $height = preg_replace('/[^0-9.]/', '', $height);
-                            $table->addRow($this->inchToPx($height));
-                        }   
-                        
-                        foreach($cellObj->childNodes as $pObj){
-                            if($pObj->nodeName === 'p'){
-                                $pStyle = $this->parseProps($pObj->getAttribute('style'));
-                                $width = array_key_exists('width'.$cellStyle['colunmNum'], $tableStyle) ? $tableStyle['width'.$cellStyle['colunmNum']] : '';
-                                $width = preg_replace('/[^0-9.]/', '', $width);
-                                $table->addCell($this->inchToPx($width), $cellStyle)->addText($pObj->nodeValue, $pStyle);
-                            }
-                        }
-                    }
-                 }
-        }
-        function parseTextBody($onto)
-        
-        
-        public function drawImage($section, $xr){
-            // Get The Name of image
-            $imageId = $xr->getAttribute('dataid');
-            $path = '/tmp/'.$imageId.'.jpg';
-            if(file_exists($path)){
-                $imageStyle = $this->parseProps($xr->getAttribute('props'));
-                $width = preg_replace('/[^0-9.]/', '', $imageStyle['width']);
-                $height = preg_replace('/[^0-9.]/', '', $imageStyle['height']);
-                $section->addImage($path, array('width'=>$this->inchToPx($width), 'height'=>$this->inchToPx($height), 'align'=>'center'));
-            }
-        }
-        
-        public function parseProps($attribute)
-        {
-            $data = explode(';', $attribute);
-            if(count($data) == 1){
-                return $attribute;
-            }
-            return $this->convertAttributes($data);
-        }
-        
-        public function convertAttributes($data){
-            foreach ($data as $attrs){
-                $attr = explode(':', $attrs);
-                switch (trim($attr[0])) {
-                    case 'table-column-props':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $index => $prop){
-                            $attrArray['width'.$index] = $prop;
-                        }
-                        break;
-                    case 'table-row-heights':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $index => $prop){
-                            $attrArray['height'.$index] = $prop;
-                        }
-                        break;
-                    case 'left-attach':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['colunmNum'] = $prop;
-                        }
-                        break;
-                    case 'top-attach':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['rowNum'] = $prop;
-                        }
-                        break;
-                    case 'top-color':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['borderTopColor'] = $prop;
-                        }
-                        break;
-                    case 'left-color':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['borderLeftColor'] = $prop;
-                        }
-                        break;
-                    case 'right-color':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['borderRightColor'] = $prop;
-                        }
-                        break;
-                    case 'bot-color':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['borderBottomColor'] = $prop;
-                        }
-                        break;
-                    case 'height':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['height'] = $prop;
-                        }
-                        break;
-                    case 'width':
-                        $props = explode('/', $attr[1]);
-                        foreach($props as $prop){
-                            $attrArray['width'] = $prop;
-                        }
-                        break;
-                }
-            }
-            return $attrArray;
-        }
-
         function handle_d()
         {
             if ($this->pass == 2) {
@@ -236,27 +107,173 @@ class File_Convert_AbiToDocx
             $this->pass = 2;
             $this->parseAbi();
         }
+//                    // Handle All The Elements
+//                    if($xr->name === 'table'){
+//                        //New Section
+//                        $section = $PHPWord->createSection();
+//                        // Draw Table
+//                        $this->drawTable($section, $xr);
+//                    }elseif($xr->name === 'image'){
+//                        //New Section
+//                        $section = $PHPWord->createSection();
+//                        // Draw Image
+//                        $this->drawImage($section,$xr);
+//                    }
+//                }
+//                // Close XML Reader
+//                $xr->close();
+//                // Save File
+//                $this->saveDocx($PHPWord);
+//	}
+//        
         
         
-        public function inchToPx($num){
-            return $num * 75;
-        }
-         
-        public function saveDocx(Document_Word_Writer $PHPWord){
-            require_once __DIR__ . '/../../Document/Word/Writer/IOFactory.php';
-            $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
-            $objWriter->save('/tmp/AbiToDocx.docx');
-        }
-        
-        public function getAbiFileName() 
+//        public function drawTable($section, $xr){
+//                // Define table style arrays
+//                $tableStyle = $this->parseProps($xr->getAttribute('props'));
+//                // Add table
+//                $table = $section->addTable();
+//                // Convert xr Element to DOM Object
+//                $tableObj = $xr->expand();
+//                // Draw The Table
+//                foreach($tableObj->childNodes as $cellObj){
+//                    if($cellObj->nodeName === 'cell'){
+//                        $cellStyle = $this->parseProps($cellObj->getAttribute('props'));
+//                        if($cellStyle['colunmNum'] == 0) {
+//                            $height = array_key_exists('height'.$cellStyle['rowNum'], $tableStyle) ? $tableStyle['height'.$cellStyle['rowNum']] : '';
+//                            $height = preg_replace('/[^0-9.]/', '', $height);
+//                            $table->addRow($this->inchToPx($height));
+//                        }   
+//                        
+//                        foreach($cellObj->childNodes as $pObj){
+//                            if($pObj->nodeName === 'p'){
+//                                $pStyle = $this->parseProps($pObj->getAttribute('style'));
+//                                $width = array_key_exists('width'.$cellStyle['colunmNum'], $tableStyle) ? $tableStyle['width'.$cellStyle['colunmNum']] : '';
+//                                $width = preg_replace('/[^0-9.]/', '', $width);
+//                                $table->addCell($this->inchToPx($width), $cellStyle)->addText($pObj->nodeValue, $pStyle);
+//                            }
+//                        }
+//                    }
+//                 }
+//        }
+        function parseTextBody($onto)
         {
-		return $this->_abiFileName;
-	}
-	
-	public function setProperties($abiFileName) 
-        {
-		$this->_abiFileName = $abiFileName;
-		return $this;
-	}    
+            
+        }
+        
+        
+//        public function drawImage($section, $xr){
+//            // Get The Name of image
+//            $imageId = $xr->getAttribute('dataid');
+//            $path = '/tmp/'.$imageId.'.jpg';
+//            if(file_exists($path)){
+//                $imageStyle = $this->parseProps($xr->getAttribute('props'));
+//                $width = preg_replace('/[^0-9.]/', '', $imageStyle['width']);
+//                $height = preg_replace('/[^0-9.]/', '', $imageStyle['height']);
+//                $section->addImage($path, array('width'=>$this->inchToPx($width), 'height'=>$this->inchToPx($height), 'align'=>'center'));
+//            }
+//        }
+        
+//        public function parseProps($attribute)
+//        {
+//            $data = explode(';', $attribute);
+//            if(count($data) == 1){
+//                return $attribute;
+//            }
+//            return $this->convertAttributes($data);
+//        }
+        
+//        public function convertAttributes($data){
+//            foreach ($data as $attrs){
+//                $attr = explode(':', $attrs);
+//                switch (trim($attr[0])) {
+//                    case 'table-column-props':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $index => $prop){
+//                            $attrArray['width'.$index] = $prop;
+//                        }
+//                        break;
+//                    case 'table-row-heights':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $index => $prop){
+//                            $attrArray['height'.$index] = $prop;
+//                        }
+//                        break;
+//                    case 'left-attach':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['colunmNum'] = $prop;
+//                        }
+//                        break;
+//                    case 'top-attach':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['rowNum'] = $prop;
+//                        }
+//                        break;
+//                    case 'top-color':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['borderTopColor'] = $prop;
+//                        }
+//                        break;
+//                    case 'left-color':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['borderLeftColor'] = $prop;
+//                        }
+//                        break;
+//                    case 'right-color':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['borderRightColor'] = $prop;
+//                        }
+//                        break;
+//                    case 'bot-color':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['borderBottomColor'] = $prop;
+//                        }
+//                        break;
+//                    case 'height':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['height'] = $prop;
+//                        }
+//                        break;
+//                    case 'width':
+//                        $props = explode('/', $attr[1]);
+//                        foreach($props as $prop){
+//                            $attrArray['width'] = $prop;
+//                        }
+//                        break;
+//                }
+//            }
+//            return $attrArray;
+//        }
+
+        
+        
+        
+//        public function inchToPx($num){
+//            return $num * 75;
+//        }
+//         
+//        public function saveDocx(Document_Word_Writer $PHPWord){
+//            require_once __DIR__ . '/../../Document/Word/Writer/IOFactory.php';
+//            $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
+//            $objWriter->save('/tmp/AbiToDocx.docx');
+//        }
+//        
+//        public function getAbiFileName() 
+//        {
+//		return $this->_abiFileName;
+//	}
+//	
+//	public function setProperties($abiFileName) 
+//        {
+//		$this->_abiFileName = $abiFileName;
+//		return $this;
+//	}    
 }
 ?>
