d5cdcce7920f90076a0b2188441aaa3544ea525d
Edward <edward@roojs.com>
2012-12-06 13:18:02 +0800

    Document/Word/Examples/BasicTable.php

--- Document/Word/Examples/BasicTable.php
+++ Document/Word/Examples/BasicTable.php
@@ -10,16 +10,21 @@ $section = $PHPWord->createSection();
 // Add table
 $table = $section->addTable();
 
-for($r = 1; $r <= 10; $r++) { // Loop through rows
-	// Add row
-	$table->addRow();
-	
-	for($c = 1; $c <= 5; $c++) { // Loop through cells
-		// Add Cell
-		$table->addCell(1750)->addText("Row $r, Cell $c");
-	}
-}
-
+//for($r = 1; $r <= 10; $r++) { // Loop through rows
+//	// Add row
+//	$table->addRow();
+//	
+//	for($c = 1; $c <= 5; $c++) { // Loop through cells
+//		// Add Cell
+//		$table->addCell(1750)->addText("Row $r, Cell $c");
+//	}
+//}
+$table->addRow(200);
+$table->addCell(300)->addText('ffffffffffffffffffffffffffffffffffffffffffffff');
+$table->addCell(100)->addText('dddddddddddddd');
+$table->addRow(200);
+$table->addCell(300)->addText('ffffffffffffffffffffffffffffffffffffffffffffff');
+$table->addCell(100)->addText('dddddddddddddd');
 // Save File
 require_once __DIR__ . '/../Writer/IOFactory.php';
 $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
