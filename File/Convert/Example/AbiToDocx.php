6ac78642ee14cde34f9f72c5624e200b2ae0ca55
Edward <edward@roojs.com>
2012-12-06 12:52:32 +0800

    File/Convert/Example/AbiToDocx.php

--- File/Convert/Example/AbiToDocx.php
+++ File/Convert/Example/AbiToDocx.php
@@ -8,24 +8,24 @@ $abiFileName = __DIR__ . '/../../../../../Documents/146-test.abw';
 $conv = new File_Convert_AbiToDocx();
 $conv->save($abiFileName);
 //$xml = new XMLReader();
-
+echo gethostname();
 // Download the file for testing
-$file = '/tmp/abiTodocx.docx';
-if (file_exists($file)) {
-    echo 'Prepare for download!!';
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    ob_clean();
-    flush();
-    readfile($file);
-    exit;
-}
+//$file = '/tmp/abiTodocx.docx';
+//if (file_exists($file)) {
+//    echo 'Prepare for download!!';
+//    header('Content-Description: File Transfer');
+//    header('Content-Type: application/octet-stream');
+//    header('Content-Disposition: attachment; filename='.basename($file));
+//    header('Content-Transfer-Encoding: binary');
+//    header('Expires: 0');
+//    header('Cache-Control: must-revalidate');
+//    header('Pragma: public');
+//    header('Content-Length: ' . filesize($file));
+//    ob_clean();
+//    flush();
+//    readfile($file);
+//    exit;
+//}
 
 
 ?>
