400ac9a2c9bfe4c6cadcd48acb3b9674ed6b7226
Edward <edward@roojs.com>
2012-12-06 18:53:45 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -142,8 +142,9 @@ class File_Convert_AbiToDocx
             }
             $this->setNodeStyle('image', 'props'); // Define image style
             $image = $this->xr->getAttribute('dataid');
+            print_r($image);
             print_r($this->style['image'].'<br/>');
-            $this->section->addImage($image . '.jpg', array('width'=>210, 'height'=>210, 'align'=>'center'));
+           // $this->section->addImage($image . '.jpg', array('width'=>210, 'height'=>210, 'align'=>'center'));
             
         }
         
