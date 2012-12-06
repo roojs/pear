d3dfc8b715cf9f0492737c7a2ea344d9a8fbc189
Edward <edward@roojs.com>
2012-12-06 14:38:52 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -89,7 +89,7 @@ class File_Convert_AbiToDocx
             if(array_key_exists('width' . $this->style['cell']['colunmNum'], $this->style['table'])){
                 $width = $this->parseWH($this->style['table']['width' . $this->style['cell']['colunmNum']]);
             }
-            $num = rand(0, 9)
+            $num = rand(0, 9);
             $cellStyle = array('bgColor'=>'C0C'.$num.'C'.$num);
             $this->cell = $this->table->addCell($width, $cellStyle);
             $this->lastNode = 'cell';
