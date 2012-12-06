10fee8510e3d5b1e5d69886beaf9bfe66aa349bf
Edward <edward@roojs.com>
2012-12-06 19:38:15 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -151,7 +151,7 @@ class File_Convert_AbiToDocx
         
         function handle_pbr() 
         {
-            $this->section = $this->writer->createSection(array('borderColor'=>'00FF00', 'borderSize'=>12));
+            $this->section = $this->writer->createSection();
         }
         
         function parseWH($wh,$type=null)
