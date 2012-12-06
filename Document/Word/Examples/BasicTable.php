00c98cd095d54c69cc323cc1152e0bc18efd4cd8
Edward <edward@roojs.com>
2012-12-06 13:18:37 +0800

    Document/Word/Examples/BasicTable.php

--- Document/Word/Examples/BasicTable.php
+++ Document/Word/Examples/BasicTable.php
@@ -29,4 +29,21 @@ $table->addCell(100)->addText('dddddddddddddd');
 require_once __DIR__ . '/../Writer/IOFactory.php';
 $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
 $objWriter->save('/tmp/BasicTable.docx');
+
+$file = '/tmp/BasicTable.docx';
+if (file_exists($file)) {
+    echo 'Prepare for download!!';
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
 ?>
