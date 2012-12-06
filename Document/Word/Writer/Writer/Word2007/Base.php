0c9fb6acd68793f3252b4985e2556fa177329ee0
Edward <edward@roojs.com>
2012-12-06 16:24:51 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -402,7 +402,6 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                                 $objWriter->writeAttribute('w:type', 'fixed');
                                 $objWriter->endElement();
                                 $objWriter->endElement();
-
                                 
 				$_heights = $table->getRowHeights();
 				for($i=0; $i<$_cRows; $i++) {
