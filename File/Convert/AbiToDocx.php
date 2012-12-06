42764359b8a5db0aebd9a167b050a0b5f9ec429e
Edward <edward@roojs.com>
2012-12-06 17:34:46 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -101,7 +101,7 @@ class File_Convert_AbiToDocx
             $this->setNodeStyle('p', 'props'); // Define p style
             $pStyle = $this->style['p'];
             
-            if(strpos($this->xr->readInnerXML(), 'ahnews'))
+            if(strpos($this->xr->readInnerXML(), '<a'))
                 echo 'ddd';
             echo $this->xr->depth . '<br/>';
             if($this->lastNode == 'cell'){
