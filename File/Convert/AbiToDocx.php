9e2f075aea921a6df44522c0a1f83795d8686603
Edward <edward@roojs.com>
2012-12-06 11:04:01 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -47,7 +47,7 @@ class File_Convert_AbiToDocx
                     }
                     $method = 'handle_'.$this->xr->name;
                     if (!method_exists($this, $method)) {
-                        echo "NOT HANLED {$this->xr->name} <br/>";
+//                        echo "NOT HANLED {$this->xr->name} <br/>";
                     }else{
                         $this->$method();   
                     }
