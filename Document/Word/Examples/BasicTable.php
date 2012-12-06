c0cfb953cc29c69374258d93eb4bbb8c7015751f
Edward <edward@roojs.com>
2012-12-06 13:35:32 +0800

    Document/Word/Examples/BasicTable.php

--- Document/Word/Examples/BasicTable.php
+++ Document/Word/Examples/BasicTable.php
@@ -26,4 +26,27 @@ $table->addCell(2000)->addText('Cell 6');
 require_once __DIR__ . '/../Writer/IOFactory.php';
 $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
 $objWriter->save('/tmp/BasicTable.docx');
+
+// Download the file for testing
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    exit;
+}
+$file = '/tmp/BasicTable.docx';
+if (file_exists($file)) {
+    echo 'Prepare for download!!';
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+}
+
 ?>
