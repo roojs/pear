9abf2ae33acea4cc304c24072ce0e90f5d459c94
Edward <edward@roojs.com>
2012-12-06 13:20:01 +0800

    Document/Word/Examples/BasicTable.php

--- Document/Word/Examples/BasicTable.php
+++ Document/Word/Examples/BasicTable.php
@@ -10,40 +10,18 @@ $section = $PHPWord->createSection();
 // Add table
 $table = $section->addTable();
 
-//for($r = 1; $r <= 10; $r++) { // Loop through rows
-//	// Add row
-//	$table->addRow();
-//	
-//	for($c = 1; $c <= 5; $c++) { // Loop through cells
-//		// Add Cell
-//		$table->addCell(1750)->addText("Row $r, Cell $c");
-//	}
-//}
-$table->addRow(200);
-$table->addCell(300)->addText('ffffffffffffffffffffffffffffffffffffffffffffff');
-$table->addCell(100)->addText('dddddddddddddd');
-$table->addRow(200);
-$table->addCell(300)->addText('ffffffffffffffffffffffffffffffffffffffffffffff');
-$table->addCell(100)->addText('dddddddddddddd');
+for($r = 1; $r <= 10; $r++) { // Loop through rows
+	// Add row
+	$table->addRow();
+	
+	for($c = 1; $c <= 5; $c++) { // Loop through cells
+		// Add Cell
+		$table->addCell(1750)->addText("Row $r, Cell $c");
+	}
+}
+
 // Save File
 require_once __DIR__ . '/../Writer/IOFactory.php';
 $objWriter = Document_Word_Writer_IOFactory::createWriter($PHPWord, 'Word2007');
 $objWriter->save('/tmp/BasicTable.docx');
-
-$file = '/tmp/BasicTable.docx';
-if (file_exists($file)) {
-    echo 'Prepare for download!!';
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    ob_clean();
-    flush();
-    readfile($file);
-    exit;
-}
 ?>
