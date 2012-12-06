80877390863179032b1bd1fe91a864050e94e092
Edward <edward@roojs.com>
2012-12-06 13:54:39 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -116,7 +116,7 @@ class File_Convert_AbiToDocx
         {
             print_r($wh);
             $num = preg_replace('/[^0-9.]/', '', $wh);
-            print_r($num);
+            print_r('<br/> ' . $num);
             return $num * $this->DPI;
         }
         
