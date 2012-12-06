bbc3a06adfc92b61bb5b736b0446f3fb0e8cf8ce
Edward <edward@roojs.com>
2012-12-06 19:32:43 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -151,7 +151,6 @@ class File_Convert_AbiToDocx
         
         function handle_pbr() 
         {
-            echo 'here<br/>';
             $this->section = $this->writer->createSection();
         }
         
