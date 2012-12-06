3411ca4736d7f924c781b3c9f1855bcf4a169d0a
Edward <edward@roojs.com>
2012-12-06 14:37:31 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -89,7 +89,8 @@ class File_Convert_AbiToDocx
             if(array_key_exists('width' . $this->style['cell']['colunmNum'], $this->style['table'])){
                 $width = $this->parseWH($this->style['table']['width' . $this->style['cell']['colunmNum']]);
             }
-            $cellStyle = array('bgColor'=>'C0C0C0');
+            $num = rand(0, 9);
+            $cellStyle = array('bgColor'=>'C0C0C'.$num);
             $this->cell = $this->table->addCell($width, $cellStyle);
             $this->lastNode = 'cell';
         }
