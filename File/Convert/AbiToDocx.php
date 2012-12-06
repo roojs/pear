41a2e4e5b4477521c5fc035a339f6083bfeb5bf0
Edward <edward@roojs.com>
2012-12-06 15:40:21 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -102,7 +102,7 @@ class File_Convert_AbiToDocx
                 if($pStyle == 'Normal'){
                     $pStyle = $this->style['Normal'];
                 }
-                $this->cell->addText(wordwrap($this->xr->readString(),$this->cellWidth,'<br/>\n'), $pStyle);
+                $this->cell->addText(wordwrap($this->xr->readString(),20,'<br/>\n'), $pStyle);
             }
             
         }
