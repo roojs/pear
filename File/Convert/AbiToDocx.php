50332093e4fe4d4546fab44597049a8d3175af83
Edward <edward@roojs.com>
2012-12-07 12:33:53 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -215,12 +215,10 @@ class File_Convert_AbiToDocx
             {
                 if($this->xr->readString() != ''){
                     $this->headerText .= $this->xr->readString();
-                    $this->header->addText($this->headerText , $this->style['c']);
                 }
             }elseif($this->sectionType == 'footer') {
                 if($this->xr->readString() != ''){
                     $this->footerText .= $this->xr->readString();
-                    $this->footer->addText($this->footerText , $this->style['c']);
                 }
             }
         }
