b4c6d9ff8cec4be85f5a228a3eff8a1c8195cf36
Edward <edward@roojs.com>
2012-12-07 15:38:43 +0800

    File/Convert/Example/AbiToDocx.php

--- File/Convert/Example/AbiToDocx.php
+++ File/Convert/Example/AbiToDocx.php
@@ -4,16 +4,16 @@ require_once __DIR__ . '/../AbiToDocx.php';
 
 //Example AbiWord file
 $abiFileName = __DIR__ . '/../../../../../Documents/ExportBookWord.abw';
-$fn = '/tmp/ExportBookWord.doc';
+$fn = '/tmp/ExportBookWord.docx';
 $conv = new File_Convert_AbiToDocx($abiFileName);
 $conv->save($fn);
 //$xml = new XMLReader();
 
 // Download the file for testing
-//if($_SERVER['SERVER_NAME'] == 'localhost')
-//{
-//    exit;
-//}
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    exit;
+}
 if (file_exists($fn)) {
     echo 'Prepare for download!!';
     header('Content-Description: File Transfer');
