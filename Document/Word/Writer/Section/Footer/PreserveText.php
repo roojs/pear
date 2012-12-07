9a60ee0378775e2b80970b8031d91e829af735c7
Edward <edward@roojs.com>
2012-12-07 13:28:18 +0800

    Document/Word/Writer/Section/Footer/PreserveText.php

--- Document/Word/Writer/Section/Footer/PreserveText.php
+++ Document/Word/Writer/Section/Footer/PreserveText.php
@@ -83,6 +83,7 @@ class Document_Word_Writer_Section_Footer_PreserveText
 		
 		// Set paragraph style
 		if(is_array($styleParagraph)) {
+                        require_once __DIR__ . '/../../Style/Paragraph.php';
 			$this->_styleParagraph = new Document_Word_Writer_Style_Paragraph();
 			
 			foreach($styleParagraph as $key => $value) {
