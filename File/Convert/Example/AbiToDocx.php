55c7d6b2881492f95fbc72cf7c0a99300dbe94ea
Edward <edward@roojs.com>
2012-12-07 12:27:03 +0800

    File/Convert/Example/AbiToDocx.php

--- File/Convert/Example/AbiToDocx.php
+++ File/Convert/Example/AbiToDocx.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../AbiToDocx.php';
 
 //Example AbiWord file
 $abiFileName = __DIR__ . '/../../../../../Documents/146-test.abw';
-
+$fn = '/tmp/abiword.docx';
 $conv = new File_Convert_AbiToDocx($abiFileName);
-$conv->save('/tmp/abiword.docx');
+$conv->save($fn);
 //$xml = new XMLReader();
 
 // Download the file for testing
@@ -14,20 +14,19 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
 {
     exit;
 }
-$file = '/tmp/abiTodocx.docx';
-if (file_exists($file)) {
+if (file_exists($fn)) {
     echo 'Prepare for download!!';
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Disposition: attachment; filename='.basename($fn));
     header('Content-Transfer-Encoding: binary');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
+    header('Content-Length: ' . filesize($fn));
     ob_clean();
     flush();
-    readfile($file);
+    readfile($fn);
     exit;
 }
 
