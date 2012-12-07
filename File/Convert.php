7b10eeff2d9d80b1bb3940300b83358a02288df1
Alan Knowles <alan@roojs.com>
2012-12-07 10:44:27 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -841,9 +841,7 @@ class File_Convert_Solution
         clearstatcache();
         
         return  file_exists($target)  && filesize($target) ? $target : false;
-        
-        
-        
+         
     }
     function ffmpeg($fn)
     {
@@ -892,6 +890,23 @@ class File_Convert_Solution
         `$cmd`;
         $this->cmd = $cmd;
         clearstatcache();
+        
+        return  file_exists($target)  && filesize($target) ? $target : false;
+    }
+     function abitodocx($fn)
+    {
+        require_once 'File/MimeType.php';
+        $fmt = new File_MimeType();
+        $fext = $fmt->toExt($this->from);
+        
+        $ext = $this->ext;
+        $target = str_replace('.', '_', $fn) . '.' . $ext;
+        if (file_exists($target)  && filesize($target) && filemtime($target) > filemtime($fn)) {
+            return $target;
+        }
+        require_once 'File/Convert/AbiToDocx.php';
+        $conv = new File_Convert_AbiToDocx($fn);
+        $conv->save($target); 
         
         return  file_exists($target)  && filesize($target) ? $target : false;
     }
