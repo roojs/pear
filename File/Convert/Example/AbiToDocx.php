2238df0ff70a977bb512e84aa3565430eb9166c2
Edward <edward@roojs.com>
2012-12-07 13:44:09 +0800

    File/Convert/Example/AbiToDocx.php

--- File/Convert/Example/AbiToDocx.php
+++ File/Convert/Example/AbiToDocx.php
@@ -3,7 +3,7 @@ ini_set('include_path', __DIR__  . '/../../..:.');
 require_once __DIR__ . '/../AbiToDocx.php';
 
 //Example AbiWord file
-$abiFileName = __DIR__ . '/../../../../../Documents/146-test.abw';
+$abiFileName = __DIR__ . '/../../../../../Documents/ExportBookWord.abw';
 $fn = '/tmp/abiword.docx';
 $conv = new File_Convert_AbiToDocx($abiFileName);
 $conv->save($fn);
