7b8862063d1893bbcf1b3c849b0c3fe3470456f8
Edward <edward@roojs.com>
2012-12-07 15:37:00 +0800

    File/Convert/Example/AbiToDocx.php

--- File/Convert/Example/AbiToDocx.php
+++ File/Convert/Example/AbiToDocx.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../AbiToDocx.php';
 
 //Example AbiWord file
 $abiFileName = __DIR__ . '/../../../../../Documents/ExportBookWord.abw';
-$fn = '/tmp/ExportBookWord.docx';
+$fn = '/tmp/ExportBookWord.doc';
 $conv = new File_Convert_AbiToDocx($abiFileName);
 $conv->save($fn);
 //$xml = new XMLReader();
