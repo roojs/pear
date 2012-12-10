7456a1b7b691eb62a13acf000456ee1260c4938c
Alan Knowles <alan@roojs.com>
2012-12-10 17:13:21 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -404,21 +404,24 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
         <w:gridCol w:w="7030" />
       </w:tblGrid>
             */
+            $cw = $table->getColumnWidths();
             
-            $objWriter->startElement('w:tblGrid');
-            foreach($table->getColumnWidths() as $w) {
-                $objWriter->startElement('w:gridCol');
-                $objWriter->setAttribute('w:w', $w);
-            }
-            $objWriter->endElement();
-
-            
+            if (empty($cw)) {
+                $objWriter->startElement('w:tblGrid');
+                foreach($table->getColumnWidths() as $w) {
+                    $objWriter->startElement('w:gridCol');
+                    $objWriter->setAttribute('w:w', $w);
+                }
+                $objWriter->endElement();
+            } else { 
             
             
             // Fixed the cell width
-            $objWriter->startElement('w:tblLayout');
-            $objWriter->writeAttribute('w:type', 'fixed');
-            $objWriter->endElement();
+                $objWriter->startElement('w:tblLayout');
+                $objWriter->writeAttribute('w:type', 'fixed');
+                $objWriter->endElement();
+            }
+            
             $objWriter->endElement();
 
             $_heights = $table->getRowHeights();
