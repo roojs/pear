38c2c195c4647a3610431de4d8725498eb54a286
Edward <edward@roojs.com>
2012-12-10 14:32:45 +0800

    Document/Word/Examples/Image.php

--- Document/Word/Examples/Image.php
+++ Document/Word/Examples/Image.php
@@ -13,7 +13,7 @@ $section = $PHPWord->createSection();
 //$section->addTextBreak(2);
 $section->addText('Below is earth image!'); 
 $section->addTextBreak(2);
-$section->addImage('_earth.JPG', array('width'=>210, 'height'=>210));
+$section->addImage('_earth.JPG', array('width'=>500, 'height'=>500));
 $section->addTextBreak(2);
 $section->addText('Above is earth image!');
 //$section->addImage('_mars.jpg');
