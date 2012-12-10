f42806cbabd9e289e9fe8f2edbfb09914c4c8513
Alan Knowles <alan@roojs.com>
2012-12-10 17:40:55 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -709,7 +709,7 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
 //                $objWriter->endElement(); // End wp:wrapSquare
                 $objWriter->startElement('wp:docPr');
                 $objWriter->writeAttribute('descr','A description...');
-                $objWriter->writeAttribute('id',$embedid++;);
+                $objWriter->writeAttribute('id',$embedid++);
                 $objWriter->writeAttribute('name','Picture');
                 $objWriter->endElement(); // End wp:docPr
                 $objWriter->startElement('wp:cNvGraphicFramePr');
