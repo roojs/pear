a4b5af349c6c390881c970722f228636c9f0b326
Alan Knowles <alan@roojs.com>
2012-12-10 18:44:34 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -251,10 +251,13 @@ class File_Convert_AbiToDocx
             if($this->sectionType == 'header')
             {
                 if($this->xr->readString() != ''){
+                    // fixme - kludge as parse does not subparse <fields>
                     $this->header->addText($this->xr->readString() , $this->style['c']);
                 }
             }elseif($this->sectionType == 'footer') {
                 if($this->xr->readString() != ''){
+                    // fixme - kludge as parse does not subparse <fields>
+                    
                     $this->footer->addText($this->xr->readString() , $this->style['c']);
                 }
             }
