c64d6a19f89efd236269da1eac785d513faac755
Edward <edward@roojs.com>
2012-12-18 20:16:08 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -107,7 +107,7 @@ class File_Convert_AbiToDocx
                     //echo implode('..', $stack). ':' .$this->xr->name.'<BR/>';
                     //$this->dumpsections($sections);
                     
-                    if (!$this->xr->isEmptyElement && $this->xr->name == 's') {
+                    if (!$this->xr->isEmptyElement || $this->xr->name == 's') {
                         echo $this->xr->name.'<Br/>';
                        $stack[] = $this->xr->name;
                        $sections[] = $this->section;
