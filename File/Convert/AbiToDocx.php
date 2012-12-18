86c33fe8abca0e6bae287a5ff0baa39b23aa71cb
Edward <edward@roojs.com>
2012-12-18 16:32:23 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -31,8 +31,8 @@ class File_Convert_AbiToDocx
         {
                 require_once __DIR__ . '/../../Document/Word/Writer.php';
                 require_once __DIR__ . '/../../System.php';
-                $this->tmpdir  = System::mktemp("-d abitodocx");
-                //$this->tmpdir  = '/tmp';
+               // $this->tmpdir  = System::mktemp("-d abitodocx");
+                $this->tmpdir  = '/tmp';
                 $this->lastNode = '';
                 $this->style[] = array();
                 $this->metadata[] = array();
