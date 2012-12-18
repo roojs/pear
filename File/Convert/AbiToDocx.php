7d3f8df4034ca61b999f3d8c8fd0892db8896269
Edward <edward@roojs.com>
2012-12-18 19:02:27 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -78,8 +78,7 @@ class File_Convert_AbiToDocx
                             continue;
 //                        echo "NOT HANLED {$this->xr->name} <br/>";
                     } 
-                    echo $method . '<br/>';
-                  //  $this->$method();  
+                  /  $this->$method();  
                     $sections[] = $this->section;
                     $state[] = $this->style;
                 }
