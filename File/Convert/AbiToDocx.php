22fecdd67f3fa97a7e00100e17d17e3dbeede713
Edward <edward@roojs.com>
2012-12-18 17:15:59 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -43,8 +43,6 @@ class File_Convert_AbiToDocx
                 $this->parseAbi();
                 $this->pass = 2;
                 $this->parseAbi();
-                print_r($this->styles);
-                exit;
                 $this->saveDocx( $fn ); // uses this->writer...
                 
         }
