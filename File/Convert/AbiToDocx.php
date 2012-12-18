710a9e6db05497f9e34394efc15ae89964fbc0ed
Edward <edward@roojs.com>
2012-12-18 19:20:47 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -78,10 +78,10 @@ class File_Convert_AbiToDocx
                             continue;
 //                        echo "NOT HANLED {$this->xr->name} <br/>";
                     } 
-                   // $this->$method();  
+                    $this->$method();  
                     
                     if($this->xr->isEmptyElement){
-                        echo $method.'<Br/>';
+                        //echo $method.'<Br/>';
                             continue;
                     }
                     
