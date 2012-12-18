0eb86d34f3edc054043d9b46dc1e2bb3554e63f1
Edward <edward@roojs.com>
2012-12-18 16:58:30 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -17,7 +17,7 @@ class File_Convert_AbiToDocx
             require_once dirname(__FILE__) .'/../Convert.php';
             $x = new File_Convert($fn, 'application/abiword' );
             $x->convert('application/abiword');
-            $x->serve('attachment');
+            $x->serve('inline');
         
             exit;
             
