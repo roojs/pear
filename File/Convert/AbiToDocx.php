4e551530419d4ddd15d913e8b23b065e353cf539
Edward <edward@roojs.com>
2012-12-19 11:17:24 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -110,6 +110,7 @@ class File_Convert_AbiToDocx
                        $sections[] = $this->section;
                        $state[] = $this->style;
                     }
+                    echo get_class($this->section) . '<br/>';
                     $this->$method();  
                     
                     //if($this->xr->isEmptyElement){
