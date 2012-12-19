8b3b4ca68797a07f8e128e33ec6bffde01924c86
Edward <edward@roojs.com>
2012-12-19 12:29:48 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -628,7 +628,7 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
 		}
 	}
 	
-	function _writeImage(Document_Word_Writer_Shared_XMLWriter $objWriter = null, $image) 
+	function _writeImage(Document_Word_Writer_Shared_XMLWriter $objWriter = null, $image, $skip_para = false) 
     {
              
             static $embedid = 0;
