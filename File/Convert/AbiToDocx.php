14ade1db38682444e8d2a7563b9efc2202e5ec35
Edward <edward@roojs.com>
2012-12-20 14:57:14 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -90,7 +90,7 @@ class File_Convert_AbiToDocx
                         if(strpos($text, '{#PAGE#}') !== false || strpos($text, '{#NUMPAGES#}') !== false){
                             $this->section->addPreserveText(str_replace("#", "", $text), $this->style,$this->style);
                             
-                        }elseif(is_array ($this->style) && !empty($this->style['href'])) {
+                        }elseif(!empty($this->style['href'])) {
                             $this->section->addLink($this->style['href'], $text,  $this->style);
                            
                         }else{
