4d681ed2d6587bd4d0744b0d6f30ae558e6e0979
Alan Knowles <alan@roojs.com>
2012-12-28 10:01:58 +0800

    remove comments

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -752,12 +752,7 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                 $objWriter->endElement(); // End wp:effectExtent
                 
                 
-                
-               // $objWriter->startElement('wp:wrapTopAndBottom');
-                //$objWriter->endElement(); // End wp:wrapNone
-//                $objWriter->startElement('wp:wrapSquare');
-//                $objWriter->writeAttribute('wrapText','largest');
-//                $objWriter->endElement(); // End wp:wrapSquare
+                 
                 $objWriter->startElement('wp:docPr');
                 $objWriter->writeAttribute('descr','A description...');
                 $objWriter->writeAttribute('id',$embedid);
