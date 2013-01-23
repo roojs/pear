67fdf71eb4bc32c64fa2d28bd7d595471be5c48c
Chris <chris@roojs.com>
2013-01-23 13:47:54 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -443,7 +443,9 @@ class File_Convert_AbiToDocx
         function handle_pagesize()
         {
             // fill this in so it can handle landscape
-            
+            if ($this->pass != 2) {
+                return;
+            }
             return;
         }
         
