91b0046940d0de74d4352f67672378e8f9608b3f
Chris <chris@roojs.com>
2013-01-23 14:48:18 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -264,7 +264,7 @@ class File_Convert_AbiToDocx
                 return;
             }
             $this->style =  $this->parseProps();
-            
+            print_r($this->section);exit;
         }
         
         function handle_d()
