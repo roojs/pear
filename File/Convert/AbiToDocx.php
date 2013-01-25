03d82c42aef2776af744d35f3118492595020973
Chris <chris@roojs.com>
2013-01-25 13:19:22 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -453,7 +453,7 @@ class File_Convert_AbiToDocx
             
             $this->style['pageSizeW'] = $this->xr->getAttribute('width');
             $this->style['pageSizeH'] = $this->xr->getAttribute('height');
-            print_r($this);
+            print_r($this->section);
             exit;
            // print_r($this->xr->getAttribute('orientation'));exit;
             return;
