ae60faa9fe106fcf8e908c49669fe4ec06dc7487
Edward <edward@roojs.com>
2013-01-28 19:34:33 +0800

    HTML/CSS/InlineStyle.php

--- HTML/CSS/InlineStyle.php
+++ HTML/CSS/InlineStyle.php
@@ -229,7 +229,6 @@ class HTML_CSS_InlineStyle
 */
     public function parseStylesheet($stylesheet)
     {
-        print_r($stylesheet);
         $parsed = array();
         $stylesheet = $this->_stripStylesheet($stylesheet);
         $stylesheet = trim(trim($stylesheet), "}");
