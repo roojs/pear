b740e295dfb9aefb774552fbefeb424bd5b49da9
Chris <chris@roojs.com>
2013-03-04 18:49:54 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -137,7 +137,7 @@ class File_Convert_AbiToDocx
                 return;
             }
             $this->style =  $this->parseProps();
-            print_r($this->style);
+//            print_r($this->style);
             $this->section = $this->section->addTable($this->style); // Add table
             
         }
