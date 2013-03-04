76fffbb596707e0f599c1fecf7a255fcd7974b23
Chris <chris@roojs.com>
2013-03-04 18:23:48 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -427,9 +427,9 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                     $objWriter->writeAttribute('w:w', array_sum($cw));
                     $objWriter->endElement();
                 }
-                $objWriter->startElement('w:tblLayout');
-                $objWriter->writeAttribute('w:type', 'fixed');
-                $objWriter->endElement();
+//                $objWriter->startElement('w:tblLayout');
+//                $objWriter->writeAttribute('w:type', 'fixed');
+//                $objWriter->endElement();
 //                
 //                <w:tblPr>
 //                    <w:tblW w:type="dxa" w:w="8430"/>
@@ -438,7 +438,7 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                
                 $tblStyle = $table->getStyle();
                 
-//                var_dump($table);exit;
+                var_dump($cw);exit;
                 
                 if ($tblStyle instanceof Document_Word_Writer_Style_Table) {
                     $this->_writeTableStyle($objWriter, $tblStyle);
