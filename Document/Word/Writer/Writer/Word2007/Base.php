f386e6b4c7768ffd3a41dcdce34bce6a47f76e40
Chris <chris@roojs.com>
2013-03-04 18:26:07 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -438,7 +438,7 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                
                 $tblStyle = $table->getStyle();
                 
-                var_dump($cw);exit;
+//                var_dump($cw);exit;
                 
                 if ($tblStyle instanceof Document_Word_Writer_Style_Table) {
                     $this->_writeTableStyle($objWriter, $tblStyle);
