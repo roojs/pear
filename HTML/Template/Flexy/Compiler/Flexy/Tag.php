ee2da1e16741bd95fc6ec41f3f14c27c9febb1fc
Chris <chris@roojs.com>
2013-05-28 17:37:29 +0800

    HTML/Template/Flexy/Compiler/Flexy/Tag.php

--- HTML/Template/Flexy/Compiler/Flexy/Tag.php
+++ HTML/Template/Flexy/Compiler/Flexy/Tag.php
@@ -961,12 +961,6 @@ class HTML_Template_Flexy_Compiler_Flexy_Tag
             
         }
         if (in_array(strtoupper($this->element->getAttribute('TYPE')), array('TEXT',''))){
-            print_r($this->element->getAttribute('ID'));
-                print_r($this->element->getAttribute('PLACEHOLDER'));
-            if($this->element->getAttribute('ID') == 'search-key'){
-                
-                die('inin');
-            }
             $this->compiler->addStringToGettext($this->element->getAttribute('PLACEHOLDER'));
         } 
         // form elements : format:
