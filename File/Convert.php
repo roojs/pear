533536eec50900c68247d43afc7e108046ec16da
Chris <chris@roojs.com>
2013-06-17 18:41:50 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1362,7 +1362,7 @@ class File_Convert_Solution
             // both x & y..
             default: 
                 $scale = "{$x}x{$y}>"; 
-                $extent =" -gravity center -crop {$x}x{$y}+0+{$x}";
+                $extent =" -gravity center -crop {$x}x{$y}+0x{$x}";
                 break;
         }
         require_once 'System.php';
