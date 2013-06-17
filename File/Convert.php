00a2b4c33e173607afefe3859a017776e2acfc25
Chris <chris@roojs.com>
2013-06-17 19:04:46 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1336,7 +1336,7 @@ class File_Convert_Solution
         $target = $fn . '.'.$x.'c'.$y.'.' . $ext;
 //        echo "GOT TARGET"  . $target;exit;
         if (file_exists($target)  && filesize($target) && filemtime($target) > filemtime($fn)) {
-//            echo "GOT TARGET"  . $target;exit;
+            echo "GOT TARGET"  . $target;exit;
             return $target;
         }
         
