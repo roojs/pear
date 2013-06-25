876bc88c2d2321cb17e7e73fe9b7314e042b48b9
Edward <edward@roojs.com>
2013-06-25 12:49:15 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -94,9 +94,7 @@ class File_Convert
     {
          //echo '<PRE>'; print_r(array('convert', func_get_args()));
         if ($toMimetype != $this->mimetype) {
-           
-           
-            print_r('in1?');
+
             $action = $this->getConvMethods($this->mimetype, $toMimetype);
             
             //echo '<PRE>';print_r($action);
