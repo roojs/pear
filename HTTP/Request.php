93182ea25cb81e86f1cd728bedb3b985d62dd3ea
Alan Knowles <alan@akbkhome.com>
2013-07-04 00:18:07 +0800

    HTTP/Request.php

--- HTTP/Request.php
+++ HTTP/Request.php
@@ -825,7 +825,7 @@ class HTTP_Request
             AND !empty($this->_response->_headers['location'])) {
 
             print_r($this->_response->_cookies);exit;
-            $rediret a= $this->_response->_headers['location'];
+            $redirect = $this->_response->_headers['location'];
 
             // Absolute URL
             if (preg_match('/^https?:\/\//i', $redirect)) {
