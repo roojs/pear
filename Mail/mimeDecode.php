a1f8cc6026bb1a2a37b8c91cb43e2e44e99af367
Alan Knowles <alan@roojs.com>
2013-08-05 11:15:31 +0800

    Mail/mimeDecode.php

--- Mail/mimeDecode.php
+++ Mail/mimeDecode.php
@@ -826,7 +826,13 @@ class Mail_mimeDecode extends PEAR
         $input = preg_replace("/=\r?\n/", '', $input);
 
         // Replace encoded characters
-		$input = preg_replace('/=([a-f0-9]{2})/ie', "chr(hexdec('\\1'))", $input);
+		// $input = preg_replace('/=([a-f0-9]{2})/ie', "chr(hexdec('\\1'))", $input);
+        
+        $input = preg_replace_callback(
+            '/=([a-f0-9]{2})/ie',
+            function($matches) {
+                return chr(hexdec($matches[0]));
+            }, $input);
 
         return $input;
     }
