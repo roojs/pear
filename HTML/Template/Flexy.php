e3e50fa2b00bde28c29cfe6a7675d6e745dbaaa3
Alan Knowles <alan@roojs.com>
2013-09-24 10:04:13 +0800

    HTML/Template/Flexy.php

--- HTML/Template/Flexy.php
+++ HTML/Template/Flexy.php
@@ -654,13 +654,13 @@ class HTML_Template_Flexy
     {
         $this->_bufferHandle = fopen($filename, 'w');
         
-        ob_start(array($this, 'addToBuffer') , 8192, true); // outer nesting..
-        ob_start( array($this, 'addToBuffer') , 8192, true);
+        //ob_start(array($this, 'addToBuffer') , 8192, true); // outer nesting..
+        ob_start( array($this, 'addToBuffer') , 4096, true);
         $this->outputObject($t,$elements);
         @ob_end_clean();
         
         // not sure why, but this emits errors... when it should not!
-        @ob_end_clean();
+        //@ob_end_clean();
         
         fclose($this->_bufferHandle);
         $this->_bufferHandle = false;
