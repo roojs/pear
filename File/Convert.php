3c8067ae3e0cedd39ee6fd02e085012461b9a728
Alan Knowles <alan@roojs.com>
2013-11-14 18:12:18 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1333,7 +1333,7 @@ class File_Convert_Solution
             case (empty($y)) :
                 $scale = "{$x}x>";
                 
-                print_r(array($x,$width));
+               
                 exit;
                 if ($x == $width) {  // no need to change
                     return $fn;
