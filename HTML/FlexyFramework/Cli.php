ac0ed8dc73981d085c9e1fa36cbbf02b7f8faf08
Alan Knowles <alan@roojs.com>
2013-11-15 13:19:45 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -195,7 +195,7 @@ Available commands:
                 $vadd = $cls->getStaticPropertyValue('cli_opts') ;
                 $val = array_merge($val, is_array($vadd) ? $vadd : array()  );
             }
-            var_dump($classname, 'cli_opts');
+            var_dump($classname, 'cli_opts'); exot;
             if (method_exists($classname, 'cli_opts')) {
                 die("got a cli_opts method'");
             }
