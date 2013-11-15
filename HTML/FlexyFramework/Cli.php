dbe639f0b24ae96fe62c8995d6a41fc0d254878c
Alan Knowles <alan@roojs.com>
2013-11-15 16:20:17 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -214,7 +214,7 @@ Available commands:
             
             
         } catch (Exception $e) {
-            echo "Warning:  {$e->toString()}\n";
+            echo "Warning:  ". ((string) )$e) ."\n";
         }
         if (empty($val)) {
             return false;
