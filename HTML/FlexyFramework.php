d4791083fe48e13b4ecb8e0bdbe228df81dcde7d
Alan Knowles <alan@roojs.com>
2013-12-06 15:47:06 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -398,7 +398,7 @@ class HTML_FlexyFramework {
         $this->DB_DataObject[$dbini] = $iniCache;
         $this->_exposeToPear();
         
-        $_DB_DATAOBJECT['INI'][$this->_database] =   parse_ini_file($iniCache, true);
+        $GLOBALS['DB_DATAOBJECT']['INI'][$this->_database] =   parse_ini_file($iniCache, true);
         // clear any dataobject cache..
          
         
