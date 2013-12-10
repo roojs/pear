db33cdc0a275622da469bba074d24cebc5a64c49
Chris <chris@roojs.com>
2013-12-10 15:59:54 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1092,7 +1092,7 @@ class File_Convert_Solution
     }
     function pdftoppm($fn, $x, $y, $pg=false)
     {
-        
+        echo ('run?');exit;
         $xscale = 400; // min size?
         if (!empty($x) && $x> $xscale ) {
             $xscale = $x;
