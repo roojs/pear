7e1c3e1b5fd48d7cad4d5b3f6a4b91cd580277ca
Alan Knowles <alan@roojs.com>
2013-12-16 12:49:09 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -4707,7 +4707,11 @@ class DB_DataObject extends DB_DataObject_Overload
 
         if (is_array($this->_link_loaded)) {
             foreach ($this->_link_loaded as $do) {
-                if (!empty($this->{$do}) && is_object($this->{$do}) && method_exists($this->{$do}, 'free')) {
+                if (
+                        !empty($this->{$do}) &&
+                        is_object($this->{$do}) &&
+                        method_exists($this->{$do}, 'free')
+                    ) {
                     $this->{$do}->free();
                 }
             }
