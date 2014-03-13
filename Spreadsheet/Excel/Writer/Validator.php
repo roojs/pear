eac9f20af133eeba5fd0875f17df245bc1f2b815
Edward <edward@roojs.com>
2014-03-13 14:57:44 +0800

    Spreadsheet/Excel/Writer/Validator.php

--- Spreadsheet/Excel/Writer/Validator.php
+++ Spreadsheet/Excel/Writer/Validator.php
@@ -166,13 +166,15 @@ class Spreadsheet_Excel_Writer_Validator
         if (!$this->_incell) {
             $options |= 0x200;
         }
-        print_r($this->_type);exit;
+        
         if ($this->_showprompt) {
             $options |= 0x40000;
         }
+        
         if ($this->_showerror) {
             $options |= 0x80000;
         }
+        print_r($this->_type);exit;
       $options |= $this->_operator << 20;
       
       return $options;
