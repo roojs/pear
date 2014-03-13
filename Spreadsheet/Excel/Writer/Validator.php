bd90268a80ad7355a6384eb70d2c54fba2d96f97
Edward <edward@roojs.com>
2014-03-13 15:13:23 +0800

    Spreadsheet/Excel/Writer/Validator.php

--- Spreadsheet/Excel/Writer/Validator.php
+++ Spreadsheet/Excel/Writer/Validator.php
@@ -124,12 +124,13 @@ class Spreadsheet_Excel_Writer_Validator
     {
         // Parse the formula using the parser in Parser.php
         $error = $this->_parser->parse($formula);
-        print_r($error);exit;
+        
         if (PEAR::isError($error)) {
             return $this->_formula1;
         }
 
         $this->_formula1 = $this->_parser->toReversePolish();
+        print_r($this->_formula1);exit;
         if (PEAR::isError($this->_formula1)) {
             return $this->_formula1;
         }
