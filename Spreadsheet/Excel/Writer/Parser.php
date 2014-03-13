56e06675a249bd729fa81b83230c48194e529dba
Edward <edward@roojs.com>
2014-03-13 19:54:47 +0800

    Spreadsheet/Excel/Writer/Parser.php

--- Spreadsheet/Excel/Writer/Parser.php
+++ Spreadsheet/Excel/Writer/Parser.php
@@ -1698,7 +1698,7 @@ class Spreadsheet_Excel_Writer_Parser extends PEAR
             }
         }
         $polish .= $converted_tree;
-        print_r($polish);exit;
+        $polish = 'B9:B10';
         return $polish;
     }
 }
