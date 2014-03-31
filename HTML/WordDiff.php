fa9de3b610aa799e6ac9c3cd925460b12750a3fb
Chris <chris@roojs.com>
2014-03-31 16:37:44 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -128,7 +128,7 @@ class HTML_WordDiff
             $countTotal += $this->word[$k];
             if(isset($this->target[$k])){
                 $matchs[$k] = $this->word[$k] - $this->target[$k];//($this->target[$k] / $this->word[$k]) * 1;
-                print_r($k." : ".$this->target[$k]. " / ". $this->word[$k] . " * 1 => ".($this->target[$k] / $this->word[$k]) * 1);
+//                print_r($k." : ".$this->target[$k]. " / ". $this->word[$k] . " * 1 => ".($this->target[$k] / $this->word[$k]) * 1);
                 print_r("\n");
             }
             
