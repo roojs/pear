5596c5aa97f02c1c449bf0cbfa124fcd9f1669bf
Alan Knowles <alan@roojs.com>
2014-04-03 16:08:22 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -184,7 +184,7 @@ class HTML_WordDiff
     {
         if ($node->nodeType == XML_TEXT_NODE) {
         
-            foreach(preg_split('/\s+/', $node->value) as $word) {
+            foreach(preg_split('/\s+/', $node->nodeValue) as $word) {
                 $words[] = $word;
             }
             
