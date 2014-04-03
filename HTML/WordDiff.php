d49a899ce684ed9e8a995e015a6f9f63599a59a1
Alan Knowles <alan@roojs.com>
2014-04-03 16:12:26 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -72,6 +72,8 @@ class HTML_WordDiff
      */
     function __construct($config)
     {
+        print_r($config);
+        
         if(!is_array($config)){
             trigger_error("Word Diff got error, the argument IS NOT array");
             return;
