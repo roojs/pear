90932e34c0b1a621fd524ca58d8d8263bee336bd
Alan Knowles <alan@roojs.com>
2014-04-04 15:11:01 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1196,7 +1196,7 @@ class File_Convert_Solution
 //        print_r($yscale);
         
         $PDFTOPPM = System::which("pdftocairo");
-        $cmd = "$PDFTOPPM -jpeg -f $pg " 
+        $cmd = "$PDFTOPPM   -f $pg " 
                     . "-l $pg  " 
                     //. "-png "
                     . "-r 300 " // was 1200?
