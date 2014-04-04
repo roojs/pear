9cfef717e522b136fd4e6def52ff0dd226a85003
Chris <chris@roojs.com>
2014-04-04 16:34:16 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -1340,7 +1340,7 @@ class File_Convert_Solution
                     . escapeshellarg($fn.'-conv');
         
         // expect this file..
-//        echo "$cmd <br/>";exit;
+        echo "$cmd <br/>";exit;
         if ($this->debug) {
            echo "$cmd <br/>";
            
