bd197532c70f506a2079aa2a38a42b23c012468f
Chris <chris@roojs.com>
2014-05-05 19:55:06 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -191,7 +191,7 @@ class HTML_WordDiff
 //        }
 //        print_r(mb_detect_encoding($searchPage));
 //        print_r($searchPage);exit;
-        @$pageDom->loadHTML($this->htmlDom);
+        @$pageDom->loadHTML($searchPage);
 //        exit;
         $words = $this->domExtractWords($pageDom->documentElement, array());
 //        print_r($words);exit;
