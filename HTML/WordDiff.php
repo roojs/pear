680e3e5f0f64bc7d45bfc4182f7e74238088d3c8
Chris <chris@roojs.com>
2014-05-08 17:40:13 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -151,7 +151,7 @@ class HTML_WordDiff
             $this->wordMax = 10*count($a);
         }
         if($this->debug_on){
-            print_r($a);
+//            print_r($a);
             //exit;
         }
         $ret = array();
