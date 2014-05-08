76ae7ed10456afe36f4c77bc9ca016befb16c3b0
Alan Knowles <alan@roojs.com>
2014-05-08 11:14:00 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -202,7 +202,7 @@ class HTML_WordDiff
         
 //        $string = preg_replace('/[^\pL\pS\pN]/u', '-', $pageDom->documentElement->getElementsByTagName('body')->item(0)->textContent);
         if($this->debug_on){
-            print_r($words);
+           // print_r($words);
             exit;
         }
         return $words;
