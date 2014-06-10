7b637ae4f1c9d442201f1ffe35ddcb0d11b10c87
Edward <edward@roojs.com>
2014-06-10 14:42:06 +0800

    HTML/Template/Flexy.php

--- HTML/Template/Flexy.php
+++ HTML/Template/Flexy.php
@@ -290,7 +290,7 @@ class HTML_Template_Flexy
             if (@!file_exists($tmplDir . DIRECTORY_SEPARATOR .$file)) {
                 continue;
             }
-            
+            print_r($file);exit;
             if (!$this->options['multiSource'] && ($ret !== false)) {
                   return $this->raiseError("You have more than one template Named {$file} in your paths, found in both".
                         "<BR>{$this->currentTemplate }<BR>{$tmplDir}" . DIRECTORY_SEPARATOR . $file,  
