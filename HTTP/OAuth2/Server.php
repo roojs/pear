f633c86919c86e84c960156925f6f34ca6943fb7
Edward <edward@roojs.com>
2014-06-18 19:53:52 +0800

    HTTP/OAuth2/Server.php

--- HTTP/OAuth2/Server.php
+++ HTTP/OAuth2/Server.php
@@ -710,14 +710,14 @@ class Server implements HTTP_OAuth2_Controller_ResourceControllerInterface,
     protected function createDefaultIdTokenResponseType()
     {
         if (!isset($this->storages['user_claims'])) {
-            throw new \LogicException("You must supply a storage object implementing OAuth2\OpenID\Storage\UserClaimsInterface to use openid connect");
+            throw new LogicException("You must supply a storage object implementing OAuth2\OpenID\Storage\UserClaimsInterface to use openid connect");
         }
         if (!isset($this->storages['public_key'])) {
-            throw new \LogicException("You must supply a storage object implementing OAuth2\Storage\PublicKeyInterface to use openid connect");
+            throw new LogicException("You must supply a storage object implementing OAuth2\Storage\PublicKeyInterface to use openid connect");
         }
 
         $config = array_intersect_key($this->config, array_flip(explode(' ', 'issuer id_lifetime')));
-        return new IdToken($this->storages['user_claims'], $this->storages['public_key'], $config);
+        return new HTTP_OAuth2_OpenID_ResponseType_IdToken($this->storages['user_claims'], $this->storages['public_key'], $config);
     }
 
     protected function createDefaultTokenIdTokenResponseType()
