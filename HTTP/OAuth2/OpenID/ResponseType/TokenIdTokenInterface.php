84a1f2969f9d226f6ff37d5a00a4bec9a67b39dd
Edward <edward@roojs.com>
2014-06-18 17:25:10 +0800

    HTTP/OAuth2/ClientAssertionType/ClientAssertionTypeInterface.php HTTP/OAuth2/ClientAssertionType/HttpBasic.php HTTP/OAuth2/ClientAssertionType HTTP/OAuth2/Controller HTTP/OAuth2/Encryption HTTP/OAuth2/GrantType HTTP/OAuth2/OpenID HTTP/OAuth2/ResponseType HTTP/OAuth2/Storage HTTP/OAuth2/TokenType HTTP/OAuth2/Autoloader.php HTTP/OAuth2/Request.php HTTP/OAuth2/RequestInterface.php HTTP/OAuth2/Response.php HTTP/OAuth2/ResponseInterface.php HTTP/OAuth2/Scope.php HTTP/OAuth2/ScopeInterface.php HTTP/OAuth2/Server.php HTTP/OAuth2 HTTP/OAuth2/Controller/AuthorizeController.php HTTP/OAuth2/Controller/AuthorizeControllerInterface.php HTTP/OAuth2/Controller/ResourceController.php HTTP/OAuth2/Controller/ResourceControllerInterface.php HTTP/OAuth2/Controller/TokenController.php HTTP/OAuth2/Controller/TokenControllerInterface.php HTTP/OAuth2/Encryption/EncryptionInterface.php HTTP/OAuth2/Encryption/Jwt.php HTTP/OAuth2/GrantType/AuthorizationCode.php HTTP/OAuth2/GrantType/ClientCredentials.php HTTP/OAuth2/GrantType/GrantTypeInterface.php HTTP/OAuth2/GrantType/JwtBearer.php HTTP/OAuth2/GrantType/RefreshToken.php HTTP/OAuth2/GrantType/UserCredentials.php HTTP/OAuth2/OpenID/Controller/AuthorizeController.php HTTP/OAuth2/OpenID/Controller/AuthorizeControllerInterface.php HTTP/OAuth2/OpenID/Controller/UserInfoController.php HTTP/OAuth2/OpenID/Controller/UserInfoControllerInterface.php HTTP/OAuth2/OpenID/Controller HTTP/OAuth2/OpenID/GrantType HTTP/OAuth2/OpenID/ResponseType HTTP/OAuth2/OpenID/Storage HTTP/OAuth2/OpenID/GrantType/AuthorizationCode.php HTTP/OAuth2/OpenID/ResponseType/AuthorizationCode.php HTTP/OAuth2/OpenID/ResponseType/AuthorizationCodeInterface.php HTTP/OAuth2/OpenID/ResponseType/IdToken.php HTTP/OAuth2/OpenID/ResponseType/IdTokenInterface.php HTTP/OAuth2/OpenID/ResponseType/TokenIdToken.php HTTP/OAuth2/OpenID/ResponseType/TokenIdTokenInterface.php HTTP/OAuth2/OpenID/Storage/AuthorizationCodeInterface.php HTTP/OAuth2/OpenID/Storage/UserClaimsInterface.php HTTP/OAuth2/ResponseType/AccessToken.php HTTP/OAuth2/ResponseType/AccessTokenInterface.php HTTP/OAuth2/ResponseType/AuthorizationCode.php HTTP/OAuth2/ResponseType/AuthorizationCodeInterface.php HTTP/OAuth2/ResponseType/CryptoToken.php HTTP/OAuth2/ResponseType/ResponseTypeInterface.php HTTP/OAuth2/Storage/AccessTokenInterface.php HTTP/OAuth2/Storage/AuthorizationCodeInterface.php HTTP/OAuth2/Storage/Cassandra.php HTTP/OAuth2/Storage/ClientCredentialsInterface.php HTTP/OAuth2/Storage/ClientInterface.php HTTP/OAuth2/Storage/CryptoToken.php HTTP/OAuth2/Storage/CryptoTokenInterface.php HTTP/OAuth2/Storage/JwtBearerInterface.php HTTP/OAuth2/Storage/Memory.php HTTP/OAuth2/Storage/Mongo.php HTTP/OAuth2/Storage/Pdo.php HTTP/OAuth2/Storage/PublicKeyInterface.php HTTP/OAuth2/Storage/Redis.php HTTP/OAuth2/Storage/RefreshTokenInterface.php HTTP/OAuth2/Storage/ScopeInterface.php HTTP/OAuth2/Storage/UserCredentialsInterface.php HTTP/OAuth2/TokenType/Bearer.php HTTP/OAuth2/TokenType/Mac.php HTTP/OAuth2/TokenType/TokenTypeInterface.php

--- HTTP/OAuth2/OpenID/ResponseType/TokenIdTokenInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OAuth2\OpenID\ResponseType;
-
-require_once 'HTTP/OAuth2/ResponseType/HTTP_OAuth2_ResponseTypeInterface';
-
-interface TokenIdTokenInterface extends ResponseTypeInterface
-{
-}
