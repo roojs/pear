061e8e374cc05dff12bc057656a2e00aa69d9fc2
Edward <edward@roojs.com>
2014-06-19 16:39:10 +0800

    HTTP/OAuth2/GrantType/UserCredentials.php

--- HTTP/OAuth2/GrantType/UserCredentials.php
+++ HTTP/OAuth2/GrantType/UserCredentials.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'HTTP/OAuth2/Storage/UserCredentialsInterface';
-require_once 'HTTP/OAuth2/ResponseType/AccessTokenInterface';
-require_once 'HTTP/OAuth2/RequestInterface';
-require_once 'HTTP/OAuth2/ResponseInterface';
+require_once 'HTTP/OAuth2/Storage/UserCredentialsInterface.php';
+require_once 'HTTP/OAuth2/ResponseType/AccessTokenInterface.php';
+require_once 'HTTP/OAuth2/RequestInterface.php';
+require_once 'HTTP/OAuth2/ResponseInterface.php';
 
 /**
  *
