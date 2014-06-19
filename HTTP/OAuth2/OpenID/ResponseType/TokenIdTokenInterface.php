a7eaa5ba61235faf2a90f3a6dc7c0f260d76563e
Edward <edward@roojs.com>
2014-06-19 14:03:59 +0800

    HTTP/OAuth2/OpenID/ResponseType/TokenIdTokenInterface.php

--- HTTP/OAuth2/OpenID/ResponseType/TokenIdTokenInterface.php
+++ HTTP/OAuth2/OpenID/ResponseType/TokenIdTokenInterface.php
@@ -2,6 +2,6 @@
 
 require_once 'HTTP/OAuth2/ResponseType/ResponseTypeInterface';
 
-interface HTTP_OAuth2_ResponseType_TokenIdTokenInterface extends HTTP_OAuth2_ResponseType_ResponseTypeInterface
+interface HTTP_OAuth2_OpenID_ResponseType_TokenIdTokenInterface extends HTTP_OAuth2_ResponseType_ResponseTypeInterface
 {
 }
