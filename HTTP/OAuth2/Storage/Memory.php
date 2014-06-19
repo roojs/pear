5abd914d53f1a63894518d3298a0560197a2592a
Edward <edward@roojs.com>
2014-06-19 16:46:19 +0800

    HTTP/OAuth2/Storage/Memory.php

--- HTTP/OAuth2/Storage/Memory.php
+++ HTTP/OAuth2/Storage/Memory.php
@@ -3,6 +3,7 @@
 require_once 'HTTP/OAuth2/OpenID/Storage/UserClaimsInterface.php';
 require_once 'HTTP/OAuth2/OpenID/Storage/AuthorizationCodeInterface.php';
 require_once 'HTTP/OAuth2/Storage/UserCredentialsInterface.php';
+require_once 'HTTP/OAuth2/Storage/ClientCredentialsInterface.php';
 
 /**
  * Simple in-memory storage for all storage types
