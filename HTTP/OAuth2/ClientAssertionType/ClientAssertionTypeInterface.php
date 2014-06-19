b05c42fc828e4ccf1cb15df71d592666b56a69eb
Edward <edward@roojs.com>
2014-06-19 12:51:06 +0800

    HTTP/OAuth2/ClientAssertionType/ClientAssertionTypeInterface.php

--- HTTP/OAuth2/ClientAssertionType/ClientAssertionTypeInterface.php
+++ HTTP/OAuth2/ClientAssertionType/ClientAssertionTypeInterface.php
@@ -6,8 +6,8 @@ require_once 'HTTP/OAuth2/ResponseInterface';
 /**
  * Interface for all OAuth2 Client Assertion Types
  */
-interface ClientAssertionTypeInterface
+interface HTTP_OAuth2_ClientAssertionType_ClientAssertionTypeInterface
 {
-    public function validateRequest(RequestInterface $request, ResponseInterface $response);
+    public function validateRequest(HTTP_OAuth2_RequestInterface $request, HTTP_OAuth2_ResponseInterface $response);
     public function getClientId();
 }
