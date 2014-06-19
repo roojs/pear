f9f0df32e920c63d585c13701463d82d250be099
Edward <edward@roojs.com>
2014-06-19 13:38:44 +0800

    HTTP/OAuth2/OpenID/Controller/AuthorizeControllerInterface.php

--- HTTP/OAuth2/OpenID/Controller/AuthorizeControllerInterface.php
+++ HTTP/OAuth2/OpenID/Controller/AuthorizeControllerInterface.php
@@ -3,7 +3,7 @@
 require_once 'HTTP/OAuth2/RequestInterface';
 require_once 'HTTP/OAuth2/ResponseInterface';
 
-interface AuthorizeControllerInterface
+interface HTTP_OAuth2_OpenID_Controller_AuthorizeControllerInterface
 {
     const RESPONSE_TYPE_ID_TOKEN = 'id_token';
     const RESPONSE_TYPE_TOKEN_ID_TOKEN = 'token id_token';
