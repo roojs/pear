cebd894884de595892c5d74033a5907c1bcdb36f
Edward <edward@roojs.com>
2014-06-19 16:40:51 +0800

    HTTP/OAuth2/ResponseType/CryptoToken.php

--- HTTP/OAuth2/ResponseType/CryptoToken.php
+++ HTTP/OAuth2/ResponseType/CryptoToken.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once 'HTTP/OAuth2/Encryption/EncryptionInterface';
-require_once 'HTTP/OAuth2/Encryption/Jwt';
-require_once 'HTTP/OAuth2/Storage/AccessTokenInterface';
-require_once 'HTTP/OAuth2/Storage/RefreshTokenInterface';
-require_once 'HTTP/OAuth2/Storage/PublicKeyInterface';
-require_once 'HTTP/OAuth2/Storage/Memory';
+require_once 'HTTP/OAuth2/Encryption/EncryptionInterface.php';
+require_once 'HTTP/OAuth2/Encryption/Jwt.php';
+require_once 'HTTP/OAuth2/Storage/AccessTokenInterface.php';
+require_once 'HTTP/OAuth2/Storage/RefreshTokenInterface.php';
+require_once 'HTTP/OAuth2/Storage/PublicKeyInterface.php';
+require_once 'HTTP/OAuth2/Storage/Memory.php';
 
 /**
  *
