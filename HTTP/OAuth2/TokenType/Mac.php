cc3707be940dc522657f33d7095dc2ee0d93bdee
Edward <edward@roojs.com>
2014-06-19 14:32:12 +0800

    HTTP/OAuth2/TokenType/Mac.php

--- HTTP/OAuth2/TokenType/Mac.php
+++ HTTP/OAuth2/TokenType/Mac.php
@@ -6,15 +6,15 @@ require_once 'HTTP/OAuth2/ResponseInterface';
 /**
 * This is not yet supported!
 */
-class Mac implements TokenTypeInterface
+class HTTP_OAuth2_TokenType_Mac implements HTTP_OAuth2_TokenType_TokenTypeInterface
 {
     public function getTokenType()
     {
         return 'mac';
     }
 
-    public function getAccessTokenParameter(RequestInterface $request, ResponseInterface $response)
+    public function getAccessTokenParameter(HTTP_OAuth2_RequestInterface $request, HTTP_OAuth2_ResponseInterface $response)
     {
-        throw new \LogicException("Not supported");
+        throw new LogicException("Not supported");
     }
 }
