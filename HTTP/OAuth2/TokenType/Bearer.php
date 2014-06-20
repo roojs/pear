057e72f9b824c4151d19fd33039c0a305bc1ee4c
Edward <edward@roojs.com>
2014-06-20 19:48:43 +0800

    HTTP/OAuth2/TokenType/Bearer.php

--- HTTP/OAuth2/TokenType/Bearer.php
+++ HTTP/OAuth2/TokenType/Bearer.php
@@ -74,7 +74,7 @@ class HTTP_OAuth2_TokenType_Bearer implements HTTP_OAuth2_TokenType_TokenTypeInt
 
             return null;
         }
-        print_r($this);exit;
+        print_r($request);exit;
         /**
          * If no authentication is provided, set the status code
          * to 401 and return no other error information
