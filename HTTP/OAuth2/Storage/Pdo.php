29e0fac1f35928dd80f229a895553a3dcb435fc5
Edward <edward@roojs.com>
2014-07-03 17:10:56 +0800

    HTTP/OAuth2/Storage/Pdo.php

--- HTTP/OAuth2/Storage/Pdo.php
+++ HTTP/OAuth2/Storage/Pdo.php
@@ -326,7 +326,7 @@ class HTTP_OAuth2_Storage_Pdo implements HTTP_OAuth2_Storage_AuthorizationCodeIn
 
         // if it exists, update it.
         if ($this->getUser($username)) {
-            $stmt = $this->db->prepare($sql = sprintf('UPDATE %s SET password=:password, first_name=:firstName, last_name=:lastName where username=:username', $this->config['user_table']));
+            $stmt = $this->db->prepare($sql = sprintf('UPDATE %s SET password=:password, first_name=:firstName, last_name=:lastName where %s=:username', $this->config['user_table'], $this->config['user_table.username']));
         } else {
             $stmt = $this->db->prepare(sprintf('INSERT INTO %s (username, password, first_name, last_name) VALUES (:username, :password, :firstName, :lastName)', $this->config['user_table']));
         }
