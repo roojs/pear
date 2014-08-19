c21128d8324a1aa78dea3232f1010058ca870ab3
Alan Knowles <alan@roojs.com>
2014-08-19 16:28:26 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -222,7 +222,7 @@ class HTML_WordDiff
 //        print_r(mb_detect_encoding($this->htmlDom));
         
         // may produce errors - so we hide them...
-        $searchPage = @mb_convert_encoding($this->htmlDom, $charset ,  $charset=="UTF-8" ? "auto" :$charset);
+        $searchPage = @mb_convert_encoding($this->htmlDom, "UTF-8" ,  $charset=="UTF-8" ? "auto" :$charset);
         
 //        $searchPage = mb_convert_encoding($this->htmlDom, "UTF-8",  "HTML-ENTITIES");
 //        echo $searchPage;
