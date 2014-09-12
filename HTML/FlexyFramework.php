a1a57f8020ae15b5aa6fb4dc8e2ab600b3a267ad
Alan Knowles <alan@roojs.com>
2014-09-12 15:04:12 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -1106,7 +1106,7 @@ class HTML_FlexyFramework {
       
     static function ensureSingle($sig, $class) 
     {
-        echo "check single: $sig / $cls \n";
+        echo "check single: $sig / $class \n";
         $ff = HTML_FlexyFramework::get();
         if (function_exists('posix_getpwuid')) {
             $uinfo = posix_getpwuid( posix_getuid () ); 
