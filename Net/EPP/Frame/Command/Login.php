09ce3951273d40fad44bebea7adaa148af2fb31e
Edward <edward@roojs.com>
2014-11-12 15:27:20 +0800

    Net/EPP/Frame/Command/Login.php

--- Net/EPP/Frame/Command/Login.php
+++ Net/EPP/Frame/Command/Login.php
@@ -10,25 +10,25 @@
             
 			parent::__construct('login');
 
-//			$this->clID = $this->createElement('clID');
-//			$this->command->appendChild($this->clID);
-//
-//			$this->pw = $this->createElement('pw');
-//			$this->command->appendChild($this->pw);
-//
-//			$this->options = $this->createElement('options');
-//			$this->command->appendChild($this->options);
-//
-//			$this->eppVersion = $this->createElement('version');
-//			$this->options->appendChild($this->eppVersion);
-//
-//			$this->eppLang = $this->createElement('lang');
-//			$this->options->appendChild($this->eppLang);
-//
-//			$this->svcs = $this->createElement('svcs');
-//			$this->command->appendChild($this->svcs);
-//                
-//            $this->addCommandParams($params);
+			$this->clID = $this->createElement('clID');
+			$this->command->appendChild($this->clID);
+
+			$this->pw = $this->createElement('pw');
+			$this->command->appendChild($this->pw);
+
+			$this->options = $this->createElement('options');
+			$this->command->appendChild($this->options);
+
+			$this->eppVersion = $this->createElement('version');
+			$this->options->appendChild($this->eppVersion);
+
+			$this->eppLang = $this->createElement('lang');
+			$this->options->appendChild($this->eppLang);
+
+			$this->svcs = $this->createElement('svcs');
+			$this->command->appendChild($this->svcs);
+                
+            $this->addCommandParams($params);
                 
                 
 		}
