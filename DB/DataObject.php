feeb44a7187ac8afb4db73515074b6cf4651cdee
Edward <edward@roojs.com>
2014-12-18 11:48:46 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -2160,7 +2160,6 @@ class DB_DataObject extends DB_DataObject_Overload
         if (count($args)) {
             $this->_database_keys = $args;
         }
-        print_R($this->_database);exit;
         if (isset($this->_database_keys)) {
             return $this->_database_keys;
         }
