a9703d42d39c4eebfc2f1cc53cf3fef9484c8f7a
Michael <michael@roojs.com>
2015-02-06 13:45:28 +0800

    HTML/FlexyFramework/Page.php

--- HTML/FlexyFramework/Page.php
+++ HTML/FlexyFramework/Page.php
@@ -284,11 +284,10 @@ class HTML_FlexyFramework_Page  {
 
         $template_engine = new HTML_Template_Flexy();
         $template_engine->compile($this->template);
-
-        if ($this->elements) { /* BC crap! */
-            $this->elements = HTML_Template_Flexy_Factory::setErrors($this->elements,$this->errors);
-        }
-        echo "<pre>";print_r($this);exit;
+//        if ($this->elements) { /* BC crap! */
+ //           $this->elements = HTML_Template_Flexy_Factory::setErrors($this->elements,$this->errors);
+  //      }
+                
         $template_engine->elements = $this->elements;
         $template_engine->outputObject($this,$this->elements);
         
