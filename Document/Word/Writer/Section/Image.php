442ab8613a32269c14f56b22c2c783931924bbb0
Edward <edward@roojs.com>
2015-03-18 17:47:26 +0800

    Document/Word/Writer/Section/Image.php

--- Document/Word/Writer/Section/Image.php
+++ Document/Word/Writer/Section/Image.php
@@ -76,11 +76,6 @@ class Document_Word_Writer_Section_Image
 		$_supportedImageTypes = array('jpg', 'jpeg', 'gif', 'png', 'bmp', 'tif', 'tiff');
 		
 		$inf = pathinfo($src);
-                
-                if(empty($inf['extension'])){
-                    return false;
-                }
-                
 		$ext = strtolower($inf['extension']);
 		if(file_exists($src) && in_array($ext, $_supportedImageTypes)) {
                         require_once __DIR__ . '/../Style/Image.php';
