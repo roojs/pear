ca96e657e44b728f793b9508f8279000660e1e57
Edward <edward@roojs.com>
2015-03-19 11:00:55 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -44,6 +44,7 @@ class File_Convert_AbiToDocx
                 $this->section = $this->writer->createSection();
                 $this->pass = 1;
                 $this->parseAbi();
+                print_R($this->style);exit;
                 $this->pass = 2;
                 $this->parseAbi();
                 $this->saveDocx( $fn ); // uses this->writer...
