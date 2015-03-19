3a13b3364a537de61e837cc067fea391da197ce6
Edward <edward@roojs.com>
2015-03-19 11:29:01 +0800

    Document/Word/Writer/Section/Table.php

--- Document/Word/Writer/Section/Table.php
+++ Document/Word/Writer/Section/Table.php
@@ -136,11 +136,9 @@ class Document_Word_Writer_Section_Table
             $i = count($this->_rows) - 1;
             $this->_rows[$i][] = $cell;
             $col = count($this->_rows[$i] ) -1;
-            
+
             if (!empty($width)) {
-                print_R('run');exit;
                 $this->_colWidths[$col] = max($width, empty($this->_colWidths[$col] ) ? 0 : $this->_colWidths[$col] ); 
-                
             }
             return $cell;
 	}
