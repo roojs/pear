a73f9ba6c3ab872e11a03b8c0a5984bf7af3ae9a
Edward <edward@roojs.com>
2015-03-19 10:56:13 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -69,7 +69,7 @@ class File_Convert_AbiToDocx
           
             while ($this->xr->read()){
                 //$this->dumpsections($sections);
-                 // echo $this->xr->name . '::' . count($sections). "<br/>"; 
+                  echo $this->xr->name . '::' . count($sections). "<br/>"; 
                  $method = 'handle_'.$this->xr->name;
                  
                  
@@ -82,7 +82,7 @@ class File_Convert_AbiToDocx
                         $this->style = array_pop($state);
                         $this->section = array_pop($sections);
                         array_pop($stack);
-                       // echo "AFTER POP:"; $this->dumpsections($sections); 
+                        echo "AFTER POP:"; $this->dumpsections($sections); 
                      }
                      continue;
                 }
@@ -122,7 +122,7 @@ class File_Convert_AbiToDocx
                    $sections[] = $this->section;
                    $state[] = $this->style;
                 }
-                
+                echo "HANLED {$method} <br/>";
                 $this->$method();  
                
             }
