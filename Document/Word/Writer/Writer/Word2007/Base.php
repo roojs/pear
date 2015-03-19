699432915e7fcce74cf4833055ccf86d8b95b3bf
Edward <edward@roojs.com>
2015-03-19 13:48:47 +0800

    Document/Word/Writer/Writer/Word2007/Base.php

--- Document/Word/Writer/Writer/Word2007/Base.php
+++ Document/Word/Writer/Writer/Word2007/Base.php
@@ -512,13 +512,13 @@ class Document_Word_Writer_Writer_Word2007_Base extends Document_Word_Writer_Wri
                             $tblStyle = $table->getStyle();
                             
                             for ($i = $cellStyle->_columnNum; $i < $cellStyle->_mergeto; $i++){
-                                $key = '_width' . $i . '_dax';
-                                
-                                if(isset($tblStyle->{$key})){
+//                                $key = '_width' . $i . '_dax';
+//                                
+//                                if(isset($tblStyle->{$key})){
 //                                    $calcWidth = $calcWidth + $tblStyle->{$key} * 1;
-                                    continue;
-                                }
-                                $autoWidth = true;
+//                                    continue;
+//                                }
+//                                $autoWidth = true;
                             }
                             
                         }
