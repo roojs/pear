d63787813e2b22eb92304ca819bc4e6e2c09b99d
Edward <edward@roojs.com>
2015-03-19 12:41:21 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -391,6 +391,12 @@ class File_Convert_AbiToDocx
                             $attrArray['borderRightColor'] = trim($prop);
                         }
                         break;
+                    case 'right-attach':
+                        $props = explode('/', $attr[1]);
+                        foreach($props as $prop){
+                            $attrArray['merge'] = trim($prop);
+                        }
+                        break;
                     
                     case 'bot-color':
                         $props = explode('/', $attr[1]);
