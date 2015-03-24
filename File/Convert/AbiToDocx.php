b86757bcc097246f32f24e88651b725fafc81034
Alan Knowles <alan@roojs.com>
2015-03-24 10:22:28 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -66,8 +66,10 @@ class File_Convert_AbiToDocx
             $state = array();
             $sections = array();
             $stack = array();
-          
-            while ($this->xr->read()){
+            
+            // this may produce warnings...
+            
+            while (@$this->xr->read()){
                 //$this->dumpsections($sections);
                  // echo $this->xr->name . '::' . count($sections). "<br/>"; 
                  $method = 'handle_'.$this->xr->name;
