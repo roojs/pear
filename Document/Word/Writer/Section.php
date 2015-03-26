12c83763d7d63c6b0388c1f868985f440c926899
Edward <edward@roojs.com>
2015-03-26 16:02:09 +0800

    Document/Word/Writer/Section.php

--- Document/Word/Writer/Section.php
+++ Document/Word/Writer/Section.php
@@ -116,7 +116,8 @@ class Document_Word_Writer_Section
         {
                 require_once __DIR__ . '/Section/Text.php';
 		//$givenText = utf8_encode($text);
-        $text = @iconv("UTF-8", "UTF-8//IGNORE", $text);
+                print_R($text);exit;
+                $text = @iconv("UTF-8", "UTF-8//IGNORE", $text);
 		$text = new Document_Word_Writer_Section_Text($text, $styleFont, $styleParagraph);
 		$this->_elementCollection[] = $text;
 		return $text;
