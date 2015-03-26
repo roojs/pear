be78760babc615ba70263306ed5d9e8e52c0c265
Edward <edward@roojs.com>
2015-03-26 17:14:10 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -72,9 +72,15 @@ class File_Convert_AbiToDocx
             while ($this->xr->read()){
                 //$this->dumpsections($sections);
                  // echo $this->xr->name . '::' . count($sections). "<br/>"; 
+                
                  $method = 'handle_'.$this->xr->name;
                  
-                 
+//                 if($this->xr->nodeType == XMLReader::CDATA){
+//                     $text = "<![CDATA[{$this->xr->value}]]>";
+//                     $this->section->addText($text, $this->style);
+//                     continue;
+//                 }
+//                 
                  if ($this->xr->nodeType == XMLReader::END_ELEMENT) {
                      if($this->xr->name == 'section'){
                          $this->keepSection = false;
