74a0624302a4d1f39fdbc4044393b6d45f1a7a2d
Edward <edward@roojs.com>
2015-03-26 16:03:00 +0800

    Document/Word/Writer/Section.php

--- Document/Word/Writer/Section.php
+++ Document/Word/Writer/Section.php
@@ -114,6 +114,7 @@ class Document_Word_Writer_Section
 	 */
 	public function addText($text, $styleFont = null, $styleParagraph = null) 
         {
+            print_R('run');exit;
                 require_once __DIR__ . '/Section/Text.php';
 		//$givenText = utf8_encode($text);
                 print_R($text);exit;
