b7d73dbb111d369937065d651f529b740811470c
Edward <edward@roojs.com>
2015-05-04 11:23:12 +0800

    Mail/RFC822.php

--- Mail/RFC822.php
+++ Mail/RFC822.php
@@ -660,7 +660,7 @@ class Mail_RFC822 {
         }
 
         $mailbox = trim($mailbox);
-
+        print_r(substr($mailbox, -1));exit;
         // Check for name + route-addr
         if (substr($mailbox, -1) == '>' && substr($mailbox, 0, 1) != '<') {
             $parts  = explode('<', $mailbox);
