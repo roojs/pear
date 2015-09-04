4adda3244d63cc4d9e7086e7050a5aded0b252cf
edward <edward@roojs.com>
2015-09-04 14:02:52 +0800

    HTML/Template/Flexy.php

--- HTML/Template/Flexy.php
+++ HTML/Template/Flexy.php
@@ -251,7 +251,6 @@ class HTML_Template_Flexy
         if (empty($baseoptions) && class_exists('PEAR')) {
             $baseoptions = &PEAR::getStaticProperty('HTML_Template_Flexy','options');
         }
-        print_R($baseoptions);exit;
         if ($baseoptions ) {
             foreach( $baseoptions as  $key=>$aOption)  {
                 $this->options[$key] = $aOption;
