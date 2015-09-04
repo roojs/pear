da158b764d696cc326d7010af73fe0f2a02d10a3
edward <edward@roojs.com>
2015-09-04 14:18:54 +0800

    PEAR.php

--- PEAR.php
+++ PEAR.php
@@ -204,6 +204,7 @@ class PEAR
     */
     static function &getStaticProperty($class, $var)
     {
+        return $properties;
         static $properties;
         if (!isset($properties[$class])) {
             $properties[$class] = array();
