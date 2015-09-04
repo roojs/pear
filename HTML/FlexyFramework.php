a06c7d4947c03abdd31fcdafa18a2a52d856a10e
edward <edward@roojs.com>
2015-09-04 14:33:07 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -645,6 +645,8 @@ class HTML_FlexyFramework {
     }
     function _exposeToPear()
     {
+        print_R(__CLASS__);
+        print_r(get_class_vars(__CLASS__));exit;
         $cls = array_keys(get_class_vars(__CLASS__));
         $base = array();
         
