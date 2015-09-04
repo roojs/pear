77692cf913f538bfaa6efc995d71ba777959e6d8
edward <edward@roojs.com>
2015-09-04 13:50:18 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -1071,7 +1071,7 @@ class HTML_FlexyFramework {
                 $this->debug("FOUND FILE - SET CLASS = $classname <BR>");
                 break;
             } 
-            print_r($this->classPrefix);exit;
+            print_r($this->classPrefix . implode('_',$request_array));exit;
             // in here check the 'projectExtends' versions..?
             
             if(!empty($this->projectExtends)){
