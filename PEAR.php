a5e9ebf549814fef9f8d16729b83b4cfe3c5de8a
edward <edward@roojs.com>
2015-09-04 14:05:14 +0800

    PEAR.php

--- PEAR.php
+++ PEAR.php
@@ -159,6 +159,7 @@ class PEAR
 
         while ($classname && strcasecmp($classname, "pear")) {
             $destructor = "_$classname";
+            print_R($destructor);exit;
             if (method_exists($this, $destructor)) {
                 global $_PEAR_destructor_object_list;
                 $_PEAR_destructor_object_list[] = &$this;
