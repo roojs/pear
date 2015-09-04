40e14abe137bf977ef1bf02a402f59bf825944d0
edward <edward@roojs.com>
2015-09-04 13:45:57 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -1068,6 +1068,7 @@ class HTML_FlexyFramework {
             if (!empty($location) && @file_exists($floc )) {             // hide? error???
                 require_once $floc ;
                 $classname = $this->classPrefix . implode('_',$request_array);
+                print_R($classname);exit;
                 $this->debug("FOUND FILE - SET CLASS = $classname <BR>");
                 break;
             } 
