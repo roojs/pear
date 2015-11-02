d8df372a64e0e1e63d1c25957f56e2c80c011cd4
Alan Knowles <alan@roojs.com>
2015-11-02 16:15:53 +0800

    HTML/Template/Flexy/Compiler/Regex/SimpleTags.php

--- HTML/Template/Flexy/Compiler/Regex/SimpleTags.php
+++ HTML/Template/Flexy/Compiler/Regex/SimpleTags.php
@@ -101,6 +101,11 @@ class HTML_Template_Flexy_Compiler_Regex_SimpleTags
 
         $input = preg_replace_callback(
             "/".$this->start."([a-z0-9_.]+):h".$this->stop."/i",
+            function($m) {
+                return '<?php echo ' . $this->error . '$' . str_replace('.','->',$m[0])  . ' ?>';
+            },
+            
+            
             "'<?php echo ".$this->error."$'.str_replace('.','->','\\1').'?>'",
             $input);
 
