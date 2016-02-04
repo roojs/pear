4314d1b2f2e07f157595044211aef17f783781ae
Alan Knowles <alan@roojs.com>
2016-02-04 16:19:06 +0800

    Document/Word/Writer/Writer/Word2007/DocumentRels.php

--- Document/Word/Writer/Writer/Word2007/DocumentRels.php
+++ Document/Word/Writer/Writer/Word2007/DocumentRels.php
@@ -160,7 +160,7 @@ class Document_Word_Writer_Writer_Word2007_DocumentRels extends Document_Word_Wr
 	
 	private function _writeRelationship(Document_Word_Writer_Shared_XMLWriter $objWriter = null, $pId = 1, $pType = '', $pTarget = '', $pTargetMode = '') 
         {
-		if($pType != '' && $pTarget != '') {
+		if(strlen($pType)  && strlen($pTarget)) {
 			if(strpos($pId, 'rId') === false) {
 				$pId = 'rId' . $pId;
 			}
