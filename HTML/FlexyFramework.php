b9d2e872dd6a9bb362e0e172dc88e102cb7804fe
Alan Knowles <alan@roojs.com>
2016-02-17 15:55:24 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -82,7 +82,7 @@ class HTML_FlexyFramework {
     var $database; // set to true even if nodatabase=true
     
     // optional
-    var $debug = false;
+    var $debug = true;
     var $enable = false; // modules
     var $disable = false; // modules or permissions
     var $appName = false;
