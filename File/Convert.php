f2684d940e5154513565c20d3fbf5d04a7e76976
edward <edward@roojs.com>
2016-02-18 14:01:06 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -494,9 +494,7 @@ class File_Convert
             if (!in_array($from, $t[1])) {
                 continue;
             }
-            
             if (in_array($to,$t[2])) {
-                print_R($t);exit;
                 $ret =  new File_Convert_Solution($t[0], $from, $to);  // found a solid match - returns the method.
                 $ret->convert = $this;
                 return $ret;
