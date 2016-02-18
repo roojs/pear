eb1672c8801fe5058aa91e686abe43dad537e83a
edward <edward@roojs.com>
2016-02-18 14:03:43 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -521,6 +521,7 @@ class File_Convert
                 
                 $ns = $stack;
                 $ns[] = $targ;
+                print_R($to);exit;
                 $try = $this->getConvMethods($targ, $to, $ns);
                 // try will be an array of method, from, to (or false)
                 
