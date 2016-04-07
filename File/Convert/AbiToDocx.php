38ae46208f77b42cd6d3e5da3c753bc752c5d02f
Alan Knowles <alan@roojs.com>
2016-04-07 16:15:17 +0800

    File/Convert/AbiToDocx.php

--- File/Convert/AbiToDocx.php
+++ File/Convert/AbiToDocx.php
@@ -436,7 +436,7 @@ class File_Convert_AbiToDocx
                         $key = trim($attr[0]);
                         if (empty($attr[1])) {
                             // bit annoying.. but we currently only convert from trustes sources, so do not need to handle this. 
-                            throw new PEAR_Exception("value missing in style key={$attr[0]}  for prop={$attribute}",PEAR_ERROR_DIE);
+                            PEAR::staticRaiseError("value missing in style key={$attr[0]}  for prop={$attribute}",PEAR_ERROR_EXCEPTION);
                         }
                         $value = trim($attr[1]);
                         if(array_key_exists($key, $this->styleMatch)){
