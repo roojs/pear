8548393fc0564f7ecf63914f494b82d5eacd498a
edward <edward@roojs.com>
2016-04-20 11:16:21 +0800

    I18Nv2/PhonePrefix.php

--- I18Nv2/PhonePrefix.php
+++ I18Nv2/PhonePrefix.php
@@ -1,8 +1,9 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'I18Nv2/CommonList.php';
 
+class I18Nv2_PhonePrefix extends I18Nv2_CommonList
+{
+    
+}
+?>
