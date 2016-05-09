9532ffb2e2b9d5f62b200caf2dca618106bd72b0
edward <edward@roojs.com>
2016-05-09 16:38:39 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -965,6 +965,8 @@ class File_Convert_Solution
         
         $cmd = $conv .' -n ' . escapeshellarg($fn) . ' ' .escapeshellarg($target);
          
+        print_R($cmd);exit;
+        
         $res = $this->exec($cmd);
         clearstatcache();
         
