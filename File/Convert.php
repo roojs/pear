f0060ebe2fbb9b490e355b9f4c207277c6a0e24b
edward <edward@roojs.com>
2016-05-09 16:30:21 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -966,7 +966,7 @@ class File_Convert_Solution
             $cmd .= File_Convert::$options['wkhtmltopdf'];
         }
         
-        print_r($this->options);exit;
+        print_r(File_Convert::options);exit;
         
         $res = $this->exec($cmd);
         clearstatcache();
