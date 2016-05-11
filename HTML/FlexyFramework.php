745adb52d005dfb89cff6b393cce8e594ae53715
edward <edward@roojs.com>
2016-05-11 13:29:27 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -529,10 +529,11 @@ class HTML_FlexyFramework {
         if (!empty($this->enableArray)) {
                 
             $tops = array_merge( array($this->project), empty($this->projectExtends) ? array() : $this->projectExtends);
-            print_r($this->enableArray);exit;
+            
             foreach($tops as $td) {
                     
                 $bd = $this->rootDir .'/'.$td;
+                print_R($bd);exit;
                 foreach($this->enableArray as $m) {
                     // look in Pman/MODULE/DataObjects/*
                      if (file_exists($bd.'/'.$m.'/DataObjects')) {
