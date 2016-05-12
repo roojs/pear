67e724c0b851ce8cd001fdd5042542aa60f52a78
edward <edward@roojs.com>
2016-05-12 15:33:20 +0800

    Spreadsheet/Excel/Writer/Worksheet.php

--- Spreadsheet/Excel/Writer/Worksheet.php
+++ Spreadsheet/Excel/Writer/Worksheet.php
@@ -1863,6 +1863,7 @@ class Spreadsheet_Excel_Writer_Worksheet extends Spreadsheet_Excel_Writer_BIFFwr
 
         // Parse the formula using the parser in Parser.php
         $error = $this->_parser->parse($formula);
+        print_R($this->_parser->_parse_tree);exit;
         if ($this->isError($error)) {
             $this->writeString($row, $col, $error->getMessage());
             return -1;
