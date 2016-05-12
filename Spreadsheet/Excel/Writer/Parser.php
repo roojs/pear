cb5a8a5b407070444f1a2026abf8b664e21cad05
edward <edward@roojs.com>
2016-05-12 11:33:18 +0800

    Spreadsheet/Excel/Writer/Parser.php

--- Spreadsheet/Excel/Writer/Parser.php
+++ Spreadsheet/Excel/Writer/Parser.php
@@ -1309,7 +1309,7 @@ class Spreadsheet_Excel_Writer_Parser extends PEAR
     function _condition()
     {
         $result = $this->_expression();
-        print_R($this->_current_token);exit;
+        print_R($result);exit;
         if (PEAR::isError($result)) {
             return $result;
         }
