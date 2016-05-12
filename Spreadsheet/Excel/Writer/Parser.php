7a977499e09821f6cd547a353f18ca122dde329e
edward <edward@roojs.com>
2016-05-12 14:44:47 +0800

    Spreadsheet/Excel/Writer/Parser.php

--- Spreadsheet/Excel/Writer/Parser.php
+++ Spreadsheet/Excel/Writer/Parser.php
@@ -1695,7 +1695,6 @@ class Spreadsheet_Excel_Writer_Parser extends PEAR
             // add it's left subtree and return.
             return $left_tree.$this->_convertFunction($tree['value'], $tree['right']);
         } else {
-            print_R(($tree['value']));exit;
             $converted_tree = $this->_convert($tree['value']);
             if (PEAR::isError($converted_tree)) {
                 return $converted_tree;
