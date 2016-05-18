9d79b360f71aa68b989f45b9ed5dd3f5a2033495
Alan Knowles <alan@roojs.com>
2016-05-18 16:06:11 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -640,20 +640,20 @@ class HTML_FlexyFramework {
                 }
                 
             }
-            
-        }
-        
-        if (!empty($this->projectExtends) && !empty($this->enableArray))  {
-            foreach ($this->projectExtends as $extend){
-                foreach($this->enableArray as $m) {
-                    $add = $this->rootDir . '/' . $extend . '/' . $m .'/templates';
-                    if (!in_array($add,$src) && file_exists($add) && $this->appNameShort != $m) {
-                        $src[] = $add;
+            if (!empty($this->projectExtends)  )  {
+                foreach ($this->projectExtends as $extend){
+                    foreach($this->enableArray as $m) {
+                        $add = $this->rootDir . '/' . $extend . '/' . $m .'/templates';
+                        if (!in_array($add,$src) && file_exists($add) && $this->appNameShort != $m) {
+                            $src[] = $add;
+                        }
                     }
                 }
+    
             }
-
         }
+        
+        
         
         if ($this->appNameShort) {
             $src[] =  $this->baseDir . '/'. $this->appNameShort. '/templates';
