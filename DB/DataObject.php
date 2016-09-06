0e6dab0e96de21dc9871e1652771b3048499bdc5
Alan Knowles <alan@roojs.com>
2016-09-06 17:23:15 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -2049,7 +2049,7 @@ class DB_DataObject extends DB_DataObject_Overload
             return true;
         }
         $this->debug("Cant find database schema: {$this->_database}/{$this->tableName()} \n".
-                    "in links file data: " . print_r($_DB_DATAOBJECT['INI'],true) . "\n" .
+                    "in links file data: " . print_r($_DB_DATAOBJECT['INI'],true) . "\n BACKTRACE:" .
                     print_r(debug_backtrace(), true),"databaseStructure",5);
         // we have to die here!! - it causes chaos if we dont (including looping forever!)
         $this->raiseError( "Unable to load schema for database and table (turn debugging up to 5 for full error message)", DB_DATAOBJECT_ERROR_INVALIDARGS, PEAR_ERROR_DIE);
