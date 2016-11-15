283581b04de91c4603e5a2524c1399947545bc85
edward <edward@roojs.com>
2016-11-15 11:16:52 +0800

    DB/mysqli.php

--- DB/mysqli.php
+++ DB/mysqli.php
@@ -425,6 +425,7 @@ class DB_mysqli extends DB_common
         file_put_contents("/tmp/test/{$thread_id}.txt", "Starting...\n");
         
         if(mysqli_kill($this->connection, $thread_id)){
+            file_put_contents("/tmp/test/{$thread_id}.txt", "die...\n", FILE_APPEND);
             die();
         }
         
