23c2d32215699b3935fd77a169e7b6c28e444af9
edward <edward@roojs.com>
2016-11-16 11:54:23 +0800

    DB/mysqli.php

--- DB/mysqli.php
+++ DB/mysqli.php
@@ -444,7 +444,7 @@ class DB_mysqli extends DB_common
         @session_start();
         
         if(!in_array($thread_id, $_SESSION['MYSQLI_THREAD_ID'])){
-            return $this->mysqliRaiseError();
+            return $this->mysqliRaiseError(DB_ERROR_CONNECT_FAILED);
         }
         
         unset($_SESSION['MYSQLI_THREAD_ID'][array_search($thread_id, $_SESSION['MYSQLI_THREAD_ID'])]);
