87a1fef9f730509a9be6e607c2dee1827aed7672
edward <edward@roojs.com>
2016-11-17 11:14:05 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -215,7 +215,7 @@ class HTML_WordDiff
         
         @$pageDom->loadHTML(($charset == 'UTF-8' ? '<?xml version="1.0" encoding="UTF-8"?>' : ''). $searchPage);
         
-        print_R($pageDom->documentElement);exit;
+        print_R($pageDom->documentElement->nodeValue);exit;
 //        exit;
         $words = $this->domExtractWords($pageDom->documentElement, array(), $charset);
        // print_r($words);exit;
