40b65a962784ac1b63a6ea4dad7d245f9cd509f8
edward <edward@roojs.com>
2016-11-17 12:55:10 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -288,7 +288,7 @@ class HTML_WordDiff
                 }
             }
             
-            print_R($str);exit;
+            print_R($str[0]);exit;
             
             //var_dump('xx'.$str);
              //var_dump($str);
