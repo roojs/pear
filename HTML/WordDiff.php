ef6f79d3ef90413daed9e05ba1eec93c5881b810
edward <edward@roojs.com>
2016-11-17 12:42:31 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -236,6 +236,7 @@ class HTML_WordDiff
         $words = array();
         
         for ($i = 0; $i < strlen($content); $i++){
+            echo "{$content[$i]}\n";
             $words[] = $content[$i];
         }
         
