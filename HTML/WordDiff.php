8c6d95addc1df6a682b7e58778dd68fc0f27309e
edward <edward@roojs.com>
2016-11-17 12:43:24 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -223,7 +223,7 @@ class HTML_WordDiff
         
         $content = preg_replace('/\s+/', ' ', $content);
         
-        print_R($content[1]);exit;
+        print_R($content);exit;
 //        if ($charset != 'auto') {
 //            if (($this->lang == 'zh_HK' || $this->lang == 'zh_TW') && $charset == 'gb2312') {
 //                $content = mb_convert_encoding($content, $charset,  "UTF-8");
