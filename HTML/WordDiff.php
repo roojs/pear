cf5703cd45290b2cf55173cf065fd71dd82a2f5d
edward <edward@roojs.com>
2016-11-17 12:01:38 +0800

    HTML/WordDiff.php

--- HTML/WordDiff.php
+++ HTML/WordDiff.php
@@ -217,7 +217,7 @@ class HTML_WordDiff
 
         $sentence = $this->parse_node($pageDom->documentElement, array(), $charset);
         
-        $content = preg_replace('/[\w]+/u', '', implode('', $sentence));
+        $content = preg_replace('/[^\w]+/u', '', implode('', $sentence));
         
         print_R($content);exit;
         
