865ec39679a94453654840747375262bee8a2aeb
edward <edward@roojs.com>
2016-12-15 11:00:30 +0800

    XML/SvgToPdf.php

--- XML/SvgToPdf.php
+++ XML/SvgToPdf.php
@@ -126,12 +126,17 @@ class XML_SvgToPDF {
             $font = '/usr/share/fonts/truetype/msttcorefonts/Arial.ttf';
             
 
-            if (!file_exists('/usr/share/fonts/truetype/msttcorefonts/Arial.ttf')) {
-                die("install msttcorefonts package");
-            }
+//            if (!file_exists('/usr/share/fonts/truetype/msttcorefonts/Arial.ttf')) {
+//                die("install msttcorefonts package");
+//            }
+            
+            $pdf->AddFont('ARIALUNI','','/usr/share/fonts/truetype/msttcorefonts/FreeSans.ttf',true);
+            $pdf->AddFont('ARIALUNI','B','/usr/share/fonts/truetype/msttcorefonts/FreeMonoBold.ttf',true);
+            
             //$pdf->AddFont('ARIALUNI','',$font,true);
-            $pdf->AddFont('ARIALUNI','','/usr/share/fonts/truetype/msttcorefonts/Arial.ttf',true);
-            $pdf->AddFont('ARIALUNI','B','/usr/share/fonts/truetype/msttcorefonts/Arial_Bold.ttf',true);
+            
+//            $pdf->AddFont('ARIALUNI','','/usr/share/fonts/truetype/msttcorefonts/Arial.ttf',true);
+//            $pdf->AddFont('ARIALUNI','B','/usr/share/fonts/truetype/msttcorefonts/Arial_Bold.ttf',true);
             
 //            $pdf->SetFont('ARIALUNI','',14);
 //            require_once 'Fpdf/Chinese-unicode.php';
