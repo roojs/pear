9ecb39a86e7cf735aec961caecc8d2514f21d19b
Alan Knowles <alan@roojs.com>
2017-01-18 10:27:58 +0800

    HTML/FlexyFramework2.php

--- HTML/FlexyFramework2.php
+++ HTML/FlexyFramework2.php
@@ -452,7 +452,7 @@ class HTML_FlexyFramework {
         }
         // 
         
-        $this->PDO_DataObject[$dbini] =   $iniCache;
+        $this->PDO_DataObject['schema_location'] =   $iniCache;
         // we now have the configuration file name..
         
         
