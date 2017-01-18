942dbd0877264155da9172841508d476cbcf2738
Alan Knowles <alan@roojs.com>
2017-01-18 12:29:35 +0800

    Attribute changed HTML/FlexyFramework2/Cli.php Attribute changed HTML/FlexyFramework2/Generator.php Attribute changed HTML/FlexyFramework2/Page.php

---
+++ HTML/FlexyFramework2/Generator.php
@@ -0,0 +1,305 @@
+<?php
+
+
+/**
+ * This works by creating '_generate** methods, which are called by start()
+ * 
+ */
+
+require_once 'PDO/DataObject/Generator.php';
+
+
+class HTML_FlexyFramework2_Generator extends PDO_DataObject_Generator 
+{
+    // block class generation.
+    static $generateClasses = false;
+    
+    function generateClasses()
+    {
+//        echo "GENERATE CLASSES?";
+        if (!HTML_FlexyFramework2_Generator::$generateClasses ) {
+            return;
+        }
+       
+        //echo "GENERATE CLASSES?";
+        parent::generateClasses();
+    }
+    
+    
+    function generateReaders()
+    {
+        $options = PDO_DataObject::config();
+        
+        $out = array();
+        foreach($this->tables as $table) {
+              
+            $out = array_merge($out, $this->_generateReader($table));
+            
+            
+        }
+           
+        file_put_contents($options["schema_location"] . '.reader', serialize($out));
+         
+    }
+    
+    function generatePhp ()
+    {
+        // NOOP 
+    }
+    
+    /**
+     * Generate the cached readers used for meta data in the queries.
+     * 
+     */
+    function _generateReader($table)
+    {
+        $DB = $this->PDO();
+        $dbtype = $DB->getAttribute(PDO::ATTR_DRIVER_NAME);;
+         
+        $ret = array();
+        foreach($table->columns as $t) {
+             switch (strtoupper($t->type)) {
+
+                case 'INT':
+                case 'INT2':    // postgres
+                case 'INT4':    // postgres
+                case 'INT8':    // postgres
+                case 'SERIAL4': // postgres
+                case 'SERIAL8': // postgres
+                case 'INTEGER':
+                case 'TINYINT':
+                case 'SMALLINT':
+                case 'MEDIUMINT':
+                case 'BIGINT':
+                // wierd ones..
+                case 'YEAR':
+                
+                    $ret[$table->table.'.'.$t->name] = array('type' => 'int');
+                    continue;
+               
+                case 'REAL':
+                case 'DOUBLE':
+                case 'DOUBLE PRECISION': // double precision (firebird)
+                case 'FLOAT':
+                case 'FLOAT4': // real (postgres)
+                case 'FLOAT8': // double precision (postgres)
+                case 'DECIMAL':
+                case 'MONEY':  // mssql and maybe others
+                case 'NUMERIC':
+                case 'NUMBER': // oci8 
+                    $ret[$table->table.'.'.$t->name] = array('type' => 'float'); //???
+                    break;
+                    
+                case 'BIT':
+                case 'BOOL':   
+                case 'BOOLEAN':   
+                    $ret[$table->table.'.'.$t->name] = array('type' => 'boolean'); //???
+                    // postgres needs to quote '0'
+                    if ($dbtype == 'pgsql') {
+                        ///$type +=  DB_DATAOBJECT_STR;
+                    }
+                    break;
+                    
+                case 'STRING':
+                case 'CHAR':
+                case 'VARCHAR':
+                case 'VARCHAR2':
+                case 'TINYTEXT':
+                
+                case 'ENUM':
+                case 'SET':         // not really but oh well
+                
+                case 'POINT':       // mysql geometry stuff - not really string - but will do..
+                
+                case 'TIMESTAMPTZ': // postgres
+                case 'BPCHAR':      // postgres
+                case 'INTERVAL':    // postgres (eg. '12 days')
+                
+                case 'CIDR':        // postgres IP net spec
+                case 'INET':        // postgres IP
+                case 'MACADDR':     // postgress network Mac address.
+                
+                case 'INTEGER[]':   // postgres type
+                case 'BOOLEAN[]':   // postgres type
+                
+                
+                case 'TEXT':
+                case 'MEDIUMTEXT':
+                case 'LONGTEXT':
+                case 'BLOB':       /// these should really be ignored!!!???
+                case 'TINYBLOB':
+                case 'MEDIUMBLOB':
+                case 'LONGBLOB':
+                
+                case 'CLOB': // oracle character lob support
+                
+                case 'BYTEA':   // postgres blob support..
+                    $ret[$table->table.'.'.$t->name] =  $t->name; // strings are not sent as arrays..
+                   // $type = DB_DATAOBJECT_STR;
+                    break;
+                
+                
+                
+                
+                case 'DATE':    
+                    $ret[$table->table.'.'.$t->name] = array('type' => 'date', 'dateFormat' => 'Y-m-d'); //???
+                    break;
+                    
+                case 'TIME':    
+                    $ret[$table->table.'.'.$t->name] = $t->name; // technically not...
+                    break;    
+                    
+                
+                case 'DATETIME': 
+                    $ret[$table->table.'.'.$t->name] = array('type' => 'date', 'dateFormat' => 'Y-m-d H:i:s'); //???
+                    break;    
+                    
+                case 'TIMESTAMP': // do other databases use this???
+                    
+                    $ret[$table->table.'.'.$t->name] =   ($dbtype == 'mysql') ?
+                         array('type' => 'float') : 
+                        array('type' => 'date', 'dateFormat' => 'Y-m-d H:i:s');
+                    break;    
+                    
+                
+                
+                    
+                    
+                default:     
+                    $ret[$table->table.'.'.$t->name] = $t->name;
+                    break;
+            }
+        }
+        
+        return $ret;
+        
+        
+    }
+    /**
+     * Generate the cached *.ini and links.ini files (merged for all components)
+     * 
+     */
+    static function writeCache($iniCacheTmp, $iniCache)
+    {
+        
+        $fp = fopen($iniCache.".lock", "a+");
+        flock($fp,LOCK_EX);
+
+        $ff = HTML_FlexyFramework2::get();
+        $ff->debug('Framework Generator:writeCache ' . $iniCacheTmp .  ' ' . $iniCache);
+          
+        //var_dump($iniCacheTmp);
+       // echo '<PRE>';echo file_get_contents($iniCacheTmp);exit;
+        // only unpdate if nothing went wrong.
+        if (file_exists($iniCacheTmp) && filesize($iniCacheTmp)) {
+            if (file_exists($iniCache)) {
+                unlink($iniCache);
+            }
+            $ff->debug("Writing merged ini file : $iniCache\n");
+            rename($iniCacheTmp, $iniCache);
+        }
+        
+        // readers..??? not needed??? (historical)
+        if (file_exists($iniCacheTmp.'.reader') &&  filesize($iniCacheTmp.'.reader')) {
+            if (file_exists($iniCache.'.reader')) {
+                unlink($iniCache.'.reader');
+            }
+            $ff->debug("Writing merged reader file : $iniCache.reader\n");
+            rename($iniCacheTmp.'.reader', $iniCache.'.reader');
+        }
+        
+        
+        // merge and set links.. test for generated links file..
+        
+        $linksCacheTmp = preg_replace('/\.ini/', '.links.ini', $iniCacheTmp );
+        $links = array();
+        if (file_exists($linksCacheTmp )) {
+            $links = self::mergeIni( parse_ini_file($linksCacheTmp, true), $links);
+            unlink($linksCacheTmp);
+        }
+        // we are going to use the DataObject directories..
+        
+        $inis = explode(PATH_SEPARATOR,$ff->PDO_DataObject['class_location']);
+        //print_r($inis);exit;
+        $ff->debug("class_location = ". $ff->PDO_DataObject['class_location']);
+        
+        
+        $lproject = strtolower(explode('/', $ff->project)[0]);
+        
+        foreach($inis as $path) {
+            $ini = $path . '/'. strtolower( $lproject ) . '.links.ini';
+             //var_dump($ini);
+            if (!file_exists($ini)) {
+                $ff->debug("Framework Generator:writeCache PROJECT.links.ini does not exist in $path - trying glob");
+       
+                // try scanning the directory for another ini file..
+                $ar = glob(dirname($ini).'/*.links.ini');
+                
+                
+                if (empty($ar)) {
+                    continue;
+                }
+                
+                
+                sort($ar);
+                $ff->debug("Framework Generator:writeCache using {$ar[0]}");
+                
+                // first file.. = with links removed..
+                $ini = preg_replace('/\.links\./' , '.', $ar[0]);
+                $ini = preg_replace('/\.ini$/', '.links.ini', $ini);
+            }
+            
+            // why do this twice???
+            if (!file_exists($ini)) {
+                continue;
+            }
+            $ff->debug("Adding in $ini");
+            // prefer first ?
+            $links = self::mergeIni( parse_ini_file($ini, true), $links);   
+        }
+        $iniLinksCache = preg_replace('/\.ini$/', '.links.ini', $iniCache);
+        $out = array();
+        foreach($links as $tbl=>$ar) {
+            $out[] = '['. $tbl  .']';
+            foreach ($ar as $k=>$v) {
+                $out[] = $k . '=' .$v;
+            }
+            $out[] = '';
+        }
+        if (count($out)) {
+            $ff->debug("Writing merged Links file : $iniLinksCache \n");
+
+           
+            file_put_contents($iniCacheTmp. '.links.ini', implode("\n", $out));
+            if (file_exists($iniLinksCache)) {                
+                unlink($iniLinksCache);
+            }
+            rename($iniCacheTmp. '.links.ini', $iniLinksCache);
+        }
+        
+        flock($fp, LOCK_UN);
+        fclose($fp);
+        unlink($iniCache.".lock");
+        
+    }
+    /* bit like merge recursive, but it avoids doing stuff with arrays.. */
+    static function mergeIni($new, $old) 
+    {
+        foreach($new as $g => $ar) {
+            if (!isset($old[$g])) {
+                $old[$g] = $ar;
+                continue;
+            }
+            foreach($ar as $k=>$v) {
+                if (isset($old[$g][$k])) {
+                    continue;
+                }
+                $old[$g][$k] = $v;
+            }
+        }
+        return $old;
+        
+        
+    }
+    
+}
---
+++ HTML/FlexyFramework2/Page.php
@@ -0,0 +1,370 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2002 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors:  Alan Knowles <alan@akbkhome.com>                           |
+// +----------------------------------------------------------------------+
+//
+// $Id: Page.php,v 1.5 2003/02/22 01:52:50 alan Exp $
+//
+// A Base Page Class for use with HTML_Template_Flexy 
+// You could write one of these which used another template engine.
+//
+
+ 
+/**
+* The Base Page class - extend and override the methods to implement your own pages.
+*
+*
+*/
+
+class HTML_FlexyFramework2_Page  {
+    
+    
+    /**
+    * the main Template name (which can include a body template)
+    *
+    * @var string template name
+    * @access public
+    */
+    var $masterTemplate = "master.html"; 
+    /**
+    * the body Template name
+    *
+    * @var string template name
+    * @access public
+    */
+    var $template = "error.html";
+        
+   
+     
+    
+    /**
+    * cache method - 
+    *   can be 'output', or 'data'
+    * used to set a default caching method
+    *
+    * @var string
+    * @access public
+    * @see getCache()
+    */
+    var $cacheMethod = '';
+    
+     
+    
+    
+   /**
+    * cache store (PEAR's Cache Object Instance
+    *   initialized at start
+    *   set at output stage.
+    *
+    * @var object
+    * @access private
+    * @see getCache()
+    */
+   
+    var $_cache = NULL; 
+    
+    /* ---- Variables set by Page loader -------- */
+    
+   
+        
+    /**
+    * baseURL, that can be prefixed to URL's to ensure that they correctly relate to application
+    * (set by page loader)
+    * @var string
+    * @access public
+    */
+    var $baseURL; 
+    /**
+    * rootURL, the base installation directory - can be used to get images directories.
+    * (set by page loader)
+    * @var string
+    * @access public
+    */
+    var $rootURL; 
+    /**
+    * rootDir, the base installation directory - can be used to find relative files.
+    * (set by page loader)
+    * @var string
+    * @access public
+    */
+    var $rootDir; 
+    /**
+    * the full request string used by the getCacheID().
+    * (set by page loader)
+    * @var string
+    * @access public
+    */
+    var $request; // clean page request for page
+    /**
+    * overrides for elements.
+    *  
+    * @var array
+    * @access public
+    */
+    var $elements = array(); // key=>HTML_Template_Flexy_Element
+   
+     /**
+    * errors for elements
+    *  
+    * @var array
+    * @access public
+    */
+    var $errors = array(); // key(element name)=>error message
+   
+   
+   
+    /**
+    * Arguments from cli if static $cli_opts is used.
+    *  
+    * @var array
+    * @access public
+    */
+    var $cli_args = array(); // key(element name)=>error message
+   
+    /**
+     * Reference to the page loader
+     * @var type HTML_FlexyFramework - 
+     * 
+     */
+     
+    var $bootLoader = false;
+   
+   
+   
+    /**
+    * The default page handler
+    * by default relays to get(), or post() methods depending on the request.
+    *
+    * Override this if you do not handle get or post differently.
+    * 
+    * 
+    * @param   string  request, the remainder of the request not handled by the object.
+    *
+    * @return   none|string none = handled, string = redirect to another page = eg. data/list
+    * @access   public
+    */
+  
+    function start($request,$isRedirect=false,$args=array()) 
+    { 
+        $cli= HTML_Flexyframework2::get()->cli;
+        if (!$cli && !$isRedirect && !empty($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+            return $this->post($request,$args);
+        }  
+        return $this->get($request,$args,$isRedirect);
+        
+    }
+    /**
+    * The get page handler
+    *
+    * Override this if you want to handle get requests 
+    * 
+    * 
+    * @param   string  request, the remainder of the request not handled by the object.
+    *
+    * @return   none|string none = handled, string = redirect to another page = eg. data/list
+    * @access   public
+    */
+    function get($request) 
+    {
+    }
+     /**
+    * The post page handler
+    *
+    * Override this if you want to handle get requests 
+    * 
+    * 
+    * @param   string  request, the remainder of the request not handled by the object.
+    *
+    * @return   none|string none = handled, string = redirect to another page = eg. data/list
+    * @access   public
+    */
+   function post($request) 
+   {
+   }
+    /**
+    * Authentication Check method
+    * Override this with a straight return for pages that do not require authentication
+    *
+    * By default 
+    *   a) redirects to login if authenticaiton fails
+    *   b) checks to see if a isAdmin method exists on the auth object
+    *       if it does see if the user is admin and let them in.
+    *       otherwise access denied error is raised
+    *   c) lets them in.
+    * 
+    * 
+    *
+    * @return   none or string redirection to another page.
+    * @access   public
+    */
+ 
+    function getAuth() {
+         
+        
+        return false;
+        
+    }
+    
+  
+    
+     
+    /**
+    * The master Output layer.
+    * 
+    * compiles the template
+    * if no caching - just runs the template.
+    * otherwise stores it in the cache.
+    * 
+    * you dont normally need to override this.
+    * 
+    * called by the page loader.
+    * @access   public
+    */
+    
+    
+    
+    function output() 
+    {
+        
+        if (!empty($this->cli)) {
+            return;
+        }
+        
+        /* output the body if no masterTemplate is set */
+        $options = HTML_FlexyFramework2::get();
+        
+        $type = isset($this->contentType) ? $this->contentType : 'text/html'; 
+        header('Content-Type: '.$type.';charset='.( empty($options->charset) ? 'UTF-8' : $options->charset ));
+        
+         
+        if (!$this->masterTemplate) {
+            return $this->outputBody();
+        }
+        /* master template */
+        
+       require_once 'HTML/Template/Flexy.php' ;
+
+        $template_engine = new HTML_Template_Flexy(HTML_FlexyFramework2::get()->HTML_Template_Flexy);
+        $template_engine->compile($this->masterTemplate);
+        if (!$this->_cache || !$this->cacheMethod) {
+            $template_engine->outputObject($this,$this->elements);
+            return;
+        }
+        
+        $id = $this->_cache->generateID($this->getID());
+        $this->_cache->save($id, $template_engine->bufferedOutputObject($this,$this->elements));
+        echo $this->_cache->get($id);
+        
+    }
+    /**
+    * The body Output layer.
+    * 
+    * compiles the template
+    * At present there is no caching in here..  - this may change latter..
+    * 
+    * used by putting {outputBody} in the main template.
+    * @access   public
+    */    
+    function outputBody()
+    {
+
+        require_once 'HTML/Template/Flexy.php' ;
+
+        $template_engine = new HTML_Template_Flexy(HTML_FlexyFramework2::get()->HTML_Template_Flexy);
+        $template_engine->compile($this->template);
+        if ($this->elements) { /* BC crap! */
+            require_once 'HTML/Template/Flexy/Factory.php';
+            $this->elements = HTML_Template_Flexy_Factory::setErrors($this->elements,$this->errors);
+        }
+        $template_engine->elements = $this->elements;
+        $template_engine->outputObject($this,$this->elements);
+        
+    }
+    
+     
+    
+    /**
+    * Utility method : get the Class name (used on templates)
+    *
+    * @return   string   class name
+    * @access   public
+    */
+    
+    
+    function getClass() {
+        return get_class($this);
+    }
+    /**
+    * Utility method : get the Time taken to generate the page.
+    *
+    * @return   string   time in seconds..
+    * @access   public
+    */
+    
+    function getTime() {
+         
+        $m = explode(' ',microtime());
+        $ff =  HTML_FlexyFramework::get();
+        return sprintf('%0.2fs',($m[0] + $m[1]) -  $ff->start)
+                . '/ Files ' . count(get_included_files());
+    
+    
+    }
+    /**
+     * turn on off session - wrap long database queries or
+     * data processing with this to prevent locking
+     * @see
+     * @param int $state new session state - 0 = off, 1 = on
+     */ 
+    
+    function sessionState($state)
+    { 
+        static $ses_status = false;
+        static $ini = false;
+        // session status is only php5.4 and up..
+        if (!defined('PHP_SESSION_ACTIVE')) {
+            define('PHP_SESSION_ACTIVE' , 1);
+        }
+        if(!function_exists('session_status')){
+             $ses_status = 1;
+        } else {
+            $ses_status = ($ses_status === false) ? session_status() : $ses_status;        
+        }
+        if (PHP_SESSION_ACTIVE != $ses_status) {
+            return;
+        }
+        
+        switch ($state) {
+            case 0:
+                session_write_close();
+                return;
+            case 1:
+                if ($ini) {  
+                    ini_set('session.use_only_cookies', false);
+                    ini_set('session.use_cookies', false);
+                    ini_set('session.use_trans_sid', false);
+                    ini_set('session.cache_limiter', null);
+                }
+                $ini = true;
+                // sometimes raises a notice - ps_files_cleanup_dir.
+                @session_start();
+                return;
+        }
+    }
+    
+}
+
+ 
