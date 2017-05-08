1275f72a78580b6b13f525b71288be9743a9bd93
Alan Knowles <alan@roojs.com>
2017-05-08 14:22:40 +0800

    HTML/FlexyFramework.php

--- HTML/FlexyFramework.php
+++ HTML/FlexyFramework.php
@@ -516,7 +516,9 @@ class HTML_FlexyFramework {
             $this->DB_DataObject['quote_identifiers_tableinfo'] = true;
         }
         if (!file_exists(dirname($iniCache))) {
-            mkdir(dirname($iniCache),0700, true);
+            if (!mkdir(dirname($iniCache),0700, true)) {
+                die("Failed to make cache directory : $iniCache");
+            }
         }
         
         $this->DB_DataObject[$dbini] = $iniCacheTmp;
