1da7e14f4d3afaa59aba5875c7d2450d996bb879
edward <edward@roojs.com>
2017-11-24 16:32:05 +0800

    HTML/Less/Exception/Compiler.php

--- HTML/Less/Exception/Compiler.php
+++ HTML/Less/Exception/Compiler.php
@@ -1,11 +1,13 @@
 <?php
 
+require_once 'HTML/Less/Exception/Parser.php';
+
 /**
  * Compiler Exception
  *
  * @package Less
  * @subpackage exception
  */
-class HTML_Less_Exception_Compiler extends Less_Exception_Parser {
+class HTML_Less_Exception_Compiler extends HTML_Less_Exception_Parser {
     
 }
