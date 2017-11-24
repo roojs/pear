c358ac8e13f4d3a46dd3df86cae8737cdeefefe3
edward <edward@roojs.com>
2017-11-24 16:36:52 +0800

    HTML/Less/SourceMap/Generator.php

--- HTML/Less/SourceMap/Generator.php
+++ HTML/Less/SourceMap/Generator.php
@@ -1,6 +1,10 @@
 <?php
 
-
+require_once 'HTML/Less/Configurable.php';
+require_once 'HTML/Less/Configurable.php';
+require_once 'HTML/Less/Configurable.php';
+require_once 'HTML/Less/Configurable.php';
+require_once 'HTML/Less/Configurable.php';
 
 /**
  * Source map generator
