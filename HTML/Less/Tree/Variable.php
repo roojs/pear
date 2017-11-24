24757fdab4af084c4b0756866b3d3b27cdd3297d
edward <edward@roojs.com>
2017-11-24 18:08:06 +0800

    HTML/Less/Tree/Variable.php

--- HTML/Less/Tree/Variable.php
+++ HTML/Less/Tree/Variable.php
@@ -6,13 +6,13 @@
  * @package Less
  * @subpackage tree
  */
-class Less_Tree_Variable extends Less_Tree{
+class HTML_Less_Tree_Variable extends HTML_Less_Tree {
 
-	public $name;
-	public $index;
-	public $currentFileInfo;
-	public $evaluating = false;
-	public $type = 'Variable';
+    public $name;
+    public $index;
+    public $currentFileInfo;
+    public $evaluating = false;
+    public $type = 'Variable';
 
     /**
      * @param string $name
@@ -20,33 +20,33 @@ class Less_Tree_Variable extends Less_Tree{
     public function __construct($name, $index = null, $currentFileInfo = null) {
         $this->name = $name;
         $this->index = $index;
-		$this->currentFileInfo = $currentFileInfo;
+        $this->currentFileInfo = $currentFileInfo;
     }
 
-	public function compile($env) {
+    public function compile($env) {
 
-		if( $this->name[1] === '@' ){
-			$v = new Less_Tree_Variable(substr($this->name, 1), $this->index + 1, $this->currentFileInfo);
-			$name = '@' . $v->compile($env)->value;
-		}else{
-			$name = $this->name;
-		}
+        if ($this->name[1] === '@') {
+            $v = new HTML_Less_Tree_Variable(substr($this->name, 1), $this->index + 1, $this->currentFileInfo);
+            $name = '@' . $v->compile($env)->value;
+        } else {
+            $name = $this->name;
+        }
 
-		if ($this->evaluating) {
-			throw new Less_Exception_Compiler("Recursive variable definition for " . $name, null, $this->index, $this->currentFileInfo);
-		}
+        if ($this->evaluating) {
+            throw new HTML_Less_Exception_Compiler("Recursive variable definition for " . $name, null, $this->index, $this->currentFileInfo);
+        }
 
-		$this->evaluating = true;
+        $this->evaluating = true;
 
-		foreach($env->frames as $frame){
-			if( $v = $frame->variable($name) ){
-				$r = $v->value->compile($env);
-				$this->evaluating = false;
-				return $r;
-			}
-		}
+        foreach ($env->frames as $frame) {
+            if ($v = $frame->variable($name)) {
+                $r = $v->value->compile($env);
+                $this->evaluating = false;
+                return $r;
+            }
+        }
 
-		throw new Less_Exception_Compiler("variable " . $name . " is undefined in file ".$this->currentFileInfo["filename"], null, $this->index, $this->currentFileInfo);
-	}
+        throw new HTML_Less_Exception_Compiler("variable " . $name . " is undefined in file " . $this->currentFileInfo["filename"], null, $this->index, $this->currentFileInfo);
+    }
 
 }
