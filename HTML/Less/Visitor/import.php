11814dcd34ee36bbc7e8212a67898899e3463309
edward <edward@roojs.com>
2017-11-24 16:41:53 +0800

    HTML/Less/Visitor/import.php

--- HTML/Less/Visitor/import.php
+++ HTML/Less/Visitor/import.php
@@ -1,7 +1,12 @@
 <?php
 /*
 
+require_once 'HTML/Less/Parser.php';
+require_once 'HTML/Less/VisitorReplacing.php';
 
+require_once 'HTML/Less/Tree/Anonymous.php';
+require_once 'HTML/Less/Visitor.php';
+require_once 'HTML/Less/Visitor.php';
 
 class HTML_Less_Visitor_import extends HTML_Less_VisitorReplacing {
 
