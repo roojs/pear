695c7a96d8898f175276a3b6fa77f45207b53cb4
edward <edward@roojs.com>
2017-11-24 17:45:54 +0800

    HTML/Less/Tree/Negative.php

--- HTML/Less/Tree/Negative.php
+++ HTML/Less/Tree/Negative.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once 'HTML/Less/Tree.php';
+require_once 'HTML/Less/Environment.php';
 
+require_once 'HTML/Less/Tree/Operation.php';
+require_once 'HTML/Less/Environment.php';
+require_once 'HTML/Less/Environment.php';
 
 /**
  * Negative
@@ -8,32 +13,33 @@
  * @package Less
  * @subpackage tree
  */
-class HTML_Less_Tree_Negative extends HTML_Less_Tree{
+class HTML_Less_Tree_Negative extends HTML_Less_Tree {
 
-	public $value;
-	public $type = 'Negative';
+    public $value;
+    public $type = 'Negative';
 
-    public function __construct($node){
-		$this->value = $node;
-	}
+    public function __construct($node) {
+        $this->value = $node;
+    }
 
-	//function accept($visitor) {
-	//	$this->value = $visitor->visit($this->value);
-	//}
+    //function accept($visitor) {
+    //	$this->value = $visitor->visit($this->value);
+    //}
 
     /**
      * @see HTML_Less_Tree::genCSS
      */
-    public function genCSS( $output ){
-		$output->add( '-' );
-		$this->value->genCSS( $output );
-	}
+    public function genCSS($output) {
+        $output->add('-');
+        $this->value->genCSS($output);
+    }
 
     public function compile($env) {
-		if( HTML_Less_Environment::isMathOn() ){
-			$ret = new HTML_Less_Tree_Operation('*', array( new HTML_Less_Tree_Dimension(-1), $this->value ) );
-			return $ret->compile($env);
-		}
-		return new HTML_Less_Tree_Negative( $this->value->compile($env) );
-	}
+        if (HTML_Less_Environment::isMathOn()) {
+            $ret = new HTML_Less_Tree_Operation('*', array(new HTML_Less_Tree_Dimension(-1), $this->value));
+            return $ret->compile($env);
+        }
+        return new HTML_Less_Tree_Negative($this->value->compile($env));
+    }
+
 }
