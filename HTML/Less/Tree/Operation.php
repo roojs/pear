c03147a495079627c53c9faa2661625db5aef472
edward <edward@roojs.com>
2017-11-24 17:47:29 +0800

    HTML/Less/Tree/Operation.php

--- HTML/Less/Tree/Operation.php
+++ HTML/Less/Tree/Operation.php
@@ -1,5 +1,12 @@
 <?php
 
+require_once 'HTML/Less/Environment.php';
+require_once 'HTML/Less/Tree.php';
+
+require_once 'HTML/Less/Tree/Dimension.php';
+require_once 'HTML/Less/Tree/Negative.php';
+require_once 'HTML/Less/Tree/Operation.php';
+
 /**
  * Operation
  *
