d0cc7f47b9c42547a6de13197116835b8ab45583
edward <edward@roojs.com>
2017-11-24 17:49:58 +0800

    HTML/Less/Tree/Quoted.php

--- HTML/Less/Tree/Quoted.php
+++ HTML/Less/Tree/Quoted.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once 'HTML/Less/Parser.php';
+require_once 'HTML/Less/Tree.php';
+
+require_once 'HTML/Less/Tree.php';
+require_once 'HTML/Less/Tree/Variable.php';
+
 /**
  * Quoted
  *
