3fd9479769d517a9be47091275aa5a07582b3c1a
edward <edward@roojs.com>
2017-11-24 18:04:08 +0800

    HTML/Less/Tree/UnicodeDescriptor.php

--- HTML/Less/Tree/UnicodeDescriptor.php
+++ HTML/Less/Tree/UnicodeDescriptor.php
@@ -1,29 +1,31 @@
 <?php
 
+require_once 'HTML/Less/Tree.php';
+
 /**
  * UnicodeDescriptor
  *
  * @package Less
  * @subpackage tree
  */
-class Less_Tree_UnicodeDescriptor extends Less_Tree{
+class HTML_Less_Tree_UnicodeDescriptor extends HTML_Less_Tree {
 
-	public $value;
-	public $type = 'UnicodeDescriptor';
+    public $value;
+    public $type = 'UnicodeDescriptor';
 
-	public function __construct($value){
-		$this->value = $value;
-	}
+    public function __construct($value) {
+        $this->value = $value;
+    }
 
     /**
-     * @see Less_Tree::genCSS
+     * @see HTML_Less_Tree::genCSS
      */
-	public function genCSS( $output ){
-		$output->add( $this->value );
-	}
+    public function genCSS($output) {
+        $output->add($this->value);
+    }
 
-	public function compile(){
-		return $this;
-	}
+    public function compile() {
+        return $this;
+    }
+
 }
-
