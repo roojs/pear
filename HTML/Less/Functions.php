56a4f4b43bf843e2a48f580c77332ddc3199fdff
edward <edward@roojs.com>
2017-11-24 16:20:52 +0800

    HTML/Less/Functions.php

--- HTML/Less/Functions.php
+++ HTML/Less/Functions.php
@@ -1078,11 +1078,11 @@ class HTML_Less_Functions {
     }
 
     public function overlay($color1 = null, $color2 = null) {
-        if (!$color1 instanceof Less_Tree_Color) {
-            throw new Less_Exception_Compiler('The first argument to overlay must be a color' . ($color1 instanceof Less_Tree_Expression ? ' (did you forgot commas?)' : ''));
+        if (!$color1 instanceof HTML_Less_Tree_Color) {
+            throw new HTML_Less_Exception_Compiler('The first argument to overlay must be a color' . ($color1 instanceof HTML_Less_Tree_Expression ? ' (did you forgot commas?)' : ''));
         }
-        if (!$color2 instanceof Less_Tree_Color) {
-            throw new Less_Exception_Compiler('The second argument to overlay must be a color' . ($color2 instanceof Less_Tree_Expression ? ' (did you forgot commas?)' : ''));
+        if (!$color2 instanceof HTML_Less_Tree_Color) {
+            throw new HTML_Less_Exception_Compiler('The second argument to overlay must be a color' . ($color2 instanceof HTML_Less_Tree_Expression ? ' (did you forgot commas?)' : ''));
         }
 
         return $this->colorBlend(array($this, 'colorBlendOverlay'), $color1, $color2);
