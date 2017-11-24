7632e39c6ef5c60c19f95256a8b6e29c2df0515c
edward <edward@roojs.com>
2017-11-24 15:38:45 +0800

    HTML/Less/Parser.php

--- HTML/Less/Parser.php
+++ HTML/Less/Parser.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/Cache.php';
+require_once __DIR__ . '/Colors.php';
 require_once __DIR__ . '/Environment.php';
 require_once __DIR__ . '/Tree/Ruleset.php';
 
@@ -11,7 +12,7 @@ require_once __DIR__ . '/Tree/Comment.php';
 require_once __DIR__ . '/Tree/Quoted.php';
 require_once __DIR__ . '/Tree/Keyword.php';
 require_once __DIR__ . '/Tree/Color.php';
-require_once __DIR__ . '/Colors.php';
+
 
 require_once __DIR__ . '/SourceMap/Generator.php';
 
