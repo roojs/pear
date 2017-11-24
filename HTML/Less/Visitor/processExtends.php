1ccb17f41ae6907ce0ddaa7a6eba1581250b0354
edward <edward@roojs.com>
2017-11-24 16:46:41 +0800

    HTML/Less/Visitor/processExtends.php

--- HTML/Less/Visitor/processExtends.php
+++ HTML/Less/Visitor/processExtends.php
@@ -4,8 +4,8 @@ require_once 'HTML/Less/Visitor.php';
 
 require_once 'HTML/Less/Exception/Parser.php';
 require_once 'HTML/Less/Tree/Attribute.php';
-require_once 'HTML/Less/Visitor.php';
-require_once 'HTML/Less/Visitor.php';
+require_once 'HTML/Less/Tree/Element.php';
+require_once 'HTML/Less/Tree/Extend.php';
 require_once 'HTML/Less/Visitor.php';
 require_once 'HTML/Less/Visitor/extendFinder.php';
 
