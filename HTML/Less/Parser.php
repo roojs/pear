5078cb90ea0102ccc5f2a89d3aec2c09f6da42f3
edward <edward@roojs.com>
2017-12-04 11:38:05 +0800

    HTML/Less/Parser.php

--- HTML/Less/Parser.php
+++ HTML/Less/Parser.php
@@ -843,6 +843,7 @@ class HTML_Less_Parser {
             foreach ($toks as $tok) {
                 $match = $this->$tok();
                 if ($match) {
+                    print_R($match);exit;
                     return $match;
                 }
             }
