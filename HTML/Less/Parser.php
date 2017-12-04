9c6672521d7d1cfeb46613383e3e45ccbf48b8f2
edward <edward@roojs.com>
2017-12-04 11:33:45 +0800

    HTML/Less/Parser.php

--- HTML/Less/Parser.php
+++ HTML/Less/Parser.php
@@ -989,7 +989,7 @@ class HTML_Less_Parser {
 
             //$node = $this->MatchFuncs( array( 'parseMixinDefinition', 'parseRule', 'parseRuleset', 'parseMixinCall', 'parseComment', 'parseDirective'));
             $node = $this->MatchFuncs(array('parseMixinDefinition', 'parseNameValue', 'parseRule', 'parseRuleset', 'parseMixinCall', 'parseComment', 'parseRulesetCall', 'parseDirective'));
-            print_R($node);exit;
+
             if ($node) {
                 $root[] = $node;
             } elseif (!$this->MatchReg('/\\G[\s\n;]+/')) {
@@ -1001,6 +1001,7 @@ class HTML_Less_Parser {
             }
         }
 
+        print_r($root);exit;
         return $root;
     }
 
