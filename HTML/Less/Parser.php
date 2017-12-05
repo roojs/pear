4d3da61e1c9423f324adf87c66b7481748266af0
edward <edward@roojs.com>
2017-12-05 19:14:24 +0800

    HTML/Less/Parser.php

--- HTML/Less/Parser.php
+++ HTML/Less/Parser.php
@@ -722,7 +722,7 @@ class HTML_Less_Parser {
             $this->input .= file_get_contents($file_path);
             
         }
-        print_R(__DIR__);exit;
+
         $this->pos = $this->furthest = 0;
 
         // Remove potential UTF Byte Order Mark
