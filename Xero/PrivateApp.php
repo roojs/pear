fd7542015011dc6b917fd6f34c1f13908817bf6b
KH Lau <khlau@roojs.com>
2017-12-12 12:27:07 +0800

    Xero/PrivateApp.php

--- Xero/PrivateApp.php
+++ Xero/PrivateApp.php
@@ -19,6 +19,8 @@ class Xero_PrivateApp {
    
    var $SSL_KeyPath;
    
+   var $oauthSession;
+   
    function __construct($config)
    {
    	
@@ -55,50 +57,50 @@ class Xero_PrivateApp {
       }
       
       $session = persistSession ( array (
-			'oauth_token' => $XeroOAuth->config ['consumer_key'],
-			'oauth_token_secret' => $XeroOAuth->config ['shared_secret'],
+			'oauth_token' => $this->XeroOAuth->config ['consumer_key'],
+			'oauth_token_secret' => $this->XeroOAuth->config ['shared_secret'],
 			'oauth_session_handle' => '' 
-	) );
-	$oauthSession = retrieveSession ();
+	      ) );
+	   $this->oauthSession = retrieveSession ();
 	
-	if (isset ( $oauthSession ['oauth_token'] )) {
-		$XeroOAuth->config ['access_token'] = $oauthSession ['oauth_token'];
-		$XeroOAuth->config ['access_token_secret'] = $oauthSession ['oauth_token_secret'];
+	   if (isset ( $this->oauthSession ['oauth_token'] )) {
+		    $this->XeroOAuth->config ['access_token'] = $this->oauthSession ['oauth_token'];
+		    $this->XeroOAuth->config ['access_token_secret'] = $this->oauthSession ['oauth_token_secret'];
 		
-		//include 'tests/tests.php';
-	}
-	
-	echo "invoice test";
-	
-	
-    $response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices/765133d8-e03d-41d9-9462-6357fe36621f', 'core'));
-            if ($XeroOAuth->response['code'] == 200) {
-                $invoices = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
-                echo "There are " . count($invoices->Invoices[0]). " invoices in this Xero organisation, the first one is: </br>";
-                //pr($invoices->Invoices[0]->Invoice);
-                print_r($invoices);
-            } else {
-                outputError($XeroOAuth);
-            }		      
-      
+		    //include 'tests/tests.php';
+	   }
+	   
    }
    
    public function getInvoiceList()
    {
        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices', 'core'), array('order' => 'Total DESC'));
-       if ($XeroOAuth->response['code'] != 200) {
+       if ($this->XeroOAuth->response['code'] != 200) {
            // Error 
            return;      
           //outputError($XeroOAuth);
        }	
        
-       $invoices = $this->XeroOAuth->parseResponse($this->XeroOAuth->response['response'], $this->XeroOAuth->response['format']);
-       return $invoices;           
+       $invoiceList = $this->XeroOAuth->parseResponse($this->XeroOAuth->response['response'], $this->XeroOAuth->response['format']);
+       return $invoiceList;           
    }
    
    public function getInvoice($invoiceID)
    {
-   
+   	 if($invoiceID == '')
+   	 {
+   	     return;
+   	 }
+       $response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices/' . $invoiceID , 'core'));
+       
+       if ($this->XeroOAuth->response['code'] != 200) {
+           // Error 
+           return;      
+          //outputError($XeroOAuth);
+       }
+       
+       $invoice = $this->XeroOAuth->parseResponse($this->XeroOAuth->response['response'], $this->XeroOAuth->response['format']);
+       return $invoice;
    }
    
 }
