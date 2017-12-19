a976f8c0663eacb8c8feabf03ec18e6eb722e4a5
KH Lau <khlau@roojs.com>
2017-12-19 13:12:08 +0800

    Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -300,7 +300,7 @@ class Services_Xero {
            return;
        }
        $param = array();
-       $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices/' . $invoiceID , 'core'), $param ,'','json');
+       $response = $this->XeroOAuth->request('GET', $this->url('Invoices/' . $invoiceID , 'core'), $param ,'','json');
        
        if ($this->XeroOAuth->response['code'] != 200) {
        	 
@@ -322,7 +322,7 @@ class Services_Xero {
        
        $param = array('Where' => 'Name="'. $name. '"');
        
-       $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Contacts' , 'core'), $param ,'','json');
+       $response = $this->XeroOAuth->request('GET', $this->url('Contacts' , 'core'), $param ,'','json');
        
        if ($this->XeroOAuth->response['code'] != 200) {
             
