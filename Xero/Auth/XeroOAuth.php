e6ce51a1e1eee7314c8780c13294153389c36a7e
KH Lau <khlau@roojs.com>
2017-12-19 10:55:53 +0800

    Xero/Auth/XeroOAuth.php

--- Xero/Auth/XeroOAuth.php
+++ Xero/Auth/XeroOAuth.php
@@ -443,6 +443,8 @@ class XeroOAuth {
                ) ),
                'signatures' => $this->config 
          ) );
+         
+         print_r($this->sign); exit;
       } 
 
       catch ( Exception $e ) {
