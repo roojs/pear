94617730a3d3db15decf1afd35eeb118de9cdfd9
KH Lau <khlau@roojs.com>
2017-12-19 12:21:02 +0800

    Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -119,7 +119,7 @@ class Services_Xero {
          'oauth_token_secret' => $this->XeroOAuth->config ['shared_secret'],
          'oauth_session_handle' => '' 
        ) );
-	    print_r($this->session);exit;
+	    print_r($this->session);
        $this->oauthSession = $this->retrieveSession ();
 	
        if (isset ( $this->oauthSession ['oauth_token'] )) {
@@ -128,7 +128,7 @@ class Services_Xero {
            $this->XeroOAuth->config ['access_token_secret'] = $this->oauthSession ['oauth_token_secret'];
 		
        }
-	   
+	    print_r($this->oauthSession);exit;
    }
    
    function cert_check()
