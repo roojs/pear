4aacd8455797310bb5245d3b6f3c24b85d830455
edward <edward@roojs.com>
2017-12-22 16:25:18 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -2948,10 +2948,6 @@ class DB_DataObject extends DB_DataObject_Overload
             $ce = substr(phpversion(),0,1) > 4 ? class_exists($class,false) : class_exists($class);
         }
         
-        if($ce){
-            print_r('run??');
-        }
-        print_R($ce);exit;
         //self::debug("CLASS TRY " . var_export($class,true) , __FUNCTION__,5);
         
         $rclass = $ce ? $class  : DB_DataObject::_autoloadClass($class, $table);
