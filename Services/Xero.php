3616fd9a321fd6660c8a9e1aa6ac6cb47c80d494
KH Lau <khlau@roojs.com>
2017-12-27 11:28:47 +0800

    Services/Xero/OAuthSign.php Services/Xero/Invoice.php Services/Xero/OAuth.php Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -59,7 +59,7 @@ class Services_Xero {
        );       
              
        //require 'Xero/Auth/XeroOAuth.php';
-       require 'Services/Xero_OAuth.php';
+       require 'Services/Xero/OAuth.php';
        
        $this->XeroOAuth = new Services_Xero_OAuth ( array_merge ( array (
             'application_type' => $config['xero_app_type'],
