383ba4ace7a5dfb6190d594be3fd3ecf12464e17
edward <edward@roojs.com>
2018-05-23 17:13:34 +0800

    Services/Amazon/AlexaUrlInfo.php

--- Services/Amazon/AlexaUrlInfo.php
+++ Services/Amazon/AlexaUrlInfo.php
@@ -3,9 +3,10 @@
 class Services_Amazon_AlexaUrlInfo
 {
     var $config = array(
-        'accessKeyId' => '',
-        'secretAccessKey' => '',
-        'site' => ''
+        'action'            => '',
+        'accessKeyId'       => '',
+        'secretAccessKey'   => '',
+        'site'              => ''
     );
     
     var $amzDate = false;
@@ -24,6 +25,44 @@ class Services_Amazon_AlexaUrlInfo
         $this->amzDate = gmdate("Ymd\THis\Z", $now);
         $this->dateStamp = gmdate("Ymd", $now);
         
+    }
+    
+    function getUrlInfo() 
+    {
+        $canonicalQuery = $this->buildQueryParams();
+        $canonicalHeaders =  $this->buildHeaders(true);
+        $signedHeaders = $this->buildHeaders(false);
+        $payloadHash = hash('sha256', "");
+        $canonicalRequest = "GET" . "\n" . self::$ServiceURI . "\n" . $canonicalQuery . "\n" . $canonicalHeaders . "\n" . $signedHeaders . "\n" . $payloadHash;
+        $algorithm = "AWS4-HMAC-SHA256";
+        $credentialScope = $this->dateStamp . "/" . self::$ServiceRegion . "/" . self::$ServiceName . "/" . "aws4_request";
+        $stringToSign = $algorithm . "\n" .  $this->amzDate . "\n" .  $credentialScope . "\n" .  hash('sha256', $canonicalRequest);
+        $signingKey = $this->getSignatureKey();
+        $signature = hash_hmac('sha256', $stringToSign, $signingKey);
+        $authorizationHeader = $algorithm . ' ' . 'Credential=' . $this->accessKeyId . '/' . $credentialScope . ', ' .  'SignedHeaders=' . $signedHeaders . ', ' . 'Signature=' . $signature;
+
+        $url = 'https://' . self::$ServiceHost . self::$ServiceURI . '?' . $canonicalQuery;
+        $ret = self::makeRequest($url, $authorizationHeader);
+        echo "\nResults for " . $this->site .":\n\n";
+        echo $ret;
+        self::parseResponse($ret);
+    }
+    
+    function buildQueryParams() 
+    {
+        $params = array(
+            'Action'            => self::$ActionName,
+            'Count'             => self::$NumReturn,
+            'ResponseGroup'     => self::$ResponseGroupName,
+            'Start'             => self::$StartNum,
+            'Url'               => $this->site
+        );
+        ksort($params);
+        $keyvalue = array();
+        foreach($params as $k => $v) {
+            $keyvalue[] = $k . '=' . rawurlencode($v);
+        }
+        return implode('&',$keyvalue);
     }
    
 }
