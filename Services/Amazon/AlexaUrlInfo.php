df2f392ca4ecaf6f889d5bdc644453052415ef4d
edward <edward@roojs.com>
2018-05-23 17:08:11 +0800

    Services/Amazon/AlexaUrlInfo.php

--- Services/Amazon/AlexaUrlInfo.php
+++ Services/Amazon/AlexaUrlInfo.php
@@ -1,1 +1,18 @@
 <?php
+
+/**
+ *  Put standard header here...
+ *
+ */
+
+class Services_Amazon_AlexaUrlInfo
+{
+    var $config = array();
+    
+    function __construct($config)
+    {
+        
+    }
+   
+}
+
