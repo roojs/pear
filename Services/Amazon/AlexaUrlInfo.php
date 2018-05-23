a0ec045a12dadeddcbd7f1610b9e0406c0b02ee5
edward <edward@roojs.com>
2018-05-23 17:39:00 +0800

    Services/Amazon/AlexaUrlInfo.php

--- Services/Amazon/AlexaUrlInfo.php
+++ Services/Amazon/AlexaUrlInfo.php
@@ -28,44 +28,26 @@ class Services_Amazon_AlexaUrlInfo
         $this->dateStamp = gmdate("Ymd", $now);
     }
     
-    function getUrlInfo() 
+    public function getUrlInfo() 
     {
-        $this->action = 'UrlInfo';
-        
-        if(
-                empty($this->config['accessKeyId']) ||
-                empty($this->config['secretAccessKey']) ||
-                empty($this->config['site'])
-        ) {
-            throw new exception("Missing Access Information");
-        }
-        
         $canonicalQuery = $this->buildQueryParams();
-        
         $canonicalHeaders =  $this->buildHeaders(true);
         $signedHeaders = $this->buildHeaders(false);
-        
         $payloadHash = hash('sha256', "");
-        
-        $canonicalRequest = "GET" . "\n" . $this->ServiceURI . "\n" . $canonicalQuery . "\n" . $canonicalHeaders . "\n" . $signedHeaders . "\n" . $payloadHash;
-        
+        $canonicalRequest = "GET" . "\n" . self::$ServiceURI . "\n" . $canonicalQuery . "\n" . $canonicalHeaders . "\n" . $signedHeaders . "\n" . $payloadHash;
         $algorithm = "AWS4-HMAC-SHA256";
-        
-        $credentialScope = $this->dateStamp . "/" . $this->ServiceRegion . "/" . $this->ServiceName . "/" . "aws4_request";
-        
+        $credentialScope = $this->dateStamp . "/" . self::$ServiceRegion . "/" . self::$ServiceName . "/" . "aws4_request";
         $stringToSign = $algorithm . "\n" .  $this->amzDate . "\n" .  $credentialScope . "\n" .  hash('sha256', $canonicalRequest);
-        
         $signingKey = $this->getSignatureKey();
-        
         $signature = hash_hmac('sha256', $stringToSign, $signingKey);
-        
-        $authorizationHeader = $algorithm . ' ' . 'Credential=' . $this->config['accessKeyId'] . '/' . $credentialScope . ', ' .  'SignedHeaders=' . $signedHeaders . ', ' . 'Signature=' . $signature;
+        $authorizationHeader = $algorithm . ' ' . 'Credential=' . $this->accessKeyId . '/' . $credentialScope . ', ' .  'SignedHeaders=' . $signedHeaders . ', ' . 'Signature=' . $signature;
 
         $url = 'https://' . self::$ServiceHost . self::$ServiceURI . '?' . $canonicalQuery;
         $ret = self::makeRequest($url, $authorizationHeader);
         echo "\nResults for " . $this->site .":\n\n";
         echo $ret;
         self::parseResponse($ret);
+        
     }
     
     function buildQueryParams() 
