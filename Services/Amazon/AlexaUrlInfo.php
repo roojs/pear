29a87f974d4887a9f0028bc80f19c2da67e6559d
edward <edward@roojs.com>
2018-05-23 17:10:42 +0800

    Services/Amazon/AlexaUrlInfo.php

--- Services/Amazon/AlexaUrlInfo.php
+++ Services/Amazon/AlexaUrlInfo.php
@@ -1,16 +1,22 @@
 <?php
 
-/**
- *  Put standard header here...
- *
- */
-
 class Services_Amazon_AlexaUrlInfo
 {
-    var $config = array();
+    var $config = array(
+        'accessKeyId' => '',
+        'secretAccessKey' => '',
+        'site' => ''
+    );
     
     function __construct($config)
     {
+        foreach($this->config as $k => $v) {
+            if (isset($config[$k])) {
+               $this->config[$k] = $config[$k];
+            }
+        }
+        
+        
         
     }
    
