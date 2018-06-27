499747ce8df66d451480993f3c74bff7707c657f
edward <edward@roojs.com>
2018-06-27 10:51:50 +0800

    Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -9,9 +9,8 @@ require_once 'Services/Xero/OAuth.php';
 class Services_Xero
 {
     var $oauth_config = array (
-        'consumer_key' => '', //$config['consumer_key'],
-        'shared_secret' => '', //$config['shared_secret'],
-        // API versions
+        'consumer_key' => '',
+        'shared_secret' => '',
         'core_version' => '2.0',
         'payroll_version' => '1.0',
         'file_version' => '1.0',
