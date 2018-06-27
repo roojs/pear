d5451e1469a7d6d005c48e5ad6c36956b8f44106
edward <edward@roojs.com>
2018-06-27 13:01:00 +0800

    Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- *  Put standard header here...
- *
+ * Put standard header here...
+ * https://developer.xero.com/documentation/libraries/php
  */
-
 require 'Services/Xero/OAuth.php';
 
-class Services_Xero
+class Services_Xero 
 {
-    var $oauth_config = array (
+
+    var $oauth_config = array(
         'consumer_key' => '', //$config['consumer_key'],
         'shared_secret' => '', //$config['shared_secret'],
         // API versions
@@ -18,264 +18,239 @@ class Services_Xero
         'file_version' => '1.0',
         'rsa_private_key' => '',
         'rsa_public_key' => '',
-        
         'application_type' => 'Private',
         'oauth_callback' => 'oob',
         CURLOPT_USERAGENT => "XeroOAuth-PHP Private App Test",
         'ca_cert_path' => '',
-    );  
-    
+    );
     var $XeroOAuth;
-    
     var $initialCheck;
-    
     var $checkErrors;
-    
     var $oauthSession;
-    
     var $session;
-    
-    var $_xero_defaults = array (
+    var $_xero_defaults = array(
         'xero_url' => 'https://api.xero.com/',
         'site' => 'https://api.xero.com',
         'authorize_url' => 'https://api.xero.com/oauth/Authorize',
         'signature_method' => 'RSA-SHA1'
-    );       
-   
-  
-   
-    function __construct($config)
+    );
+
+    function __construct($config) 
     {
-        foreach($this->oauth_config as $k=>$v) {
+        foreach ($this->oauth_config as $k => $v) {
             if (isset($config[$k])) {
-               $this->oauth_config[$k] = $config[$k];
+                $this->oauth_config[$k] = $config[$k];
             }
         }
-        
-       
+
+
         if (!empty($config['application_type']) && ($config['application_type'] == "Public")) {
-             $this->_xero_defaults['signature_method'] =  'HMAC-SHA1';
+            $this->_xero_defaults['signature_method'] = 'HMAC-SHA1';
         } else {
-           
+
             $this->oauth_config ['rsa_private_key'] = $config['ca_cert_path'] . '/privatekey.pem';
-            $this->oauth_config ['rsa_public_key'] = $config['ca_cert_path'] . '/publickey.cer'; 
-            
-             
+            $this->oauth_config ['rsa_public_key'] = $config['ca_cert_path'] . '/publickey.cer';
         }
-       
-       $this->XeroOAuth = new Services_Xero_OAuth (   $this->oauth_config   );
-       
-       $this->connectXero();
-       
+
+        $this->XeroOAuth = new Services_Xero_OAuth($this->oauth_config);
+
+        $this->connectXero();
     }
-   
-    function connectXero()
+
+    function connectXero() 
     {
-        $this->initialCheck = $this->XeroOAuth->diagnostics ();
-        $this->checkErrors = count ( $this->initialCheck );
-        
+        $this->initialCheck = $this->XeroOAuth->diagnostics();
+        $this->checkErrors = count($this->initialCheck);
+
         if ($this->checkErrors > 0) {
-            return false;	        
+            return false;
         }
-        
-        $this->session = $this->persistSession ( array (
+
+        $this->session = $this->persistSession(array(
             'oauth_token' => $this->XeroOAuth->config ['consumer_key'],
             'oauth_token_secret' => $this->XeroOAuth->config ['shared_secret'],
-            'oauth_session_handle' => '' 
-        ) );
-             
-        $this->oauthSession = $this->retrieveSession ();
-        
-        if (isset ( $this->oauthSession ['oauth_token'] )) {
+            'oauth_session_handle' => ''
+                ));
+
+        $this->oauthSession = $this->retrieveSession();
+
+        if (isset($this->oauthSession ['oauth_token'])) {
             $this->XeroOAuth->config ['access_token'] = $this->oauthSession ['oauth_token'];
-              
+
             $this->XeroOAuth->config ['access_token_secret'] = $this->oauthSession ['oauth_token_secret'];
-         
         }
-       
     }
 
-   
-   
-   
-
-   function refreshToken()
-   {
-       $response = $this->XeroOAuth->refreshToken($this->oauthSession['oauth_token'], $this->oauthSession['oauth_session_handle']);
-       if ($response['code'] != 200) {
+    function refreshToken() 
+    {
+        $response = $this->XeroOAuth->refreshToken($this->oauthSession['oauth_token'], $this->oauthSession['oauth_session_handle']);
+        if ($response['code'] != 200) {
             if ($response['helper'] == "TokenExpired") {
                 $this->XeroOAuth->refreshToken($this->oauthSession['oauth_token'], $this->oauthSession['session_handle']);
             }
-            
+
             return false;
-            
-       }
-       
-       $this->session = $this->persistSession($response);
-       $this->oauthSession = $this->retrieveSession();
-       
-       return true;
-   }
+        }
+
+        $this->session = $this->persistSession($response);
+        $this->oauthSession = $this->retrieveSession();
+
+        return true;
+    }
+
     /**
      * Persist the OAuth access token and session handle somewhere
      * In my example I am just using the session, but in real world, this is should be a storage engine
      *
      * @param array $params the response parameters as an array of key=value pairs
      */
-   function persistSession($response)
-   {
-       if (isset($response)) {
-           $_SESSION[__CLASS__]['access_token'] = $response['oauth_token'];
-           
-           $_SESSION[__CLASS__]['oauth_token_secret'] = $response['oauth_token_secret'];
-           
-           if(isset($response['oauth_session_handle'])) {
-               $_SESSION[__CLASS__]['session_handle']     = $response['oauth_session_handle'];
-           } 
-       } else {
-           return false;
-       }
-
-   }
-
-   /**
-    * Retrieve the OAuth access token and session handle
-    * In my example I am just using the session, but in real world, this is should be a storage engine
-    *
-    */
-   function retrieveSession()
-   {
-       if (isset($_SESSION[__CLASS__]['access_token'])) {
-           $response['oauth_token']            =    $_SESSION[__CLASS__]['access_token'];
-           
-           $response['oauth_token_secret']     =    $_SESSION[__CLASS__]['oauth_token_secret'];
-           
-           $response['oauth_session_handle']   =    $_SESSION[__CLASS__]['session_handle'];
-           
-           return $response;
-       }       
-       
-       return false;
-
-   }   
-   
-   public function getInvoiceList()
-   {
-       $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices', 'core'), array('order' => 'Total DESC'));
-        if (empty($response['code']) ||  $response['code'] != 200) {
-            throw new Exception('Xero Error: ' . $response['response']);
-           
-           return;                
-       }	
-       
-       $invoiceList = $this->XeroOAuth->parseResponse($response['response'], $response['format']);
-       return $invoiceList;           
-   }
-    
-    public function getInvoicesByFilter($param)
+    function persistSession($response) 
     {
-        if(empty($param)) {
-            throw new Exception("Xero Error: invalid arguments to getInvoicesByFilter" );
-        }
-        
-        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices' , 'core'), $param ,'','json');
-        
-        if (empty($response['code']) ||  $response['code'] != 200) {
-          
-            throw new Exception('Xero Error: ' . $response['response']);
-                     
-           //outputError($XeroOAuth);
-        }
-        
-        $result = $response['result'];
-        
-        if ( !$result || !$response['result'] || empty($response['result']->Invoices)) {
+        if (isset($response)) {
+            $_SESSION[__CLASS__]['access_token'] = $response['oauth_token'];
+
+            $_SESSION[__CLASS__]['oauth_token_secret'] = $response['oauth_token_secret'];
+
+            if (isset($response['oauth_session_handle'])) {
+                $_SESSION[__CLASS__]['session_handle'] = $response['oauth_session_handle'];
+            }
+        } else {
             return false;
         }
-        
-        return $result->Invoices;
-    }   
-   
-    public function getInvoice($invoiceID)
+    }
+
+    /**
+     * Retrieve the OAuth access token and session handle
+     * In my example I am just using the session, but in real world, this is should be a storage engine
+     *
+     */
+    function retrieveSession() 
     {
-        if($invoiceID == '') {
+        if (isset($_SESSION[__CLASS__]['access_token'])) {
+            $response['oauth_token'] = $_SESSION[__CLASS__]['access_token'];
+
+            $response['oauth_token_secret'] = $_SESSION[__CLASS__]['oauth_token_secret'];
+
+            $response['oauth_session_handle'] = $_SESSION[__CLASS__]['session_handle'];
+
+            return $response;
+        }
+
+        return false;
+    }
+
+    public function getInvoiceList() 
+    {
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices', 'core'), array('order' => 'Total DESC'));
+        if (empty($response['code']) || $response['code'] != 200) {
+            throw new Exception('Xero Error: ' . $response['response']);
+
+            return;
+        }
+
+        $invoiceList = $this->XeroOAuth->parseResponse($response['response'], $response['format']);
+        return $invoiceList;
+    }
+
+    public function getInvoicesByFilter($param) 
+    {
+        if (empty($param)) {
+            throw new Exception("Xero Error: invalid arguments to getInvoicesByFilter");
+        }
+
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices', 'core'), $param, '', 'json');
+
+        if (empty($response['code']) || $response['code'] != 200) {
+
+            throw new Exception('Xero Error: ' . $response['response']);
+
+            //outputError($XeroOAuth);
+        }
+
+        $result = $response['result'];
+
+        if (!$result || !$response['result'] || empty($response['result']->Invoices)) {
+            return false;
+        }
+
+        return $result->Invoices;
+    }
+
+    public function getInvoice($invoiceID) 
+    {
+        if ($invoiceID == '') {
             return;
         }
         $param = array();
-        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices/' . $invoiceID , 'core'), $param ,'','json');
-        
-        if (empty($response['code']) ||  $response['code'] != 200) {
-          
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Invoices/' . $invoiceID, 'core'), $param, '', 'json');
+
+        if (empty($response['code']) || $response['code'] != 200) {
+
             throw new Exception('Xero Error: ' . $response['response']);
-                  
-            return;      
-           //outputError($XeroOAuth);
+
+            return;
+            //outputError($XeroOAuth);
         }
-        
+
         $result = $this->XeroOAuth->parseResponse($response['response'], $response['format']);
-        
-        if(    !$result->Invoices 
-            || !count($result->Invoices[0]) 
-            || !$result->Invoices[0]->InvoiceNumber
-           ) {          	
-           return;                        
-        }
-         
-        return $result->Invoices[0];
-    }
-   
-    public function getContact($name)
-    {
-        if($name == '') {
+
+        if (!$result->Invoices || !count($result->Invoices[0]) || !$result->Invoices[0]->InvoiceNumber
+        ) {
             return;
         }
-        
-        $param = array('Where' => 'Name="'. $name. '"');
-        
-        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Contacts' , 'core'), $param ,'','json');
-         
-        if (empty($response['code']) ||  $response['code'] != 200) {     
+
+        return $result->Invoices[0];
+    }
+
+    public function getContact($name) 
+    {
+        if ($name == '') {
+            return;
+        }
+
+        $param = array('Where' => 'Name="' . $name . '"');
+
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Contacts', 'core'), $param, '', 'json');
+
+        if (empty($response['code']) || $response['code'] != 200) {
             throw new Exception('Xero Error: ' . $response['response']);
         }
-        
+
         $contact = $response['result'];
-        
+
         if (!$contact || empty($contact->Contacts[0])) {
             throw new Exception('Could not find contact: ' . $response['response']);
         }
-        
-        
+
+
         return $contact->Contacts[0];
     }
-   
-   
-   
-   
-    public function createInvoice($inv)
+
+    public function createInvoice($inv) 
     {
-        if($inv == '') {
-             return;
+        if ($inv == '') {
+            return;
         }
-        
-        $response = $this->XeroOAuth->request('POST', $this->XeroOAuth->url('Invoices', 'core'), array(), $inv->toXMLString(),'json');
-      
-        if (empty($response['code']) ||  $response['code'] != 200) {
-             throw new Exception('Xero Error: ' . $response['response']);     
+
+        $response = $this->XeroOAuth->request('POST', $this->XeroOAuth->url('Invoices', 'core'), array(), $inv->toXMLString(), 'json');
+
+        if (empty($response['code']) || $response['code'] != 200) {
+            throw new Exception('Xero Error: ' . $response['response']);
         }
-              
+
         $result = $response['result'];
-        
-        if(  !$result ||
-             !$result->Invoices  || 
-             !count($result->Invoices[0]) || 
-             !$result->Invoices[0]->InvoiceNumber
-            ) {          	
-            return;                        
+
+        if (!$result ||
+                !$result->Invoices ||
+                !count($result->Invoices[0]) ||
+                !$result->Invoices[0]->InvoiceNumber
+        ) {
+            return;
         }
-          
-        return $result->Invoices[0];           
-       
+
+        return $result->Invoices[0];
     }
+
     /**
      * get the branding themes (normally templates of invoices etc..)
      *
@@ -284,28 +259,24 @@ class Services_Xero
      * eg. getBrandingThemes( [ "Name" => "Currency Conversion"] )
      *
      */
-    function getBrandingThemes($match = array())
+    function getBrandingThemes($match = array()) 
     {
-        $response = $this->XeroOAuth->request('GET',
-                $this->XeroOAuth->url('BrandingThemes', 'core'), array(), '','json');
-        
-        if (empty($response['code']) ||  $response['code'] != 200) {
-            throw new Exception('Xero Error: ' . $response['response']);     
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('BrandingThemes', 'core'), array(), '', 'json');
+
+        if (empty($response['code']) || $response['code'] != 200) {
+            throw new Exception('Xero Error: ' . $response['response']);
         }
         if (empty($match)) {
-            return  $response['result']->BrandingThemes;
+            return $response['result']->BrandingThemes;
         }
         // not sure if this is an array when only one theme
         foreach ($response['result']->BrandingThemes as $th) {
-            foreach($match as $k=>$v) {
+            foreach ($match as $k => $v) {
                 if ($th->{$k} == $v) {
                     return $th;
                 }
             }
-            
         }
-        
-        
     }
-}
 
+}
