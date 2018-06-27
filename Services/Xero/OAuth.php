02e7d23c34680540b3a3da04bc648a71c10a7261
edward <edward@roojs.com>
2018-06-27 12:01:25 +0800

    Services/Xero/OAuth.php

--- Services/Xero/OAuth.php
+++ Services/Xero/OAuth.php
@@ -569,7 +569,42 @@ class Services_Xero_OAuth
         }
     }
 	
-   /*
+    /**
+    * Returns the current URL.
+    * This is instead of PHP_SELF which is unsafe
+    *
+    * @param bool $dropqs
+    *        	whether to drop the querystring or not. Default true
+    * @return string the current URL
+    */
+    public static function php_self($dropqs = true)
+    {
+        $protocol = 'http';
+        if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') {
+            $protocol = 'https';
+        } elseif (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] == '443')) {
+            $protocol = 'https';
+        }
+        $url = sprintf('%s://%s%s', $protocol, $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']);
+        $parts = parse_url($url);
+        $port = $_SERVER['SERVER_PORT'];
+        $scheme = $parts['scheme'];
+        $host = $parts['host'];
+        $path = @$parts['path'];
+        $qs = @$parts['query'];
+        $port or $port = ($scheme == 'https') ? '443' : '80';
+        if (($scheme == 'https' && $port != '443') || ($scheme == 'http' && $port != '80')) {
+            $host = "$host:$port";
+        }
+        $url = "$scheme://$host$path";
+        if (!$dropqs) {
+            return "{$url}?{$qs}";
+        }
+        
+        return $url;
+    }
+
+    /*
     * Run some basic checks on our config options etc to make sure all is ok
     * Oddly - this is run from the caller???
     * 
