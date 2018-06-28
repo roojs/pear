d45e729542b35a20fe10cfc30650a68162decd69
edward <edward@roojs.com>
2018-06-28 13:57:49 +0800

    Services/Xero.php

--- Services/Xero.php
+++ Services/Xero.php
@@ -20,7 +20,7 @@ class Services_Xero
     
     function getContacts($params = array())
     {
-        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Contacts', 'core'), $params);
+        $response = $this->XeroOAuth->request('GET', $this->XeroOAuth->url('Contacts', 'core'), $params, '', 'json');
         
         if (empty($this->XeroOAuth->response['code']) || $this->XeroOAuth->response['code'] != 200) {
             throw new Exception('Xero Error: ' . $this->XeroOAuth->response['response']);
