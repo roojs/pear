d5f40159a0f727fd6a3d1dfe73f9bee4fd43fd7d
Edward <edward@roojs.com>
2018-07-11 18:26:07 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -297,7 +297,7 @@ Available commands:
         $call[] = array_shift($ar); 
         //var_dump($ar);
         $val = self::$cli_opts;
-        print_r($val);exit;
+        
         $newargs = Console_Getargs::factory($val, $ar);
         
         if (is_a($newargs, 'PEAR_Error')) {
