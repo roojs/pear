359a46ae721002a4fd786edd7a9b0105333efad0
benny <benny@roojs.com>
2018-07-18 11:54:26 +0800

    Console/Getopt.php

--- Console/Getopt.php
+++ Console/Getopt.php
@@ -214,7 +214,7 @@ class Console_Getopt
                     if ($i + 1 < strlen($arg)) {
                         $opts[] = array($opt,  substr($arg, $i + 1));
                         break;
-                    } else if (list(, $opt_arg) = each($args)) { 
+                    }else if (list(, $opt_arg) = each($args)) { 
                     //} else if (foreach($args as $opt_arg))) { 
                         /* Else use the next argument. */;
                         if (Console_Getopt::_isShortOpt($opt_arg)
