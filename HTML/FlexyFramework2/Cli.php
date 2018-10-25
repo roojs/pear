9400703fb567f3f869fb71e13f4ecc5910eef4a9
Alan Knowles <alan@roojs.com>
2018-10-25 11:09:03 +0800

    HTML/FlexyFramework2/Cli.php

--- HTML/FlexyFramework2/Cli.php
+++ HTML/FlexyFramework2/Cli.php
@@ -192,7 +192,7 @@ Available commands:
     
         try {
             // look up the parent tree for core opts.
-            $val = '';
+            $val = array();
              $cls = new ReflectionClass($classname);
             if (method_exists($classname, 'cli_opts')) {
                 $val = $classname::cli_opts();
