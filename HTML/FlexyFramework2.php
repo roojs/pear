2ea136e65d146bceb287a9cbfc5ba95e66965225
john <john@roojs.com>
2018-12-03 16:28:28 +0800

    HTML/FlexyFramework2.php

--- HTML/FlexyFramework2.php
+++ HTML/FlexyFramework2.php
@@ -201,6 +201,8 @@ class HTML_FlexyFramework2 {
         $proj = $cfg['project'];
         $rootDir = realpath(dirname($_SERVER["SCRIPT_FILENAME"]));
 
+        print_r($mods);exit;
+
         foreach($mods as $m) {
             $cls = $proj. (strlen($m) ? '_'. $m  : '' ) . '_Config';
 
