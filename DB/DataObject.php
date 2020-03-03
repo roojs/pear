f0d5719a6e94f2e7f6625c9fc42c22574ccae88b
Alan Knowles <alan@roojs.com>
2020-03-03 16:09:52 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -368,8 +368,10 @@ class DB_DataObject extends DB_DataObject_Overload
                     $this->_query['group_by'] . " \n" .
                     $this->_query['having'] . " \n" .
                 ') ' . $this->_query['derive_table'] . " \n" .
-                (strlen($this->_query['derive_condition']) ? ' WHERE '  : '') . "   " .
-                $this->_query['derive_condition'] . " \n";
+                (strlen($this->_query['derive_condition']) ? ' WHERE '  : '') .
+                    $this->_query['derive_condition'] . " \n"
+                (strlen($this->_query['derive_having']) ? ' HAVING'  : '') .
+                    $this->_query['derive_having'] . " \n";
             return $sql;
             
             
