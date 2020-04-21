167944259bb1dac2b1d583ec4e83ca0f798e29b4
Alan Knowles <alan@roojs.com>
2020-04-21 13:02:52 +0800

    Services/Gapi.php

--- Services/Gapi.php
+++ Services/Gapi.php
@@ -5,7 +5,7 @@ class Services_Gapi
   
     const DEV_MODE = false;
   
-    var $api = 'heets'
+    var $api = 'sheets.googleapis.com';
     var $auth = null;
     var $account_entries = array();
     var $report_aggregate_metrics = array();
@@ -25,13 +25,19 @@ class Services_Gapi
         require_once 'Services/Gapi/OAuth2.php';
         $this->auth = new Services_Gapi_OAuth2();
         $this->auth->fetchToken($client_email, $key_file, null);
+        switch ($api) {
+            case 'sheets':  $this->url = 'http://sheets.google.com/v4/spreadsheets/'; break;
+            case 'sheets':  $this->url = 'http://sheets.google.com/v4/spreadsheets/'; break;
+            default:
+                throw new Exception("Invalid API");
+        }
     }
     
     function request($endpoint, $args = array())
     {
         require_once 'Services/Gapi/Request.php';
         $req = new Services_Gapi_Request($endpoint);
-        $res = $url->get($args, $this->auth_method->generateAuthHeader());
+        $res = $url->get($args, $this->auth->generateAuthHeader());
 
         
     }
