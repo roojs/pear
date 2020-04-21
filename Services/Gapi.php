73bf5af532f1a71bd5c574bfebec552585f9c3b6
Alan Knowles <alan@roojs.com>
2020-04-21 12:32:33 +0800

    Services/Gapi.php

--- Services/Gapi.php
+++ Services/Gapi.php
@@ -3,26 +3,25 @@
 class Services_Gapi
 {
   
-  const DEV_MODE = false;
-
-  private $auth_method = null;
-  private $account_entries = array();
-  private $report_aggregate_metrics = array();
-  private $report_root_parameters = array();
-  private $results = array();
-
-  /**
-   * Constructor function for new gapi instances
-   *
-   * @param string $client_email Email of OAuth2 service account (XXXXX@developer.gserviceaccount.com)
-   * @param string $key_file Location/filename of .p12 key file
-   * @param string $delegate_email Optional email of account to impersonate
-   * @return gapi
-   */
-  public function __construct($client_email, $key_file, $delegate_email = null) {
-    if (version_compare(PHP_VERSION, '5.3.0') < 0) {
-      throw new Exception('GAPI: PHP version ' . PHP_VERSION . ' is below minimum required 5.3.0.');
+    const DEV_MODE = false;
+  
+    var  $auth_method = null;
+    var $account_entries = array();
+    var $report_aggregate_metrics = array();
+    var $report_root_parameters = array();
+    var $results = array();
+  
+    /**
+     * Constructor function for new gapi instances
+     *
+     * @param string $client_email Email of OAuth2 service account (XXXXX@developer.gserviceaccount.com)
+     * @param string $key_file Location/filename of .p12 key file
+     * @param string $delegate_email Optional email of account to impersonate
+     * @return gapi
+     */
+    public function __construct($client_email, $key_file, $delegate_email = null)
+    {
+        require_once 'Gapi/OAuth2.php';
+        $this->auth_method = new Services_Gapi_OAuth2();
+        $this->auth_method->fetchToken($client_email, $key_file, $delegate_email);
     }
-    $this->auth_method = new gapiOAuth2();
-    $this->auth_method->fetchToken($client_email, $key_file, $delegate_email);
-  }
