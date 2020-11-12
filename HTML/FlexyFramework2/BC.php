6804ed0d1e347dc6e1abbc0000aabf405ef04352
Alan Knowles <alan@roojs.com>
2020-11-12 14:04:30 +0800

    Fix #6469 - BC code for framework

---
+++ HTML/FlexyFramework2/BC.php
@@ -0,0 +1,10 @@
+<?php
+
+class HTML_FlexyFramework extends HTML_FlexyFramework2 {
+    static function get() {
+        return HTML_FlexyFramework2::get();
+    }
+}
+
+class HTML_FlexyFramework_Page extends HTML_FlexyFramework2_Page {}
+ 
