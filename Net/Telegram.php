68d18ae4bff73f2fa0c6ed7d0a45e51a0947734b
Alan Knowles <alan@roojs.com>
2021-01-07 16:01:11 +0800

    Net/Telegram.php

--- Net/Telegram.php
+++ Net/Telegram.php
@@ -4,17 +4,22 @@
 class Net_Telegram {
     
     
+    static $tokens = array();
+    
+    var $id;
     
     function __construct($tok)
     {
-        $this->_token = $tok;
-        
+        static $id=0;
+        $this->id = $id;
+        self::$tokens[$id++]  = $tok;
     }
     
     function factory($e, $o=false) {
         require_once 'Net/Telegram/'.ucfirst($e).'.php';
         $cls = 'Net_Telegram_'. $e;
-        return new $cls($o);
+        $ret = new $cls($o);
+        $ret->_telegram = $this;
     }
     
 }
