a85f5184bfa3226b8d40cdfe93563acb84fc9cba
Alan Knowles <alan@roojs.com>
2021-01-07 15:54:08 +0800

    Net/Telegram/Call.php

--- Net/Telegram/Call.php
+++ Net/Telegram/Call.php
@@ -6,14 +6,25 @@ class Net_Telegram_Call extends Net_Telegram_Entity {
     
     function send($tok)
     {
-        $ch = curl_init($website . '/sendMessage');
+        $params = array();
+        foreach((array) $this as $k=>$v) {
+            if (substr($k,0,1) =='_') {
+                continue;
+            }
+            $params[$k] = $v;
+        }
+        
+        $cls = explode('_',get_class($this));
+        $method = lcfirst(array_pop($cls));
+        $ch = curl_init("https://api.telegram.org/bot{$tok}/".lcfirst($method));
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close($ch);
+        return $result;
     }
     
     
