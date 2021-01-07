23ec8d9c124fb151341b1f9618ebffdb43c4fce5
Alan Knowles <alan@roojs.com>
2021-01-07 15:46:12 +0800

    Net/Telegram/SendMessage.php

--- Net/Telegram/SendMessage.php
+++ Net/Telegram/SendMessage.php
@@ -1,18 +1,14 @@
 <?php
 
 require_once 'Entity.php';
-class Net_Telegram_Message extends Net_Telegram_Entity {
+class Net_Telegram_SendMessage extends Net_Telegram_Entity {
     
-    var $message_id;
-    var $from;
-    var $chat;
-    var $date;
+    var $chat_id; // the user id... or channel name ?
     var $text;
+    var $parse_mode; // HTML|Markdown|MarkdownV2
     
     var $_types = array(
-        'from' => 'User',
-        'chat' => 'Chat',
-        'contact' => 'Contact'
+        
     );
     
     function send($tok)
