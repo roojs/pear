ab1a1dfcbc62c3b794077cc29282503c1ed15572
Alan Knowles <alan@roojs.com>
2021-01-07 17:07:37 +0800

    Net/Telegram/SetMyCommands.php

---
+++ Net/Telegram/SetMyCommands.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'Call.php';
+class Net_Telegram_SetMyCommands extends Net_Telegram_Call {
+    
+    var $commands; // array of commands
+    
+    var $_types = array(
+        
+    );
+    
+    function send()
+    {
+          $res = parent::send();
+       // print_R($res);
+        return $this->_telegram->factory('Message',$res);
+    }
+    
+    
+}
+
+    
