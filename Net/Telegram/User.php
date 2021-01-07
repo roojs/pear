49f33637e1e1a9ca1e93ecdc1e79f936063792a0
Alan Knowles <alan@roojs.com>
2021-01-07 15:18:49 +0800

    Net/Telegram/User.php

---
+++ Net/Telegram/User.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Entity.php';
+class Net_Telegram_User extends Net_Telegram_Entity{
+    
+    var $id;
+    var $first_name;
+    var $type;
+    
+    var $_types = array(
+        
+    );
+    
+    
+     
+    
+    
+}
