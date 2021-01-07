8fcd522a33fb6fa400a91158821bf1444734818f
Alan Knowles <alan@roojs.com>
2021-01-07 17:10:18 +0800

    Net/Telegram/BotCommand.php

---
+++ Net/Telegram/BotCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'Entity.php';
+class Net_Telegram_Contact extends Net_Telegram_Entity{
+    
+    var $command;
+    var $description;
+     
+    var $_types = array(
+        
+    );
+    
+    
+     
+    
+    
+}
