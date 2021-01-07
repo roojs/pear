c6dfe4126639d8368fe158933af5bded0ffd2647
Alan Knowles <alan@roojs.com>
2021-01-07 15:39:57 +0800

    Net/Telegram/Contact.php

---
+++ Net/Telegram/Contact.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'Entity.php';
+class Net_Telegram_Contact extends Net_Telegram_Entity{
+    
+    var $phone_number;
+    var $first_name;
+    var $last_name;
+    var $user_id;
+    var $vcard;
+    
+    var $_types = array(
+        
+    );
+    
+    
+     
+    
+    
+}
