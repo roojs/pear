a30851e87188abce499d9defd12061ef1648f936
Alan Knowles <alan@roojs.com>
2021-01-07 16:40:23 +0800

    Net/Telegram/Message.php

--- Net/Telegram/Message.php
+++ Net/Telegram/Message.php
@@ -18,13 +18,14 @@ class Net_Telegram_Message extends Net_Telegram_Entity {
     // quick reply..
     function reply($str)
     {
-        $this->_telegram->factory('SendMessage', array(
+        $mg = $this->_telegram->factory('SendMessage', array(
             'chat_id' => $this->from->id,
             'text' => $str,
             'reply_to_message_id' => $this->message_id,
             'allow_sending_without_reply' => true
             
         ));
+        $mg->send();
         
     }
     
