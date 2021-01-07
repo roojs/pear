fbae41f76e0ad65beb2c8294282b30a9383d3230
Alan Knowles <alan@roojs.com>
2021-01-07 15:17:06 +0800

    Net/Telegram/Entity.php

--- Net/Telegram/Entity.php
+++ Net/Telegram/Entity.php
@@ -2,12 +2,24 @@
 
 class Net_Telegram_Entity {
     
+    
+    var $_types = array();
     function __construct($o=false)
     {
         if ($o === false) {
             return;
         }
         foreach((array)$o as $k=>$v) {
+            if (substr($k,0,1) == '_') {
+                continue;
+            }
+            if (isset($this->_types[$k])) {
+                require_once 'Net/Telegram/'. $this->_types[$k] .'.php';
+                $cls = 'Net_Telegram_'. $this->_types[$k];
+                $this->$k = new $cls($v);
+                continue;
+            }
+            
             $this->$k = $v;
         }
     }
