dcf734cf0c0745ac1cf0aa2cccf3a14f9c0066af
Alan Knowles <alan@roojs.com>
2021-01-07 15:18:37 +0800

    Net/Telegram/Chat.php

--- Net/Telegram/Chat.php
+++ Net/Telegram/Chat.php
@@ -3,15 +3,12 @@
 require_once 'Entity.php';
 class Net_Telegram_Chat extends Net_Telegram_Entity{
     
-    var $message_id;
-    var $from;
-    var $chat;
-    var $date;
-    var $text;
+    var $id;
+    var $first_name;
+    var $type;
     
     var $_types = array(
-        'from' => 'User',
-        'chat' => 'Chat'
+        
     );
     
     
