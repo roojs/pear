ed4eaaeabdd157e6083fa012f863e9fd42165aed
Alan Knowles <alan@roojs.com>
2021-01-07 15:06:21 +0800

    Net/Telegram/Update.php

--- Net/Telegram/Update.php
+++ Net/Telegram/Update.php
@@ -3,7 +3,13 @@
 require_once 'Entity.php';
 class Net_Telegram_Update extends Net_Telegram_Entity {
     
-     
     
+    function __construct($o)
+    {
+        parent::__construct($o);
+        if (isset($this->message)) {
+            $this->message = new Net_Telegram_Update($this->message);
+        }
+    }
     
 }
