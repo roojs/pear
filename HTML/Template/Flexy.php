f7becb26ec75f701f979722e1002e404191c14fd
Alan Knowles <alan@roojs.com>
2021-02-26 12:55:46 +0800

    HTML/Template/Flexy.php

--- HTML/Template/Flexy.php
+++ HTML/Template/Flexy.php
@@ -463,8 +463,7 @@ class HTML_Template_Flexy
              
         }
         
-        var_dump(array($this->currentTemplate, $recompile , date("r",filemtime( $this->compiledTemplate ))));
-        if (!$recompile) {
+         if (!$recompile) {
             $this->debug("File looks like it is uptodate.");
             return true;
         }
