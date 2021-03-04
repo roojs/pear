c9a53a77ecbde67acb039861f7d11eeaf92827ee
Alan Knowles <alan@roojs.com>
2021-03-04 15:04:26 +0800

    fix decode

--- Mail/mimeDecode.php
+++ Mail/mimeDecode.php
@@ -833,11 +833,11 @@ class Mail_mimeDecode extends PEAR
         $input = preg_replace("/=\r?\n/", '', $input);
 
         // Replace encoded characters
-		 
-         
+		  
         $input = preg_replace_callback( '/=([a-f0-9]{2})/i',
                 function($matches) {
-                    return chr(hexdec($matches[0]));
+                 //   var_dump($matches[1]);
+                    return chr(hexdec($matches[1]));
                 }, $input
             );
 
