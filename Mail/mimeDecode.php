13ce74ba73551721f926a8f4802355e7b8dc8afe
Alan Knowles <alan@roojs.com>
2021-03-04 14:55:48 +0800

    PHP7 support

--- Mail/mimeDecode.php
+++ Mail/mimeDecode.php
@@ -834,9 +834,12 @@ class Mail_mimeDecode extends PEAR
 
         // Replace encoded characters
 		 
-        $cb = create_function('$matches',  ' return chr(hexdec($matches[0]));');
          
-        $input = preg_replace_callback( '/=([a-f0-9]{2})/i', $cb, $input);
+        $input = preg_replace_callback( '/=([a-f0-9]{2})/i',
+                function($matches) {
+                    return chr(hexdec($matches[0]));
+                }, $input
+            );
 
         return $input;
     }
