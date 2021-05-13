95fa0bf9bcf50cf99b06095bb15be707bd7eeeab
Alan Knowles <alan@roojs.com>
2021-05-13 17:18:59 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -189,8 +189,7 @@ class File_Convert
      */
     function serve($type=false, $filename =false, $delete_after = false) // may die **/
     {
-        die("HERE");
-        
+        var_dump($this->target);exit;
         if (empty($this->target)) {
             // broken image? for images...
             $cmd = isset($this->lastaction->cmd) ? $this->lastaction->cmd : "No Method";
