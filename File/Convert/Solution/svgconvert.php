cc27cd45f714fd31fe8b3b948b7d3aff32b0e1f1
Alan Knowles <alan@roojs.com>
2021-05-13 15:44:54 +0800

    File/Convert/Solution/svgconvert.php

---
+++ File/Convert/Solution/svgconvert.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class File_Convert_Solution_svgconvert extends File_Convert_Solution
+{
+    
+   
+    var $rules = array(
+        array(
+         
+            'from' =>    array( //source
+                'video/avi',
+                'video/x-ms-wmv',
+                'video/mp4',
+                'video/x-msvideo',
+                'video/mpeg',
+                'video/quicktime',
+            ),
+            'to' =>    array( //target
+                'image/jpeg',
+            )
+        ),
+        
+         
+    );
+    
+    function svgconvert($fn, $x, $y) 
+    {
+        
+        switch($this->to) {
+            case 'application/pdf';
+                $ext = '.pdf';
+                $cvt = 'pdf';
+                break;
+            case 'image/png';
+                $ext = '.png';
+                $cvt = 'png';
+                break;
+            
+                
+        }
+        $opts = '';
+        if (!empty($x)) {
+            $opts .= ' -w '. ((int) $x);
+        }
+        if (!empty($x)) {
+            $opts .= ' -h '. ((int) $y);
+        }
+        
+        $target = $fn . $ext;
+        if (file_exists($target)  && filesize($target) && filemtime($target) > filemtime($fn)) {
+            return $target;
+        }
+        require_once 'System.php';
+        $conv = System::which('rsvg-convert');
+        if (!$conv) {
+            echo "RSVG-CONVERT to available - install librsvg2-bin";
+            return false;
+        }
+        // white background - if you need transparancy -- use another tool?
+        
+        $cmd = "$conv --background-color=white  -f $cvt -o " . escapeshellarg($target) . ' ' .escapeshellarg($fn);
+        
+        $this->exec($cmd);
+        
+        clearstatcache();
+         
+        
+        return  file_exists($target)  && filesize($target) ? $target : false;
+    }
