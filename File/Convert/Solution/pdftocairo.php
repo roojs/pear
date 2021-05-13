57440693c4775a4e14e6b324411b17303b13a7fe
Alan Knowles <alan@roojs.com>
2021-05-13 15:52:41 +0800

    File/Convert/Solution/pdftocairo.php

---
+++ File/Convert/Solution/pdftocairo.php
@@ -0,0 +1,150 @@
+
+    
+      // new version - but does not appear to work that well..
+    
+    function pdftocairo($fn, $x, $y, $pg=false)
+    {
+        
+        $xscale = 600; // min size?
+        if (!empty($x) && $x> $xscale ) {
+            $xscale = $x;
+        }
+        
+        
+        $ext = $this->ext; //'png'; //$this->ext;
+        
+        $target = $fn . '-' . $xscale . '.' .  $ext;
+        if ($pg !== false) {
+            $target = $fn . '-' . $xscale . '-pg'. $pg . '.' .  $ext;
+        }
+        $finaltarget = $target ; //. ($this->ext == 'png' ?  '' : '.jpeg');
+        
+        $this->debug("final target check - $finaltarget ");
+        $this->debug("FE: " . (file_exists($finaltarget)  ? 1 : 0));
+        $this->debug("FS0: " . (file_exists($finaltarget) && filesize($finaltarget) ? 1  : 0));
+        $this->debug("FS: " . (file_exists($finaltarget) ? (filemtime($finaltarget) . ">" . filemtime($fn)) : 'n/a'));
+                     
+        if ($this->debug < 2 && file_exists($finaltarget)  && filesize($finaltarget) && filemtime($finaltarget) > filemtime($fn)) {
+            $this->debug("final target exists - $finaltarget - skipping");
+            return $finaltarget;
+        }
+        require_once 'System.php';
+        
+        
+        
+        // revised version using pdfinfo and pdftoppm
+        
+        $PDFINFO = System::which("pdfinfo");
+        $this->debug("PDFINFO: $PDFINFO");
+        $GREP = System::which("grep");
+        $this->debug("GREP: $GREP");
+        $STRINGS= System::which("strings");
+        $this->debug("PDFINFO: $STRINGS");
+        // needs strings if starngs chars are in there..
+        $cmd = $PDFINFO . ' '. escapeshellarg($fn) . " | $STRINGS | $GREP 'Page size'";
+         
+//        var_dump($cmd);exit;
+        $info = trim( $this->exec($cmd));
+        $match = array();
+        // very presumtiuos...
+       
+       
+        if (!preg_match("/([0-9.]+)[^0-9]+([0-9.]+)/",$info, $match)) {
+            $this->cmd .= " could not find 0-0 in the return string";
+            return false;
+        }
+        
+        $yscale =  floor( ($match[2] / $match[1]) * $xscale) * 3;
+        $xscale = floor($xscale) * 3;
+        $pg = ($pg === false) ? 1 : $pg;
+        
+        
+        // older versions only support png...
+        
+//        print_r($xscale);
+//        print_r($yscale);
+        
+        $PDFTOPPM = System::which("pdftocairo");
+        if (!$PDFTOPPM) {
+            $this->debug("NO PDFTOCAIRO trying pdftoppm");
+            return $this->pdftoppm($fn,$x,$y, $pg);
+            
+        }
+        $cmd = "$PDFTOPPM   -f $pg " 
+                    . "-l $pg  " 
+                    //. "-png "
+                    . "-r 300 " // was 1200?
+//                    . "-rx 1200 "
+//                    . "-ry 1200 "
+                    . '-' . $ext . " "
+                    . " -scale-to-x {$xscale} " 
+                    . " -scale-to-y {$yscale} " 
+                    .  escapeshellarg($fn) . " " 
+                    . escapeshellarg($fn.'-conv');
+        
+        // expect this file..
+//        echo "$cmd <br/>";exit;
+         
+        $res = $this->exec($cmd);
+        $this->result = $res;
+        
+        clearstatcache();
+        // for some reason this makes 01 or 1?
+        $out = $fn . sprintf('-conv-%d.'.str_replace('e', '', $ext) , $pg);
+        
+        $fe = file_exists($out)  && filesize($out) ? true : false;
+        if ($fe) {
+            $this->debug("GOT conv file: renaming $out to $target");
+            rename($out, $target);
+            
+            @chmod($target,fileperms($fn));
+            
+            return $target;
+            
+            
+            print_R('in?');exit;
+            //FIXME never fun this???
+            $ret = $this->ext == 'png' ? $target: $this->convert($target);
+            @chmod($ret,fileperms($fn));
+            
+            return $ret;
+        }
+        $out = $fn . sprintf('-conv-%02d.'.str_replace('e', '', $ext), $pg);
+        //$out = $fn . '-conv-01.jpg';
+        
+        $fe = file_exists($out)  && filesize($out) ? true : false;
+        if ($fe) {
+            $this->debug("GOT conv file: renaming $out to $target");
+             rename($out, $target);
+            @chmod($target,fileperms($fn));
+            return $target;
+            
+            print_R('in?');exit;
+            $ret = $this->ext == 'png' ? $target: $this->convert($target);
+            @chmod($ret,fileperms($fn));
+            return $ret;
+        }
+        
+        $out = $fn . sprintf('-conv-%03d.'.str_replace('e', '', $ext), $pg);
+        //$out = $fn . '-conv-001.jpg'; .. if more than 100 pages...
+        
+        $fe = file_exists($out)  && filesize($out) ? $out : false;
+        if ($fe) {
+            $this->debug("GOT conv file: renaming $out to $target");
+             rename($out, $target);
+            @chmod($target,fileperms($fn));
+            return $target;
+            
+            print_R('in?');exit;
+            
+            //$ret = $this->ext == 'png' ? $target: $this->convert($target);
+            @chmod($ret,fileperms($fn));
+            return $ret;
+        }
+        
+        $this->debug("Could not find OUTPUT FROM pdftocairo");
+        
+        
+        return  false;
+        
+    }
