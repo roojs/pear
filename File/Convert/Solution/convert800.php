c6f38cddc3096dbf7550578877165971641a35c6
Alan Knowles <alan@roojs.com>
2021-05-13 15:47:21 +0800

    File/Convert/Solution/convert800.php

---
+++ File/Convert/Solution/convert800.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class File_Convert_Solution_convert800 extends File_Convert_Solution
+{
+    
+   
+    var $rules = array(
+        array(
+         
+            'from' =>    array( //source
+                'video/avi',
+                'video/x-ms-wmv',
+                'video/mp4',
+                'video/x-msvideo',
+                'video/mpeg',
+                'video/quicktime',
+            ),
+            'to' =>    array( //target
+                'image/jpeg',
+            )
+        ),
+        
+         
+    );
+    function convert800($fn) // might not be needed...
+    {
+        
+        
+        $ext = $this->ext;
+        $target = $fn . '.' . $ext;
+        if (file_exists($target)  && filesize($target) && filemtime($target) > filemtime($fn)) {
+            return $target;
+        }
+        
+        $targetName = $target;
+        $strip = '-strip';
+        if ($this->to == 'image/x-ms-bmp') {
+            $targetName = "bmp3:$target";
+            $strip = '';
+        }
+        
+        require_once 'System.php';
+        $CONVERT = System::which("convert");
+        $cmd = "$CONVERT $strip -colorspace sRGB -interlace none -density 300 ". 
+                        "-quality 90 -resize '400x>' ". escapeshellarg($fn) . " " . escapeshellarg($targetName);
+        
+        $this->exec($cmd);
+        clearstatcache();
+        return  file_exists($target)  && filesize($target) ? $target : false;
+        
+    }
