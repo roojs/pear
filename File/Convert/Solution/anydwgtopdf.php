d0a3368302c56da2b759ca23ebe5d4b2c12a8beb
Alan Knowles <alan@roojs.com>
2021-05-17 14:36:18 +0800

    File/Convert/Solution/anydwgtopdf.php

--- File/Convert/Solution/anydwgtopdf.php
+++ File/Convert/Solution/anydwgtopdf.php
@@ -59,7 +59,7 @@ class File_Convert_Solution_anydwgtopdf extends File_Convert_Solution
         
         
         
-        $cmd = "{$xvfb} --auto {$wine} /InFile C:\\\\{$fromb} /OutFile C:\\\\{$tob}" .
+        $cmd = "{$xvfb} --auto {$wine} \"/var/www/.wine/drive_c/Program Files (x86)/Any DWG to PDF Converter Pro/dp.exe\" /InFile C:\\\\{$fromb} /OutFile C:\\\\{$tob}" .
             "/OutMode AlltoOne /Overwrite /OutLayout Paper /OutArea ZoomExtends";
         $this->exec($cmd);
         if (!file_Exists($wto)) {
