06eb27b2223db6e3fe1ee8b5d2dc8af020b996ab
Ernest Lee <ernest@roojs.com>
2021-06-30 14:14:28 +0800

    File/DXF/SectionHeader.php

--- File/DXF/SectionHeader.php
+++ File/DXF/SectionHeader.php
@@ -2,6 +2,34 @@
 class File_DXF_SectionHeader
 {
     function parse(){
-        
+        $variable_pattern = [
+            'name' => '',
+            'values' => [],
+          ];
+          $variables = [];
+          $variable = $variable_pattern;
+          foreach ($values as $value) {
+            if ($value['key'] == 9) {
+              if (!empty($variable['values'])) {
+                $variables[] = $variable;
+              }
+              $variable = $variable_pattern;
+              $variable['name'] = $value['value'];
+              continue;
+            }
+            $variable['values'][$value['key']] = $value['value'];
+          }
+          if (!empty($variable['values'])) {
+            $variables[] = $variable;
+          }
+      
+          require_once 'File/DXF/SystemVariable.php';
+          foreach($variables as $variable) {
+            $name = str_replace('$', '', $variable['name']);
+            if (strtoupper($name) == 'ACADVER') {
+              $variable['values'] = [1 => 'AC1012'];
+            }
+            $this->header->addItem(new File_DXF_SystemVariable($name, $variable['values']));
+          }
     }
 }
