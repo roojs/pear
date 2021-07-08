7471d873ba38f271c6054dea4a8da8810a714d3e
leon <leon@roojs.com>
2021-07-08 16:02:00 +0800

    File/DXF/Table.php

--- File/DXF/Table.php
+++ File/DXF/Table.php
@@ -17,6 +17,47 @@ class File_DXF_Table extends File_DXF_BasicObject
         }
     }
 
+    function parse($dxf)
+    {
+        while ($pair = $dxf->readPair()) {
+            if ($pair['key'] == 2) {
+                switch ($this->name) {
+                    case 'LTYPE':
+                        $this->addEntry($dxf->factory('LType', array('name' => $pair['value'])));
+                        break;
+                    case 'STYLE':
+                        $this->addEntry($dxf->factory('Style', array('name' => $pair['value'])));
+                        break;
+                    case 'LAYER':
+                        $this->addEntry($dxf->factory('Layer', array('name' => $pair['value'])));
+                        break;
+                    case 'APPID':
+                        $this->addEntry($dxf->factory('AppID', array('name' => $pair['value'])));
+                        break;
+                    case 'BLOCK_RECORD':
+                        $this->addEntry($dxf->factory('BlockRecord', array('name' => $pair['value'])));
+                        break;
+                    case 'DIMSTYLE':
+                        $this->addEntry($dxf->factory('DimStyle', array('name' => $pair['value'])));
+                        break;
+                    case 'UCS':
+                        $this->addEntry($dxf->factory('UCS', array('name' => $pair['value'])));
+                        break;
+                    case 'VIEW':
+                        $this->addEntry($dxf->factory('View', array('name' => $pair['value'])));
+                        break;
+                    case 'VPORT':
+                        $this->addEntry($dxf->factory('VPort', array('name' => $pair['value'])));
+                        break;
+                    default:
+                        print_R($this->name);
+                        die("ERROR got unknown table name");
+                        break;
+                }
+            }
+        }
+    }
+
     /**
      * Public function to render an entity, returns a string representation of
      * the entity.
