5835bac7f4591e1190fb305ad111c72223d81f35
leon <leon@roojs.com>
2021-07-09 12:23:01 +0800

    File/DXF/SystemVariable.php

--- File/DXF/SystemVariable.php
+++ File/DXF/SystemVariable.php
@@ -2,44 +2,61 @@
 
 require_once 'File/DXF/BasicObject.php';
 
-const POINTITEMLABEL = 'point';
+// const POINTITEMLABEL = 'point' ;
 
 class File_DXF_SystemVariable extends File_DXF_BasicObject
 {
-    public $name;
-    public $acadVersion;
-    public $drawingCodePage;
-    public $insertionBase;
-    public $drawingExtentUpperRight;
-    public $drawingExtentLowerLeft;
 
     function parse($dxf) {
-
-        while($pair = $dxf->readPair()) {
-            // End of this variable
-            if ($pair['key'] == 0 || $pair['key'] == 9) {
-                $dxf->pushPair($pair);
-                return;
-            }
-        }
+        $this->skipParseVariable($dxf);
     }
+
+    /*
+     * OLD CODE BELOW
+     */ 
+    
+    // protected $variable;
+    // protected $values;
+
+    /**
+     * SystemVariable constructor.
+     * @param $variable
+     * @param $values
+     */
+    /*
+    function __construct($variable, $values) {
+        $this->variable = $variable;
+        $this->values = $values;
+        parent::__construct();
+    }
+
+    public function getName() {
+        return $this->variable;
+    }
+
+    public function getValues() {
+        return $this->values;
+    }
+    */
 
     /**
      * Public function to render an entity, returns a string representation of
      * the entity.
      * @return string
      */
-    function render() {
+    /*
+    public function render() {
         $output = array();
-        array_push($output, 9, "$" . strtoupper($this->name));
-        if (isset($this->data[POINTITEMLABEL])) {
-            array_push($output, $this->point($this->data[POINTITEMLABEL]));
-            unset($this->data[POINTITEMLABEL]);
+        array_push($output, 9, "$" . strtoupper($this->variable));
+        if (isset($this->values[POINTITEMLABEL])) {
+            array_push($output, $this->point($this->values[POINTITEMLABEL]));
+            unset($this->values[POINTITEMLABEL]);
         }
-        foreach ($this->data as $groupCode => $value) {
+        foreach ($this->values as $groupCode => $value) {
             array_push($output, $groupCode, $value);
         }
         return implode(PHP_EOL, $output);
     }
+    */
 
 }
