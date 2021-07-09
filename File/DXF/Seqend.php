1667e8461d38c5df8ff537beb6ddeea7ea07b9f4
leon <leon@roojs.com>
2021-07-09 13:23:46 +0800

    File/DXF/Seqend.php

--- File/DXF/Seqend.php
+++ File/DXF/Seqend.php
@@ -17,14 +17,33 @@ class File_DXF_Seqend extends File_DXF_Entity
         }
     }
 
+    /*
+     * OLD CODE BELOW
+     */
+
+    /**
+     * Seqend constructor.
+     * @param $pointer
+     * @param $layer
+     */
+    /*
+    function __construct($pointer, $layer) {
+        $this->entityType = 'seqend';
+        $this->pointer = $pointer;
+        $this->layer = $layer;
+        parent::__construct();
+    }
+    */
+
     /**
      * Public function to render an entity, returns a string representation of
      * the entity.
      * @return string
      */
-    function render()
-    {
+    /*
+    public function render() {
         $output = parent::render();
         return implode(PHP_EOL, $output);
     }
+    */
 }
