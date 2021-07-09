77361b50fd2bcebc2371ab311e6a8938a8a516f2
leon <leon@roojs.com>
2021-07-09 13:03:00 +0800

    File/DXF/SectionClasses.php File/DXF/SectionTables.php

--- File/DXF/SectionClasses.php
+++ File/DXF/SectionClasses.php
@@ -5,7 +5,7 @@ require_once 'File/DXF/Section.php';
 class File_DXF_SectionClasses extends File_DXF_Section
 {
 
-    public $name = 'classes';
+    public $name = "classes";
 	
     function parse ($dxf) {
         $this->skipParseSection($dxf);
--- File/DXF/SectionTables.php
+++ File/DXF/SectionTables.php
@@ -4,7 +4,7 @@ require_once 'File/DXF/Section.php';
 
 class File_DXF_SectionTables extends File_DXF_Section
 {
-    public $name = 'tables';
+    public $name = "tables";
 
     function parse($dxf) {
         $this->skipParseSection($dxf);
