26eadb7ab2fd4290e1fdce1db75cda73d48decff
leon <leon@roojs.com>
2021-07-09 12:55:53 +0800

    File/DXF/UCS.php File/DXF/View.php

--- File/DXF/View.php
+++ File/DXF/View.php
@@ -5,22 +5,8 @@ require_once 'File/DXF/BasicObject.php';
 class File_DXF_View extends File_DXF_BasicObject
 {
     
-    public $name;
-    public $data = array();
-
     function parse($dxf) {
-
-        while($pair = $dxf->readPair()) {
-            if ($pair['key'] == 0) {
-                // End of this table entry
-                $dxf->pushPair($pair);
-                return;
-            }
-            if ($pair['key'] == 2) {
-                $this->name = $pair['value'];
-                continue;
-            }
-            $this->data[$pair['key']] = $pair['value'];
-        }
+        $this->skipParseTableEntry($dxf);
     }
+    
 }
