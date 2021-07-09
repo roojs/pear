5e85efde76716ebdd626289d9b03a7b8b8457624
leon <leon@roojs.com>
2021-07-09 12:26:37 +0800

    File/DXF/SectionHeader.php File/DXF/SectionClasses.php

--- File/DXF/SectionClasses.php
+++ File/DXF/SectionClasses.php
@@ -8,11 +8,6 @@ class File_DXF_SectionClasses extends File_DXF_Section
     public $name = 'classes';
 	
     function parse ($dxf) {
-        while ($pair = $dxf->readPair()){
-            if ($pair['key'] == 0 && $pair['value'] == 'ENDSEC') {
-                // End of the classes section
-                return;
-            }
-        }
+        $this->skipParseSection($dxf);
     }
 }
