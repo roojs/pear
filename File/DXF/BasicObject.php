16de69f003ce8d215a7c9d09f3ba0ca2153243ca
leon <leon@roojs.com>
2021-07-09 12:17:34 +0800

    File/DXF/BasicObject.php

--- File/DXF/BasicObject.php
+++ File/DXF/BasicObject.php
@@ -2,48 +2,75 @@
 
 class File_DXF_BasicObject
 {
-    public static $id = 1;
-    public $handle;
 
     function __construct($cfg=array())
     {
-        // unique id for each instance
-        $this->handle = self::$id++;
         foreach($cfg as $k=>$v) {
           	$this->$k = $v;
 	    }
     }
+
+    function 
     
-    /**
-    * Returns a hexadecimal representation of the objects id.
-    *
-    * @return string
-    */
-    function getHandle() {
-        return $this->idToHex($this->handle);
-    }
+    /*
+     * OLD CODE BELOW
+     */
+
+    // public static $id = 1;
+    // protected $handle;
 
     /**
-    * @param $id
-    * @return string
+     * BasicObject constructor.
+     */
+    /*
+    function __construct() {
+        $this->handle = self::$id++;
+    }
     */
-    function idToHex($id) {
+
+    /**
+     * Returns a hexadecimal representation of the objects id.
+     *
+     * @return string
+     */
+    /*
+    public function getHandle() {
+        return $this->idToHex($this->handle);
+    }
+    
+
+    public function getName() {
+        return isset($this->name) ? $this->name : $this->getHandle();
+    }
+    */
+
+    /**
+     * @param $id
+     * @return string
+     */
+    /*
+    public function idToHex($id) {
         return strtoupper(dechex($id));
     }
+    */
 
     /**
      * @return int
      */
-    function getUniqueID() {
+    /*
+    public function getUniqueID() {
         return self::$id++;
     }
+    */
 
     /**
      *
      */
-    function render() {
+    /*
+    public function render() {
 
     }
+    */
 
     /**
      * Function to convert an array with coordinates of a point
@@ -53,13 +80,15 @@ class File_DXF_BasicObject
      * @param int $offset
      * @return string
      */
-    function point($point, $offset = 0) {
+    /*
+    protected function point($point, $offset = 0) {
         $output = array();
         $groupCode = 10 + $offset;
         foreach ($point as $value) {
-            array_push($output, $groupCode, sprintf("%.3f", $value));
-            $groupCode += 10;
+        array_push($output, $groupCode, sprintf("%.3f", $value));
+        $groupCode += 10;
         }
         return implode(PHP_EOL, $output);
     }
+    */
 }
