90afe819f8361203f26ed22c6cf807d55bc8d3bc
leon <leon@roojs.com>
2021-07-09 12:20:36 +0800

    File/DXF/SectionHeader.php

--- File/DXF/SectionHeader.php
+++ File/DXF/SectionHeader.php
@@ -7,23 +7,6 @@ class File_DXF_SectionHeader extends File_DXF_Section
     public $name = 'header';
 
     function parse ($dxf) {
-        while ($pair = $dxf->readPair()){
-            if ($pair['key'] == 0 && $pair['value'] == 'ENDSEC') {
-                // End of the header section
-                return;
-            }
-            if ($pair['key'] == 9) {
-                // Beginning of a new header variable
-                $name = str_replace('$', '', $pair['value']);
-                $variable = $dxf->factory('SystemVariable',
-                    array(
-                        'name' => $name,
-                    ),
-                );
-                $variable->parse();
-                $this->addItem($variable);
-            }
-
-        }
+        $this->skipParseSection($dxf);
     }
 }
