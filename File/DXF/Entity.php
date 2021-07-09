ea92425895aef11428213f284d41f06758f44a78
leon <leon@roojs.com>
2021-07-09 13:06:53 +0800

    File/DXF/Entity.php

--- File/DXF/Entity.php
+++ File/DXF/Entity.php
@@ -4,62 +4,76 @@ require_once 'File/DXF/BasicObject.php';
 
 class File_DXF_Entity extends File_DXF_BasicObject
 {
-	public $entityType;
-	public $layer = 0;
-	public $flags;
-	public $pointer = 0;
-	public $color = 256;
-	public $paperSpace = 0;
-	public $lineType = 'BYLAYER';
-	public $lineTypeScale = 1;
-	public $hidden = 0;
+
+    /*
+     * OLD CODE BELOW
+     */
+
+	// protected $entityType;
+	// protected $layer = 0;
+	// protected $flags;
+	// protected $pointer = 0;
+	// protected $color = 256;
+	// protected $paperSpace = 0;
+	// protected $lineType = 'BYLAYER';	// protected $entityType;
+	// protected $layer = 0;
+	// protected $flags;
+	// protected $pointer = 0;
+	// protected $color = 256;
+	// protected $paperSpace = 0;
+	// protected $lineType = 'BYLAYER';
+	// protected $lineTypeScale = 1;
+	// protected $hidden = 0;
 
 	/**
-	 * public move function
+	 * protected move function
 	 * Move a point with a given move vector
 	 *
 	 * @param $point array
 	 * @param $move array
 	 */
-	function movePoint(&$point, $move)
-	{
-		for ($i = 0; $i < count($move); $i++) {
-			if (!isset($point[$i])) {
-				$point[$i] = 0;
-			}
-			$point[$i] += $move[$i];
+	/*
+	protected function movePoint(&$point, $move) {
+		for($i = 0; $i < count($move); $i++) {
+		if(!isset($point[$i])) {
+			$point[$i] = 0;
+		}
+		$point[$i] += $move[$i];
 		}
 	}
+	*/
 
 	/**
-	 * public rotate function
+	 * protected rotate function
 	 * Rotate one point around a center point with an angle
 	 *
 	 * @param $point array
 	 * @param $center array
 	 * @param $angle float
 	 */
-	function rotatePoint(&$point, $center, $angle)
-	{
+	/*
+	protected function rotatePoint(&$point, $center, $angle) {
 		$xPos = $point[0];
 		$yPos = $point[1];
 		$point[0] = $center[0] + ($xPos - $center[0]) * cos($angle) - ($yPos - $center[1]) * sin($angle);
 		$point[1] = $center[1] + ($yPos - $center[1]) * cos($angle) + ($xPos - $center[0]) * sin($angle);
 	}
+	*/
 
 	/**
-	 * public function for rendering the flags
+	 * protected function for rendering the flags
 	 *
 	 * @return int|number
 	 */
-	function flagsToString()
-	{
+	/*
+	protected function flagsToString() {
 		$output = 0;
 		foreach ($this->flags as $i => $flag) {
-			$output += pow(2, $i) * $flag;
+		$output += pow(2, $i) * $flag;
 		}
 		return $output;
 	}
+	*/
 
 	/**
 	 * Public function to set flag values for entities
@@ -67,19 +81,57 @@ class File_DXF_Entity extends File_DXF_BasicObject
 	 * @param $id int
 	 * @param $value 0|1
 	 */
-	function setFlag($id, $value)
-	{
+	/*
+	public function setFlag($id, $value) {
 		$this->flags[$id] = $value;
 		return $this;
 	}
+	*/
 
 	/**
 	 * Retrieves a flag value for an entity.
 	 */
-	function getFlag($id)
-	{
+	/*
+	public function getFlag($id) {
 		return $this->flags[$id];
 	}
+	*/
+
+	/**
+	 * Public function to set the layer of an entity
+	 *
+	 * @param $layer string
+	 */
+	/*
+	public function setLayer($layer) {
+		$this->layer = $layer;
+		return $this;
+	}
+	*/
+
+	/**
+	 * Public function to set the color of an entity
+	 *
+	 * @param $color int autodesc color code 0 = BYBLOCK, 256 = BYLAYER
+	 */
+	/*
+	public function setColor($color) {
+		$this->color = $color;
+		return $this;
+	}
+	*/
+
+	/**
+	 * Public function to define if a object should belong to paper space
+	 *
+	 * @param $ps 0|1
+	 */
+	/*
+	public function setPaperSpace($ps) {
+		$this->paperSpace = $ps;
+		return $this;
+	}
+	*/
 
 	/**
 	 * Public function to set the lineType of an entity and an optional scale
@@ -87,31 +139,33 @@ class File_DXF_Entity extends File_DXF_BasicObject
 	 * @param $lineType
 	 * @param int $scale float
 	 */
-	function setLineType($lineType, $scale = 1)
-	{
+	/*
+	public function setLineType($lineType, $scale = 1) {
 		$this->lineType = $lineType;
 		$this->lineTypeScale = $scale;
 		return $this;
 	}
+	*/
 
 	/**
 	 * Public function to hide an entity
 	 *
 	 * @param $hidden
 	 */
-	public function hide($hidden)
-	{
+	/*
+	public function hide($hidden) {
 		$this->hidden = $hidden;
 		return $this;
 	}
+	*/
 
 	/**
 	 * Public function to render an entity, returns a string representation of
 	 * the entity.
 	 * @return array
 	 */
-	function render()
-	{
+	/*
+	function render() {
 		$output = array();
 		array_push($output, 0, strtoupper($this->entityType));
 		array_push($output, 5, $this->getHandle());
@@ -125,4 +179,5 @@ class File_DXF_Entity extends File_DXF_BasicObject
 		array_push($output, 60, $this->hidden);
 		return $output;
 	}
+	*/
 }
