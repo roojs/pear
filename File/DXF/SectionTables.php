317acd12c9e3635ee74a4e091c21f4a194785a01
leon <leon@roojs.com>
2021-07-09 12:38:54 +0800

    File/DXF/SectionTables.php

--- File/DXF/SectionTables.php
+++ File/DXF/SectionTables.php
@@ -9,43 +9,5 @@ class File_DXF_SectionTables extends File_DXF_Section
     function parse($dxf) {
         $this->skipParseSection($dxf);
     }
-	
-    /*
-    public function parse($dxf)
-    {
-        while ($pair = $dxf->readPair()) {
-
-            if($pair['key'] == 0) {
-                if ($pair['value'] == 'ENDSEC') {
-                    // End of the tables section
-                    return;
-                } 
-                if ($pair['value'] == 'TABLE'){
-                    // Beginning of a new table
-                    continue;
-                } 
-            }
-
-            if ($pair['key'] == 2) {
-                $tableName = $pair['value'];
-
-                switch ($tableName) {
-                    case 'LTYPE':
-                    case 'STYLE':
-                    case 'LAYER':
-                    case 'APPID':
-                    case 'BLOCK_RECORD':
-                    case 'DIMSTYLE':
-                    case 'UCS':
-                    case 'VIEW':
-                    case 'VPORT':
-                        $table = $dxf->factory('Table', array('name' => $tableName));
-                        $table->parse($dxf);
-         ENDSEC')             break;
-                    
-                }
-            }
-        }
-    }
-    */
+    
 }
