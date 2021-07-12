5b86d8346ab57dc059d8ce3ee7e6dc5ca441bf59
leon <leon@roojs.com>
2021-07-12 17:01:52 +0800

    File/DXF/AcDbAttribute.php

--- File/DXF/AcDbAttribute.php
+++ File/DXF/AcDbAttribute.php
@@ -32,6 +32,7 @@ class File_DXF_AcDbAttribute extends File_DXF_Subclass
             switch($pair['key']) {
                 case 0:
                 case 100:
+                case 1001:
                     // End of a subclass
                     $dxf->pushPair($pair);
                     return;
