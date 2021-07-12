8f7f05303d4b373245c436c54619a6a3eadb14e8
leon <leon@roojs.com>
2021-07-12 13:31:54 +0800

    File/DXF/Attrib.php

--- File/DXF/Attrib.php
+++ File/DXF/Attrib.php
@@ -7,38 +7,6 @@ class File_DXF_Attrib extends File_DXF_Entity
 
     public $subclasses = array();
 
-    // For subclass Attribute
-    public $attributeSubclassMarker; // 100
-    public $VersionNumber = 0; // 280
-    public $attributeTag; // 2
-    public $attributeFlags; // 70
-    public $fieldLength = 0; // 73
-    public $textRotation = 0; // 50
-    public $scaleX = 1; // 41
-    public $obliqueAngle = 0; // 51
-    public $textStyleName = "STANDARD"; // 7
-    public $textGenerationFlags = 0; // 71
-    public $horizontalTextJustificationType = 0; // 72
-    public $verticalTextJustificationType = 0; // 74
-    public $alignmentPointX; // 11
-    public $alignmentPointY; // 21
-    public $alignmentPointZ; // 31
-    public $extrusionDirectionX = 0; // 210
-    public $extrusionDirectionY = 0; // 220
-    public $extrusionDirectionZ = 1; // 230
-    public $lockPositionFlag; // 280
-
-    // For subclass XRecord
-    public $xrecordSubclassMarker; // 100
-    public $duplicateRecordCloingFlag; // 280
-    public $numberOfAttributeDefinitions; // 70
-    public $hardPointerOfAttributeDefinition; // 340
-    public $attributeDefinitionAlignmentPointX; // 10
-    public $xrecordInsertionPointY; // 20
-    public $xrecordInsertionPointZ; // 30
-    public $annotationScale; // 40
-    public $attributeDefinitionTagString; // 2
-
     function parse($dxf)
     {
         // parse common pair for entities
