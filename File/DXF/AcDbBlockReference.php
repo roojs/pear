f6bace067185a09b940bd3903acd0feebf6772d9
leon <leon@roojs.com>
2021-07-12 16:44:14 +0800

    File/DXF/AcDbBlockReference.php

--- File/DXF/AcDbBlockReference.php
+++ File/DXF/AcDbBlockReference.php
@@ -83,7 +83,8 @@ class File_DXF_AcDbBlockReference extends File_DXF_Subclass
                     break;
                 default:
                     $groupCode = $pair['key'];
-                    throw new Exception ("Got unknown group code for subclass AcDbBlockReference ($pair)");
+                    $value = $pair['value'];
+                    throw new Exception ("Got unknown group code for subclass AcDbBlockReference ($groupCode, $value)");
                     break;
             }
         }
