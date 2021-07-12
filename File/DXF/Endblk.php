f9242386f39bf507be248a39191e7cfee9827080
leon <leon@roojs.com>
2021-07-12 19:52:36 +0800

    File/DXF/Endblk.php

--- File/DXF/Endblk.php
+++ File/DXF/Endblk.php
@@ -1,30 +1,20 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: jpietler
- * Date: 30.12.16
- * Time: 16:18
- *
- * Dokumentation http://www.autodesk.com/techpubs/autocad/acad2000/dxf/endblk_dxf_05.htm
- * This is baed on DXF Fighter by - https://github.com/enjoping/DXFighter"
- */
-
-
-/**
- * Class Endblk
- * @package DXFighter\lib
- */
 require_once 'File/DXF/Entity.php';
 
-class File_DXF_Endblk extends File_DXF_Entity
+class File_DXF_EndBlk extends File_DXF_Entity
 {
+
+    /*
+     * OLD CODE BELOW
+     */
 
     /**
      * Endblk constructor.
      * @param $layer
      * @param $pointer
      */
+    /*
     function __construct($layer, $pointer)
     {
         $this->entityType = 'endblk';
@@ -32,16 +22,19 @@ class File_DXF_Endblk extends File_DXF_Entity
         $this->pointer = $pointer;
         parent::__construct();
     }
+    */
 
     /**
      * Public function to render an entity, returns a string representation of
      * the entity.
      * @return string
      */
+    /*
     public function render()
     {
         $output = parent::render();
         array_push($output, 100, 'AcDbBlockEnd');
         return implode(PHP_EOL, $output);
     }
+    */
 }
