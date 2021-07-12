bf85179e6c40242760c8766e228f179cacff848c
leon <leon@roojs.com>
2021-07-12 15:33:32 +0800

    File/DXF/SectionObjects.php

--- File/DXF/SectionObjects.php
+++ File/DXF/SectionObjects.php
@@ -2,19 +2,10 @@
 class File_DXF_SectionObjects extends File_DXF_Section
 {
 
-    public $name = 'objects';
-    
-    /**
-	 *
-	 * TODO ENHANCE / CHECK THE CODE BLOEW
-	 *
-	 */
-	 
-    public function parse()
-    {
-        // TODO add the actually read objects
-		require_once 'File/DXF/Dictionary.php';
+    public $name = "objects";
 
-		$this->addItem(new File_DXF_Dictionary(array('ACAD_GROUP')));
+    function parse ($dxf) {
+        $this->skipParseSection($dxf);
     }
+
 }
