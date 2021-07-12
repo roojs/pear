2e289a561970a559de1f26f11f50745045898858
leon <leon@roojs.com>
2021-07-12 15:44:21 +0800

    File/DXF/SectionBlocks.php

--- File/DXF/SectionBlocks.php
+++ File/DXF/SectionBlocks.php
@@ -1,45 +1,14 @@
 <?php
 
 require_once 'File/DXF/Section.php';
-require_once 'File/DXF/SectionEntities.php';
 
 class File_DXF_SectionBlocks extends File_DXF_Section
 {
+
+	public $name = 'blocks';
 	 
-    public function parse($handle)
-    {
+    function parse($dxf) {
+		$this->skipParseSection($dxf);
+	}
 
-		public $name = 'blocks';
-		
-        $block = [];
-		$entitiesSection = [];
-		require_once 'File/DXF/Block.php';
-
-		while ($pair = $this->readPair($handle)) {
-            if ($pair['value'] == 'ENDSEC') {
-                break;
-            }
-			if ($pair['key'] == 0) {
-				switch ($pair['value']) {
-					case 'BLOCK':
-						$block = [];
-						break;
-					case 'ENDBLK':
-						$blockEntity = new File_DXF_Block($block[2]);
-						$entities = $this->readEntitiesSection($entitiesSection);
-						foreach ($entities as $entity) {
-							$blockEntity->addEntity($entity);
-						}
-						$this->addItem($blockEntity);
-						break;
-					default:
-						$entitiesSection[] = $pair;
-				}
-			} elseif (empty($entitiesSection)) {
-				$block[$pair['key']] = $pair['value'];
-			} else {
-				$entitiesSection[] = $pair;
-			}
-		}
-    }
 }
