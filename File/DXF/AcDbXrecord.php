b1a5961cc65adb190bf97f3831f809ddd14a632e
leon <leon@roojs.com>
2021-07-12 13:40:28 +0800

    File/DXF/AcDbXrecord.php

---
+++ File/DXF/AcDbXrecord.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'File/DXF/Subclass.php';
+
+class File_DXF_AcDbXrecord extends File_DXF_Subclass
+{
+
+    // For entity ATTRIB
+    public $duplicateRecordCloingFlag; // 280
+    public $numberOfAttributeDefinitions; // 70
+    public $hardPointerOfAttributeDefinition; // 340
+    public $alignmentPointX; // 10
+    public $insertionPointY; // 20
+    public $insertionPointZ; // 30
+    public $annotationScale; // 40
+    public $attributeDefinitionTagString; // 2
+
+    function parse($dxf)
+    {
+        
+        while($pair = $dxf->readPair()) {
+
+            switch($pair['key']) {
+                case 0:
+                case 100:
+                    // End of a subclass
+                    $dxf->pushPair($pair);
+                    return;
+                    break;
+                case 280:
+                    $this->duplicateRecordCloingFlag = $pair['value'];
+                    break;
+                case 70:
+                    $this->numberOfAttributeDefinitions = $pair['value'];
+                    break;
+                case 340:
+                    $this->hardPointerOfAttributeDefinition = $pair['value'];
+                    break;
+                case 10:
+                    $this->alignmentPointX = $pair['value'];
+                    break;
+                case 20:
+                    $this->insertionPointY = $pair['value'];
+                    break;
+                case 30:
+                    $this->insertionPointZ = $pair['value'];
+                    break;
+                case 40:
+                    $this->annotationScale = $pair['value'];
+                    break;
+                case 2:
+                    $this->attributeDefinitionTagString = $pair['value'];
+                    break;
+                default:
+                    $groupCode = $pair['key'];
+                    throw new Exception ("Got unknown group code for subclass AcDbXRecord ($groupCode)");
+                    break;
+            }
+        }
+    }
+}
