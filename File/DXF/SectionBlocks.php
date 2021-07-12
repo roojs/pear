26990421c6d353367323dbb0f20082c78f42a8c6
leon <leon@roojs.com>
2021-07-12 15:37:23 +0800

    File/DXF/SectionBlocks.php

--- File/DXF/SectionBlocks.php
+++ File/DXF/SectionBlocks.php
@@ -5,17 +5,12 @@ require_once 'File/DXF/SectionEntities.php';
 
 class File_DXF_SectionBlocks extends File_DXF_Section
 {
-
-    public $name = 'blocks';
-    
-    /**
-	 *
-	 * TODO ENHANCE / CHECK THE CODE BLOEW
-	 *
-	 */
 	 
     public function parse($handle)
     {
+
+		public $name = 'blocks';
+		
         $block = [];
 		$entitiesSection = [];
 		require_once 'File/DXF/Block.php';
@@ -47,75 +42,4 @@ class File_DXF_SectionBlocks extends File_DXF_Section
 			}
 		}
     }
-
-	public function readEntitiesSection($values){
-		$entities = [];
-		$entityType = '';
-		$data = [];
-		$types = ['TEXT', 'LINE', 'ELLIPSE', 'SPLINE', 'INSERT', 'ARC', 'CIRCLE', 'LWPOLYLINE'];
-		// TODO most entity types are still missing
-		foreach ($values as $value) {
-		  if ($value['key'] == 0) {
-			// This condition happens at the end of an entity
-			if (
-			  (in_array($entityType, $types) && !empty($data)) ||
-			  in_array($entityType, ['POLYLINE', 'VERTEX']) && $value['value'] == 'SEQEND'
-			) {
-			  $entity = File_DXF_SectionEntities::addReadEntity($entityType, $data);
-			  if ($entity) {
-				$entities[] = $entity;
-			  }
-			  $data = [];
-			}
-			$entityType = $value['value'];
-			if ($value['value'] == 'VERTEX') {
-			  $data['points'][] = [];
-			}
-			if ($value['value'] == 'SPLINE') {
-			  $data['knots'] = [];
-			  $data['points'] = [];
-			}
-			if ($value['value'] == 'LWPOLYLINE') {
-			  $data['points'] = [];
-			}
-		  } else {
-			if ($entityType == 'SPLINE' && in_array($value['key'], [10, 20, 30, 40])) {
-			  switch ($value['key']) {
-				case 10:
-				  $data['points'][] = [10 => $value['value'], 20 => 0, 30 => 0];
-				  break;
-				case 20:
-				case 30:
-				  $data['points'][sizeof($data['points']) -1 ][$value['key']] = $value['value'];
-				  break;
-				case 40:
-				  $data['knots'][] = $value['value'];
-				  break;
-			  }
-			} elseif ($entityType == 'LWPOLYLINE' && in_array($value['key'], [10, 20, 42])) {
-			  switch ($value['key']) {
-				case 10:
-				  $data['points'][] = [10 => $value['value'], 20 => 0, 42 => 0];
-				  break;
-				case 20:
-				case 42:
-				  $data['points'][sizeof($data['points']) -1 ][$value['key']] = $value['value'];
-				  break;
-			  }
-			} elseif (in_array($entityType, $types) || $entityType == 'POLYLINE') {
-			  $data[$value['key']] = $value['value'];
-			} elseif ($entityType == 'VERTEX') {
-			  $data['points'][count($data['points']) - 1][$value['key']] = $value['value'];
-			}
-		  }
-		}
-		if (in_array($entityType, $types) && !empty($data)) {
-		  $entity = File_DXF_SectionEntities::addReadEntity($entityType, $data);
-		  if ($entity) {
-			$entities[] = $entity;
-		  }
-		}
-	
-		return $entities;
-	}
 }
