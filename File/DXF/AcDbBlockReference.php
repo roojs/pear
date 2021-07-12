2cac334541a8499c46650b2914723983099b2121
leon <leon@roojs.com>
2021-07-12 12:54:37 +0800

    File/DXF/AcDbBlockReference.php

--- File/DXF/AcDbBlockReference.php
+++ File/DXF/AcDbBlockReference.php
@@ -28,7 +28,7 @@ class File_DXF_AcDbBlockReference extends File_DXF_Subclass
 
             switch($pair['key']) {
                 case 0:
-                    case 100:
+                case 100:
                     // End of a subclass
                     $dxf->pushPair($pair);
                     return;
