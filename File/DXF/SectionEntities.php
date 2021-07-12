9c8a5478340d1398b5015d71cc304fd8aa5371be
leon <leon@roojs.com>
2021-07-12 19:59:15 +0800

    File/DXF/SectionEntities.php

--- File/DXF/SectionEntities.php
+++ File/DXF/SectionEntities.php
@@ -7,7 +7,7 @@ class File_DXF_SectionEntities extends File_DXF_Section
 
     public $name = "entities";
 	 
-    public function parse($dxf, $opts= array())
+    function parse($dxf, $opts= array())
     {
 
 		while ($pair = $dxf->readPair()) {
