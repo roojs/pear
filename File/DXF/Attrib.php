1097d1b0aeb9b6898fa8ba2e78e50a06a62d866d
leon <leon@roojs.com>
2021-07-12 16:25:30 +0800

    File/DXF/Attrib.php

--- File/DXF/Attrib.php
+++ File/DXF/Attrib.php
@@ -21,7 +21,8 @@ class File_DXF_Attrib extends File_DXF_Entity
                         $dxf->pushPair($pair);
                         return;
                     }
-                    $mText = $dxf->factory("MText")->parse($dxf);
+                    $mText = $dxf->factory("MText");
+                    $mText->parse($dxf);
                     break;
                 case 100:
 					// Beginning of a subclass
