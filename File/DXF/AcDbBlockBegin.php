f3c906a01b0d0dce54f7cc330478a2cb51b387c4
leon <leon@roojs.com>
2021-07-13 13:46:18 +0800

    File/DXF/AcDbBlockBegin.php

--- File/DXF/AcDbBlockBegin.php
+++ File/DXF/AcDbBlockBegin.php
@@ -5,45 +5,45 @@ require_once 'File/DXF/Subclass.php';
 class File_DXF_AcDbBlockBegin extends File_DXF_Subclass
 {
 
-	function parseToEntity($dxf, $entity)
-	{
+    function parseToEntity($dxf, $entity)
+    {
 
-		while($pair = $dxf->readPair()) {
+        while($pair = $dxf->readPair()) {
 
-			switch($pair['key']) {
-				case 0:
-				case 100:
-				case 1001:
-					// End of a subclass
-					$dxf->pushPair($pair);
-					return;
-				case 2:
-				case 3:
-					$this->blockName = $pair['value'];
-					break;
-				case 70:
-					$this->blockTypeFlags = $pair['value'];
-					break;
-				case 10:
-					$this->basePointX = $pair['value'];
-					break;
-				case 20:
-					$this->basePointY = $pair['value'];
-					break;
-				case 30:
-					$this->basePointZ = $pair['value'];
-					break;
-				case 1:
-					$this->xRefPathName = $pair['value'];
-					break;
-				case 4:
-					$this->blockDescription = $pair['value'];
-					break;
-				default:
-					$pairString = implode(", ", $pair); 
-					throw new Exception ("Got unknown pair for subclass AcDbBlockBegin ($pairString)");
-					break;
-			}
-		}
-	}
+            switch($pair['key']) {
+                case 0:
+                case 100:
+                case 1001:
+                    // End of a subclass
+                    $dxf->pushPair($pair);
+                    return;
+                case 2:
+                case 3:
+                    $entity->blockName = $pair['value'];
+                    break;
+                case 70:
+                    $entity->blockTypeFlags = $pair['value'];
+                    break;
+                case 10:
+                    $entity->basePointX = $pair['value'];
+                    break;
+                case 20:
+                    $entity->basePointY = $pair['value'];
+                    break;
+                case 30:
+                    $entity->basePointZ = $pair['value'];
+                    break;
+                case 1:
+                    $entity->xRefPathName = $pair['value'];
+                    break;
+                case 4:
+                    $entity->blockDescription = $pair['value'];
+                    break;
+                default:
+                    $pairString = implode(", ", $pair); 
+                    throw new Exception ("Got unknown pair for subclass AcDbBlockBegin ($pairString)");
+                    break;
+            }
+        }
+    }
 }
