cf89003a4478ed6941904af59eb44a52cdc409f8
leon <leon@roojs.com>
2021-07-13 12:39:34 +0800

    File/DXF/AcDbEntity.php

--- File/DXF/AcDbEntity.php
+++ File/DXF/AcDbEntity.php
@@ -4,43 +4,43 @@ require_once 'File/DXF/Subclass.php';
 
 class File_DXF_AcDbEntity extends File_DXF_Subclass
 {
-	public $isPaperSpace = 0; // 67
-	public $linetypeName = "BYLAYER"; // 6
-	public $hardPointerToMaterial = "BYLATER"; // 347
-	public $colorNumber = "BYLAYER"; // 62
-	public $linetypeScale = 1; // 48
-	public $objectVisibility = 0; // 60
+    public $isPaperSpace = 0; // 67
+    public $linetypeName = "BYLAYER"; // 6
+    public $hardPointerToMaterial = "BYLATER"; // 347
+    public $colorNumber = "BYLAYER"; // 62
+    public $linetypeScale = 1; // 48
+    public $objectVisibility = 0; // 60
 
     function parseToEntity($dxf, $entity)
-	{
+    {
 
-		while($pair = $dxf->readPair()) {
+        while($pair = $dxf->readPair()) {
 
             switch($pair['key']) {
                 case 0:
-				case 100:
-				case 1001:
+                case 100:
+                case 1001:
                     // End of this subclass
                     $dxf->pushPair($pair);
                     return;
-				case 67:
-					$entity->isPaperSpace = $pair['value'];
-					break;
-				case 8:
-					$this->layerName = $pair['value'];
-					break;
+                case 67:
+                    $entity->isPaperSpace = $pair['value'];
+                    break;
+                case 8:
+                    $this->layerName = $pair['value'];
+                    break;
                 case 6:
                     $this->linetypeName = $pair['value'];
                     break;
-				case 347:
-					$this->hardPointerToMarterial = $pair['value'];
-					break;
+                case 347:
+                    $this->hardPointerToMarterial = $pair['value'];
+                    break;
                 case 62:
                     $this->colorNumber = $pair['value'];
                     break;
-				case 370:
-					$this->lineweightEnum = $pair['value'];
-					break;
+                case 370:
+                    $this->lineweightEnum = $pair['value'];
+                    break;
                 case 48:
                     $this->linetypeScale = $pair['value'];
                     break;
@@ -51,31 +51,31 @@ class File_DXF_AcDbEntity extends File_DXF_Subclass
                     $this->proxyEntityGraphicsBytes = $pair['value'];
                     break;
                 case 310:
-					if (!isset($this->proxyEntityGraphicsData)) {
-						$this->proxyEntityGraphicsData = "";
-					}
+                    if (!isset($this->proxyEntityGraphicsData)) {
+                        $this->proxyEntityGraphicsData = "";
+                    }
                     $this->proxyEntityGraphicsData .= $pair['value'];
                     break;
                 case 420:
                     $this->colorValue = $pair['value'];
                     break;
-				case 430:
-					$this->colorName = $pair['value'];
-					break;
-				case 440:
-					$this->transparencyValue = $pair['value'];
-					break;
-				case 390:
-					$this->hardPointerToPlotStyle = $pair['value'];
-					break;
-				case 284:
-					$this->shadowMode = $pair['value'];
-					break;
+                case 430:
+                    $this->colorName = $pair['value'];
+                    break;
+                case 440:
+                    $this->transparencyValue = $pair['value'];
+                    break;
+                case 390:
+                    $this->hardPointerToPlotStyle = $pair['value'];
+                    break;
+                case 284:
+                    $this->shadowMode = $pair['value'];
+                    break;
                 default:
-					$pairString = implode(", ", $pair); 
-					throw new Exception ("Got unknown pair for subclass AcDbEntity ($pairString)");
-					break;
+                    $pairString = implode(", ", $pair); 
+                    throw new Exception ("Got unknown pair for subclass AcDbEntity ($pairString)");
+                    break;
             }
-		}
-	}
+        }
+    }
 }
