b2927f03ad7b17ed36308ce44aa088d378bb76ea
leon <leon@roojs.com>
2021-07-13 12:08:04 +0800

    File/DXF/SectionBlocks.php

--- File/DXF/SectionBlocks.php
+++ File/DXF/SectionBlocks.php
@@ -6,12 +6,16 @@ class File_DXF_SectionBlocks extends File_DXF_Section
 {
 	function parse($dxf) 
 	{
+
 		while ($pair = $dxf->readPair()) {
+
 			if($pair['key'] == 0) {
+
 				if ($pair['value'] == 'ENDSEC') {
 					// End of the blocks section
 					return;
 				}
+
 				if ($pair['value'] == 'BLOCK') {
 					// Beginning of a new block
 					$block = $dxf->factory('Block');
@@ -19,6 +23,7 @@ class File_DXF_SectionBlocks extends File_DXF_Section
 					$this->items[] = $block;
 					continue;
 				}
+				
 				$pairString = implode(", ", $pair);
 				throw new Exception ("Got invalid pair for a block definition ($pairString)");
 			}
