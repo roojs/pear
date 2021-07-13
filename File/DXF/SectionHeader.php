cf6a99304b12486e89a51350d740aa70438ef237
leon <leon@roojs.com>
2021-07-13 12:05:59 +0800

    File/DXF/SectionHeader.php

--- File/DXF/SectionHeader.php
+++ File/DXF/SectionHeader.php
@@ -4,10 +4,7 @@ require_once 'File/DXF/Section.php';
 
 class File_DXF_SectionHeader extends File_DXF_Section
 {
-    
-    public $name = "header";
-
     function parse ($dxf) {
         $this->skipParseSection($dxf);
-    }
+    }    
 }
