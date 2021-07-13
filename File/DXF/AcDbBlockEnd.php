549f0ce15dd80c42e4b0f4f76dc7ad9a2a107d01
leon <leon@roojs.com>
2021-07-13 13:44:41 +0800

    File/DXF/AcDbBlockEnd.php

--- File/DXF/AcDbBlockEnd.php
+++ File/DXF/AcDbBlockEnd.php
@@ -6,22 +6,22 @@ class File_DXF_AcDbBlockEnd extends File_DXF_Subclass
 {
 
     function parseToEntity($dxf)
-	{
-		while($pair = $dxf->readPair()) {
+    {
+        while($pair = $dxf->readPair()) {
 
             switch($pair['key']) {
                 case 0:
-				case 100:
-				case 1001:
+                case 100:
+                case 1001:
                     // End of a subclass
                     $dxf->pushPair($pair);
                     return;
-					break;
+                    break;
                 default:
-					$pairString = implode(", ", $pair); 
-					throw new Exception ("Got unknown pair for subclass AcDbBlockEnd ($pairString)");
-					break;
+                    $pairString = implode(", ", $pair); 
+                    throw new Exception ("Got unknown pair for subclass AcDbBlockEnd ($pairString)");
+                    break;
             }
-		}
-	}
+        }
+    }
 }
