865051df19336376b31e24f3dd4e4c016d7e01f8
leon <leon@roojs.com>
2021-07-13 12:56:14 +0800

    File/DXF/Subclass.php

--- File/DXF/Subclass.php
+++ File/DXF/Subclass.php
@@ -4,11 +4,4 @@ require_once 'File/DXF/BasicObject.php';
 
 class File_DXF_Subclass extends File_DXF_BasicObject
 {
-    public $subclassMarker; // 100
-    function __construct($cfg=array()) 
-	{
-		$this->subclassMarker = str_replace("File_DXF_", "", get_class($this));
-		parent::__construct($cfg=array());
-	}
-
 }
