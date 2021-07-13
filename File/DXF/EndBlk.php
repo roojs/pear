c6799102920b6b05e51e10b8882cc70dccee9065
leon <leon@roojs.com>
2021-07-13 13:43:50 +0800

    File/DXF/EndBlk.php

--- File/DXF/EndBlk.php
+++ File/DXF/EndBlk.php
@@ -5,37 +5,6 @@ require_once 'File/DXF/Entity.php';
 class File_DXF_EndBlk extends File_DXF_Entity
 {
 
-    function parse($dxf)
-    {
-        // parse common pair for entities
-        $this->parseCommon($dxf);
-
-        while($pair = $dxf->readPair()) {
-
-            switch($pair['key']) {
-                case 0:
-                    // End of this entity
-                    $dxf->pushPair($pair);
-                    return;
-                case 100:
-					// Beginning of a subclass
-					$subclass = $dxf->factory($pair['value']);
-					$subclass->parse($dxf);
-					$this->subclasses[$pair['value']] = $subclass;
-					break;
-                case 1001:
-                    $applicationGroup = $dxf->factory("ApplicationGroup", array("applicationName" => $pair['value']));
-                    $applicationGroup->parse($dxf);
-                    $this->extendedData[] = $applicationGroup;
-                    break;
-                default:
-                    $pairString = implode(", ", $pair); 
-                    throw new Exception ("Got unknown pair for entity ENDBLK ($pairString)");
-                    break;
-            }
-        }
-    }
-
     /*
      * OLD CODE BELOW
      */
