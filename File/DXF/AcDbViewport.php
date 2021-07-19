e46cef308e7bbdbaa3c63fa98a64a8ac672aea0d
Alan Knowles <alan@roojs.com>
2021-07-19 15:17:59 +0800

    File/DXF/AcDbViewport.php

---
+++ File/DXF/AcDbViewport.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'File/DXF/Subclass.php';
+
+class File_DXF_AcDbViewport extends File_DXF_Subclass
+{
+    
+    function parseToEntity($dxf, $entity)
+    {
+        
+        while($pair = $dxf->readPair()) {
+
+            switch($pair['key']) {
+                case 0:
+                case 100:
+                case 1001:
+                    // End of a subclass
+                    $dxf->pushPair($pair);
+                    return;
+                    break;
+                default:
+                    break; //.. ignore...
+            }
+        }
+    }
+}
