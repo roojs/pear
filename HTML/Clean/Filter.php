74d5ada07d6de26c33e281bad394fe929b8f94e1
Alan <alan@roojs.com>
2022-09-15 11:35:07 +0800

    HTML/Clean/Filter.php

--- HTML/Clean/Filter.php
+++ HTML/Clean/Filter.php
@@ -10,4 +10,29 @@
 class  HTML_Clean_Filter
 {
     
+    
+    
+    
+    removeNodeKeepChildren : function( node)
+    {
+    
+        ar = Array.from(node.childNodes);
+        for (var i = 0; i < ar.length; i++) {
+         
+            node.removeChild(ar[i]);
+            // what if we need to walk these???
+            node.parentNode.insertBefore(ar[i], node);
+           
+        }
+        node.parentNode.removeChild(node);
+    }
+    
+    listToArray($list)
+    {
+        $ret = array();
+        foreach($list as $l) {
+            $ret[] = $l;
+        }
+        return $ret;
+    }
 }
