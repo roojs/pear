a2cd223c9a758fb5eb563fb9271e75669f446e39
Alan <alan@roojs.com>
2022-09-15 14:05:25 +0800

    HTML/Clean/FilterStyleToTag.php

--- HTML/Clean/FilterStyleToTag.php
+++ HTML/Clean/FilterStyleToTag.php
@@ -14,5 +14,19 @@ require_once 'Filter.php';
 class HTML_Clean_FilterStyleToTag extends HTML_Clean_Filter
 {
     
+    var $tags = array(
+        'B'  => array( 'fontWeight' => 'bold' ),
+        'I' =>   array(  'fontStyle'  => 'italic' ),
+        
+        // h1.. h6 ?? font-size?
+        'SUP'  => array(   'verticalAlign'  => 'super'),
+        'SUB' => array(   'verticalAlign' => 'sub' )
+        
+    );
+    
+    function __construct($cfg)
+    {
+            $this->tags = 
+    }
     
 }
