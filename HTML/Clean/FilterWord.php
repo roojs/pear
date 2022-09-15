0f91b3d988d14faceb272a5d43eb3e512aa7900d
Alan <alan@roojs.com>
2022-09-15 11:33:30 +0800

    HTML/Clean/FilterWord.php HTML/Clean/Filter.php

--- HTML/Clean/FilterWord.php
+++ HTML/Clean/FilterWord.php
@@ -1,14 +1,52 @@
 <?php
 
+/**
+ * Does a few very specific word paste filtering - doc bullents an a name= tags.
+ * 
+ * doesnt really use filter parent much
+ *
+ */ 
+
 require_once 'Filter.php';
 
 class HTML_Clean_FilterWord extends HTML_Clean_Filter
 {
+    
+    var $tag = true;
     
     function __construct($cfg)
     {
         $this->replaceDocBullets($cfg->node);
         $this->replaceAname($cfg->node);
     }
+   
+    
+    function replaceAname  ($doc)
+    {
+        // replace all the a/name without..
+        $da= $doc->getElementsByTagName('a');
+        $aa = array();  // convert to array..
+        foreach($da as $a) {
+            $aa[] = $a;
+        }
+        
+        for (var $i = 0; $i  < count($aa); $i++) {
+            $a = $aa[$i];
+            if ($a->hasAttribute("name")) {
+                $a->removeAttribute("name");
+            }
+            if ($a->hasAttribute("href")) {
+                continue;
+            }
+            // reparent children.
+            this.removeNodeKeepChildren(a);
+            
+        }
+        
+        
+        
+    },
+    
+    
     
 }
