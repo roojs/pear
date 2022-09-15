4d8ed92c510957f22c4f80900fd2c48de9827094
Alan <alan@roojs.com>
2022-09-15 14:48:41 +0800

    HTML/Clean/FilterAttributes.php

--- HTML/Clean/FilterAttributes.php
+++ HTML/Clean/FilterAttributes.php
@@ -5,7 +5,8 @@
 /**
  *  replaces bad attributes... and attribute values
  *  
- * 
+ *  done by walking all elements
+ *   
  *  
  *
  */ 
@@ -15,5 +16,14 @@ require_once 'Filter.php';
 class HTML_Clean_FilterAttribute  extends HTML_Clean_Filter
 {
    
-   
+    var $tag =  true; // all tags
+    
+    var $attrib_black = array(); // array
+    var $attrib_clean = array(); // array
+    var $attrib_white = array(); // array
+    
+    var $style_black = array(); // array
+    var $style_white = array(); // array
+    
+     
    
