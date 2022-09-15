d7c9a70f8a94d603bdb9f0615d903e1f8fdd706b
Alan <alan@roojs.com>
2022-09-15 11:34:05 +0800

    HTML/Clean/Filter.php

---
+++ HTML/Clean/Filter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base class for all the filtering, contains a few usefull routines along with the walk code
+ * 
+ * doesnt really use filter parent much
+ *
+ */ 
+
+class  HTML_Clean_Filter
+{
+    
+}
