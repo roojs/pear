cae1f00e05f3a37aa9bccf60578a81a224757ab0
Alan <alan@roojs.com>
2022-09-15 15:21:42 +0800

    HTML/Clean/FilterBlack.php

--- HTML/Clean/FilterBlack.php
+++ HTML/Clean/FilterBlack.php
@@ -7,3 +7,34 @@
  * if it's doesnt need to use ':', then we can just do a search.
  *
  */
+
+ 
+require_once 'Filter.php';
+
+class HTML_Clean_FilterAttribute  extends HTML_Clean_Filter
+{
+   
+ 
+function __construct($cfg)
+    {
+        parent::__construct($cfg);
+        if (is_array($this->tag) && !in_array(':', $this->tag)) {
+            $this->simpleReplace();
+            return;
+        }
+        if (is_string($this->tag) && ':' != $this->tag) {
+            $this->simpleReplace();
+            return;
+        }
+        $this->walk($cfg['node']);
+    } 
+
+Roo.extend(Roo.htmleditor.FilterBlack, Roo.htmleditor.Filter,
+{
+    tag : true, // all elements.
+   
+    replaceTag : function(n)
+    {
+        n.parentNode.removeChild(n);
+    }
+});
