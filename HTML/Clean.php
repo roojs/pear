b1ee5763687786c5c9cae7fba87cd9934dc032ce
Alan <alan@roojs.com>
2022-09-15 11:09:24 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -9,10 +9,30 @@ class HTML_Clean {
     
     static function fromHTML($str)
     {
+        $str= self::cleanWordChars($str);
         $dom = new DOMDocument('1.0', 'utf8');
         $dom->loadHTML($str);
         return new HTML_Clean($dom);    
     }
+    static function cleanWordChars($str)
+    {
+        $swapCodes  = array(
+             8211=>  "&#8211;" ,  
+             8212=>  "&#8212;" ,  
+             8216=>   "'" ,   
+             8217=>  "'" ,   
+             8220=>  '"' ,   
+             8221=>  '"' ,   
+             8226=>  "*" ,   
+             8230=>  "..." 
+        );
+        foreach($swapCodes as $k=>$v) {
+            $str = str_replace(mb_chr($k), $v, $str);
+        }
+        return $str;
+    
+    }
+    
     
     var $dom; // Dom Document.
     
