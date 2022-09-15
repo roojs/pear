3a076c1af3630e7dc84122751ef3d237a624d644
Alan <alan@roojs.com>
2022-09-15 14:07:14 +0800

    HTML/Clean/FilterStyleToTag.php

--- HTML/Clean/FilterStyleToTag.php
+++ HTML/Clean/FilterStyleToTag.php
@@ -14,6 +14,9 @@ require_once 'Filter.php';
 class HTML_Clean_FilterStyleToTag extends HTML_Clean_Filter
 {
     
+    var $tag = true;
+    
+    // what we are going to change..
     var $tags = array(
         'B'  => array( 'fontWeight' => 'bold' ),
         'I' =>   array(  'fontStyle'  => 'italic' ),
@@ -32,5 +35,41 @@ class HTML_Clean_FilterStyleToTag extends HTML_Clean_Filter
         $this->walk($cfg['node']);
     }
     
+ 
+    
+    
+    
+    function replaceTag(node)
+    {
+        
+        
+        if (node.getAttribute("style") === null) {
+            return true;
+        }
+        var inject = [];
+        for (var k in this.tags) {
+            if (node.style[this.tags[k][0]] == this.tags[k][1]) {
+                inject.push(k);
+                node.style.removeProperty(this.tags[k][0]);
+            }
+        }
+        if (!inject.length) {
+            return true; 
+        }
+        var cn = Array.from(node.childNodes);
+        var nn = node;
+        Roo.each(inject, function(t) {
+            var nc = node.ownerDocument.createElement(t);
+            nn.appendChild(nc);
+            nn = nc;
+        });
+        for(var i = 0;i < cn.length;cn++) {
+            node.removeChild(cn[i]);
+            nn.appendChild(cn[i]);
+        }
+        return true /// iterate thru
+    }
+    
+})
     
 }
