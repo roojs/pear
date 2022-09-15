564340d7736226f311c0e1066254f334b1e43e69
Alan <alan@roojs.com>
2022-09-15 16:01:14 +0800

    HTML/Clean/FilterSpan.php

---
+++ HTML/Clean/FilterSpan.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * remove spans without attributes.
+ * 
+ *
+ */
+
+ 
+require_once 'Filter.php';
+
+class HTML_Clean_FilterSpan extends HTML_Clean_Filter
+{
+   
+ 
+    function __construct($cfg)
+    {
+        parent::__construct($cfg);
+        $ar = $this->arrayFrom($this->node->getElementsByTagName('span'));
+        foreach($as as $a) {
+            if ($a->hasAttributes()) {
+                continue;
+            }
+            $this->removeNodeKeepChildren($a);
+        }
+        
+    }
+    
+    
+}
+ 
