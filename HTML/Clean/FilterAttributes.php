bbd80b1587a3c85d70938dd907e3600cb70019e4
Alan <alan@roojs.com>
2022-09-15 14:47:31 +0800

    HTML/Clean/FilterAttributes.php

--- HTML/Clean/FilterAttributes.php
+++ HTML/Clean/FilterAttributes.php
@@ -1,1 +1,19 @@
 <?php
+
+
+
+/**
+ *  replaces bad attributes... and attribute values
+ *  
+ * 
+ *  
+ *
+ */ 
+
+require_once 'Filter.php';
+
+class HTML_Clean_FilterAttribute  extends HTML_Clean_Filter
+{
+   
+   
+   
