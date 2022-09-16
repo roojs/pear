d617dddf4a81f223b0c3ee509786eed42a6f4007
Alan <alan@roojs.com>
2022-09-16 12:24:29 +0800

    HTML/Clean/BlockTd.php

--- HTML/Clean/BlockTd.php
+++ HTML/Clean/BlockTd.php
@@ -8,6 +8,13 @@
 require_once 'Block.php';
 class  HTML_Clean_BlockTd extends HTML_Clean_Block
 {
+    var $width = '';
+    var $textAlign = 'left';
+    var $valign = 'top';
+    
+    var $colspan = 1;
+    var $rowspan = 1;
+    
     
      
     function __construct($cfg) {
@@ -19,4 +26,52 @@ class  HTML_Clean_BlockTd extends HTML_Clean_Block
         parent::__construct();
          
     }
+     toObject => function()
+    {
+        var ret = {
+            tag => 'td',
+            contenteditable => 'true', // this stops cell selection from picking the table.
+            'data-block' => 'Td',
+            valign => $this->valign,
+            style => {  
+                'text-align' =>  $this->textAlign,
+                border => 'solid 1px rgb(0, 0, 0)', // ??? hard coded?
+                'border-collapse' => 'collapse',
+                padding => '6px', // 8 for desktop / 4 for mobile
+                'vertical-align'=> $this->valign
+            },
+            html => $this->html
+        };
+        if ($this->width != '') {
+            ret.width = $this->width;
+            ret.style.width = $this->width;
+        }
+        
+        
+        if ($this->colspan > 1) {
+            ret.colspan = $this->colspan ;
+        } 
+        if ($this->rowspan > 1) {
+            ret.rowspan = $this->rowspan ;
+        }
+        
+           
+        
+        return ret;
+         
+    },
     
+    
+    readElement => function(node)
+    {
+        node  = node ? node => $this->node ;
+        $this->width = node.style.width;
+        $this->colspan = Math.max(1,1*node.getAttribute('colspan'));
+        $this->rowspan = Math.max(1,1*node.getAttribute('rowspan'));
+        $this->html = node.innerHTML;
+        if (node.style.textAlign != '') {
+            $this->textAlign = node.style.textAlign;
+        }
+        
+        
+    },
