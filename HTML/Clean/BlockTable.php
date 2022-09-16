dd2d146df7c4b86d971b799112e644f979300030
Alan <alan@roojs.com>
2022-09-16 12:03:40 +0800

    HTML/Clean/BlockTable.php

--- HTML/Clean/BlockTable.php
+++ HTML/Clean/BlockTable.php
@@ -7,3 +7,29 @@
  
 require_once 'Block.php';
 class  HTML_Clean_BlockTable extends HTML_Clean_Block
+{
+    
+    var $rows = array(); // row cols {array}
+    var $no_col = 1;
+    var $no_row = 1;
+    var $width = '100%';
+    
+    function __construct($cfg) {
+        if ($cfg['node']) {
+            $this->readElement($cfg['node']);
+            $this->updateElement($cfg['node']);
+        } 
+        parent::__construct();
+        if (!$this->node) {
+        
+        for($r = 0; $r < $this->no_row; $r++) {
+            $this->rows[$r] = array();
+            for($c = 0; $c < $this->no_col; $c++) {
+                $this->rows[$r][$c] = $this->emptyCell();
+            }
+        }
+    }
+    
+    
+    
+     
