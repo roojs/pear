ea520fecb166231c578241f869707be66e5a3396
leon <leon@roojs.com>
2023-04-17 14:00:34 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -140,9 +140,6 @@ class File_Convert
             $pg = false;
         }
         $fn = $this->fn;
-        if($toMimetype != 'image/jpeg') {
-            var_dump($fn);
-        }
          //echo '<PRE>'; print_r(array('convert', func_get_args()));
         if (
                 $toMimetype != $this->mimetype ||
@@ -162,10 +159,6 @@ class File_Convert
             }
             $action->debug = $this->debug;
             $fn = $action->runconvert($this->fn, $x, $y, $pg);
-            if($toMimetype != 'image/jpeg') {
-                var_dump($fn);
-                die('a');
-            }
             // delete the generated files after script execution
             if(!empty(self::$options['delete_all'])) {
                 $this->deleteOnExitAdd($fn);
