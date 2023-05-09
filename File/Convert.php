696367a84d3a8a2d1eebe2c8d7f1b8efa4b5c06e
leon <leon@roojs.com>
2023-05-09 13:07:15 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -331,8 +331,7 @@ class File_Convert
             exit;
         }
         //var_dump($fn, $mt); exit;
-        // header('Content-type: '. $mt);
-        header('Content-type: application/octet-stream');
+        header('Content-type: '. $mt);
         
         // even though we have done a file_exists above - it still errors out here occausionally.
         $fh = @fopen($fn, 'rb');
