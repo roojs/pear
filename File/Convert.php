c26c8a25023f911ac88b7e3e75d9889871323884
leon <leon@roojs.com>
2023-05-09 13:36:16 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -312,8 +312,7 @@ class File_Convert
             $filename = $sfn;
         }
         print_r($filename);
-        print_r(htmlspecialchars($filename));
-        print_r(str_replace('+', '%20', urlencode($filename)));
+        print_r(urlencode($filename));
         print_r(str_replace('_', '%5F', str_replace('+', '%20', urlencode($filename))));
         exit;
         
