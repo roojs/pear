7c6a25fa35b14fbc33dc34c58f8f3a4961b683a8
leon <leon@roojs.com>
2023-05-09 13:03:40 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -319,9 +319,9 @@ class File_Convert
         header('Content-length: '. filesize($fn));
        // if ($type != 'inline') {
             // header('Content-Disposition: '.$type.'; filename="' . htmlspecialchars($filename).  '"');
-            header('Content-Disposition: '.$type.'; filename="' . 
-            htmlspecialchars($filename).  '"; filename*=utf-8\'\'' . 
-            str_replace('+', '%20', urlencode($filename)).  '');
+            header('Content-Disposition: '.$type.'; ' .
+            'filename="' . htmlspecialchars($filename).  '"; ' . 
+            'filename*=utf-8\'\'' . str_replace('+', '%20', urlencode($filename)).  '');
        // }
        
         // needs to be removed after debugging - otherwise it logs to error.log
