b2f8709a240fb1e98f180c280108d97562295563
leon <leon@roojs.com>
2023-05-09 13:17:08 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -322,7 +322,7 @@ class File_Convert
             header('Content-Disposition: '.$type.'; ' .
             // 'filename="' . htmlspecialchars($filename).  '"; ' . 
             // 'filename*=UTF-8\'\'' . str_replace('+', '%20', urlencode($filename)).  ';'
-            "filename*=utf-8''abcdefg.docx"
+            "filename*=utf-8''\"abcdefg.docx\""
         );
        // }
        
