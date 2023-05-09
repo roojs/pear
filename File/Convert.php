cedeb3f03153a6a10de3f660de5c0af2f3ade9cb
leon <leon@roojs.com>
2023-05-09 15:45:52 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -315,7 +315,7 @@ class File_Convert
         print_r(utf8_decode($filename));
         $string = "\xE1\xE9\xF3\xFA";
         print_r(utf8_encode($string));
-        print_r(utf8_decode(utf8_decode($stinrg)));
+        print_r(utf8_decode(utf8_decode($string)));
         exit;
         
         header('Content-length: '. filesize($fn));
