a69d2db24c8834680b1bbe4dd295fee526748c4d
leon <leon@roojs.com>
2023-05-09 15:14:49 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -319,7 +319,7 @@ class File_Convert
        // if ($type != 'inline') {
             // header('Content-Disposition: '.$type.'; filename="' . htmlspecialchars($filename).  '"');
             header('Content-Disposition: '.$type.'; ' .
-            // 'filename="' . htmlspecialchars($filename).  '"; ' . 
+            'filename="' . htmlspecialchars($filename).  '"; '
             // 'filename*="UTF-8\'\'' . str_replace('+', '%20', urlencode($filename)).  '";'
         );
        // }
