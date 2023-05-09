7b996e315851807a603b19e54c9cd0db2f49f654
leon <leon@roojs.com>
2023-05-09 13:31:20 +0800

    File/Convert.php

--- File/Convert.php
+++ File/Convert.php
@@ -322,7 +322,7 @@ class File_Convert
             header('Content-Disposition: '.$type.'; ' .
             // 'filename="' . htmlspecialchars($filename).  '"; ' . 
             // 'filename*=UTF-8\'\'' . str_replace('+', '%20', urlencode($filename)).  ';'
-            "filename=\"EURO rates\"; filename*=utf-8''%e2%82%ac%20rates"
+            "filename=\"EURO ratesb\"; filename*=utf-8''%e2%82%ac%20ratesa"
         );
        // }
        
