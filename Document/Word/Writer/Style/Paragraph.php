402429efdcb4414e19525053a5689a1e80f282cc
Alan <alan@roojs.com>
2023-11-21 14:16:20 +0800

    Document/Word/Writer/Style/Paragraph.php

--- Document/Word/Writer/Style/Paragraph.php
+++ Document/Word/Writer/Style/Paragraph.php
@@ -91,11 +91,22 @@ class Document_Word_Writer_Style_Paragraph
 			$value += 240; // because line height of 1 matches 240 twips
 		}
                 
-                if($key == '_list-style'){
-                    $key = str_replace('-', '_', $key);
-                }
-                
-		$this->$key = $value;
+		if($key == '_list-style'){
+			$key = str_replace('-', '_', $key);
+		}
+        $cache = array();
+		if (empty($cache)) {
+			$ar = get_class_vars(get_class($this));
+			foreach($ar as $k => $v) {
+				$cache[strtolower($k)] = $k;
+			}
+		}
+		$key = strtolower(str_replace('-','', $key));
+		$key = isset($cache[$key]) ? $cache[$key] : $key;
+		
+		$this->$key =  $value;
+		  
+		 
 	}
 
 	/**
