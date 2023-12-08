fbb2d226ec01f0d6fc586c47095a81a04df1b33d
leon <leon@roojs.com>
2023-12-08 15:36:11 +0800

    Finance/ISIN.php

---
+++ Finance/ISIN.php
@@ -0,0 +1,25 @@
+<?php
+class ISIN 
+{
+    var $map = array();
+
+    function getISIN($stockCode) 
+    {
+        $ar = explode('.', $stockCode);
+        // invalid stock code
+        if(count($ar) != 2) {
+            return false;
+        }
+
+        $file = dirname(__FILE__) . '/ISIN/' . $ar[1] . '.php';
+
+        // invalid / not supported
+        if(!file_exists(dirname(__FILE__) . '/ISIN/' . $ar[1] . '.php')) {
+            return false;
+        }
+        
+        include $file;
+
+        return isset($this->map[$stockCode]) ? $this->map[$stockCode] : false;
+    }
+}
