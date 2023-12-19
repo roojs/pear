1b2730f3ba88ac79408aca1f5e94503f6d8002d1
leon <leon@roojs.com>
2023-12-19 15:40:39 +0800

    Finance/ISIN.php

--- Finance/ISIN.php
+++ Finance/ISIN.php
@@ -11,10 +11,6 @@ class Finance_ISIN
             return false;
         }
 
-        if($ar[1] == 'SG') {
-            return false;
-        }
-
         $file = dirname(__FILE__) . '/ISIN/' . $ar[1] . '.php';
 
         // invalid / not supported
