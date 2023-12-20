acf5c3a6b82df9cff9753c9ede2182ea82662667
leon <leon@roojs.com>
2023-12-20 11:35:14 +0800

    Finance/ISIN.php

--- Finance/ISIN.php
+++ Finance/ISIN.php
@@ -36,9 +36,9 @@ class Finance_ISIN
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, 
             http_build_query(array(
-                'searchSubmit' => 'sow', 
-                'language' => 'en', 
-                'datasource' => '5849b3c3-7bd3-4570-9fed-df92b0788426'
+                'searchSubmit' => 'sow' 
+                // 'language' => 'en', 
+                // 'datasource' => '5849b3c3-7bd3-4570-9fed-df92b0788426'
             ))
         );     
         curl_setopt($ch, CURLOPT_HTTPHEADER,
