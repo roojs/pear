ab582c3d889df3cb17f9069c9cb21252d3412121
leon <leon@roojs.com>
2023-12-21 13:40:29 +0800

    Finance/ISIN/DE.php Finance/ISIN/SI.php

--- Finance/ISIN/SI.php
+++ Finance/ISIN/SI.php
@@ -1,7 +1,7 @@
 <?php
 class Finance_ISIN_SI
 {
-    $map = array(
+    var $map = array(
         '502.SI' => 'SG0502000029',
         'NLC.SI' => 'SGXE78399073',
         'AZG.SI' => 'BMG3087Y2074',
