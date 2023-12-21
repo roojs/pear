ef305690605919efcd33934cda8e73de3350abf7
leon <leon@roojs.com>
2023-12-21 16:22:11 +0800

    Finance/ISIN/SG.php

--- Finance/ISIN/SG.php
+++ Finance/ISIN/SG.php
@@ -4,7 +4,6 @@ class Finance_ISIN_SG extends Finance_ISIN
     static function updateMap($stockCode)
     {
         $ch = curl_init();
-        // $f = tmpfile();
         curl_setopt($ch, CURLOPT_URL, 'https://www.boerse-stuttgart.de/api/bsg-feature-navigation/Search/PostSearchInput');   
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, 
@@ -25,18 +24,13 @@ class Finance_ISIN_SG extends Finance_ISIN
         );
         curl_setopt($ch, CURLOPT_COOKIE,  "website#lang=en;");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_VERBOSE, true);
-        // curl_setopt($ch, CURLOPT_STDERR, $f);
         curl_setopt($ch, CURLINFO_HEADER_OUT, true);
         $str = curl_exec($ch);
-        // fseek($f,0);
-        // var_dump( fread($f, 32*1024) );
-        // fclose($f);
         curl_close($ch);
 
 
-        var_dump(curl_getinfo($ch));
-        var_dump("GET SG ISIN\n");
+        // var_dump(curl_getinfo($ch));
+        // var_dump("GET SG ISIN\n");
         var_dump($str);
         die('test');
 
