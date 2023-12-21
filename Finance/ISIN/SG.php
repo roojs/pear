9f21f4625fac08b714fcea90e531e05fbd87d2fa
leon <leon@roojs.com>
2023-12-21 16:22:39 +0800

    Finance/ISIN/SG.php

--- Finance/ISIN/SG.php
+++ Finance/ISIN/SG.php
@@ -29,8 +29,7 @@ class Finance_ISIN_SG extends Finance_ISIN
         curl_close($ch);
 
 
-        // var_dump(curl_getinfo($ch));
-        // var_dump("GET SG ISIN\n");
+        var_dump(curl_getinfo($ch));
         var_dump($str);
         die('test');
 
