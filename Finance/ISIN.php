988d6cb8023bdafdc11f8440e7892b1b9a038a6f
leon <leon@roojs.com>
2023-12-21 16:10:01 +0800

    Finance/ISIN.php

--- Finance/ISIN.php
+++ Finance/ISIN.php
@@ -22,22 +22,18 @@ class Finance_ISIN
             return self::$maps[$ar[1]][$stockCode];
         }
 
-        // load the map if it is not loaded
-        if(!isset(self::$maps[$ar[1]])) {
+        $file = dirname(__FILE__) . '/ISIN/' . $ar[1] . '.php';
 
-            $file = dirname(__FILE__) . '/ISIN/' . $ar[1] . '.php';
-
-            // invalid / not supported
-            if(!file_exists($file)) {
-                return false;
-            }
-            
-            include_once $file;
-
-            $cls = 'Finance_ISIN_' . $ar[1];
-            $cls::updateMap($stockCode);
-            self::$maps[$ar[1]] = $cls::$map;
+        // invalid / not supported
+        if(!file_exists($file)) {
+            return false;
         }
+        
+        include_once $file;
+
+        $cls = 'Finance_ISIN_' . $ar[1];
+        $cls::updateMap($stockCode);
+        self::$maps[$ar[1]] = $cls::$map;
 
         return isset(self::$maps[$ar[1]][$stockCode]) ? self::$maps[$ar[1]][$stockCode] : false;
     }
