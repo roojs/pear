35043e7efdfce174a22a970b5aabff20d414b864
leon <leon@roojs.com>
2024-05-31 13:11:58 +0800

    Finance/ISIN/L.php

--- Finance/ISIN/L.php
+++ Finance/ISIN/L.php
@@ -4,7 +4,6 @@ class Finance_ISIN_L extends Finance_ISIN
     function getLocationISIN($stockcode)
     {
         $ar = explode('.', $stockcode);
-        $isin = false;
 
         $jsonString = file_get_contents('https://api.londonstockexchange.com/api/gw/lse/instruments/alldata/' . $ar[0]);
         $jsonString = iconv("UTF-8", "ISO-8859-1//IGNORE", $jsonString);
@@ -12,8 +11,10 @@ class Finance_ISIN_L extends Finance_ISIN
         $jsonError = json_last_error();
 
         if($jsonError != JSON_ERROR_NONE || !isset($json->isin)) {
-            return $isin;
+            return false;
         }
+
+        var_dumP($json);
 
         return $json->isin;
     }
