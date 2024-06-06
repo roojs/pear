7640454006e6ad682e3574ba10b1e106cae4fbef
Alan <alan@roojs.com>
2024-06-06 12:15:13 +0800

    fix #8157 - add london to ISIN db

---
+++ Finance/ISIN/L.php
@@ -0,0 +1,19 @@
+<?php
+class Finance_ISIN_L extends Finance_ISIN
+{
+    function getLocationISIN($stockcode)
+    {
+        $ar = explode('.', $stockcode);
+
+        $jsonString = file_get_contents('https://api.londonstockexchange.com/api/gw/lse/instruments/alldata/' . $ar[0]);
+        $jsonString = iconv("UTF-8", "ISO-8859-1//IGNORE", $jsonString);
+        $json = json_decode($jsonString);
+        $jsonError = json_last_error();
+
+        if($jsonError != JSON_ERROR_NONE || !isset($json->isin)) {
+            return false;
+        }
+        
+        return $json->isin;
+    }
+}
