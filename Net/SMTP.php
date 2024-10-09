63172a33e606bdd313158e0a1b9426db6193a480
leon <leon@roojs.com>
2024-10-09 16:48:37 +0800

    Net/SMTP.php

--- Net/SMTP.php
+++ Net/SMTP.php
@@ -453,8 +453,6 @@ class Net_SMTP
         $result = $this->_socket->connect($this->host, $this->port,
                                           $persistent, $timeout,
                                           $this->_socket_options);
-        var_dump($result);
-        die('connect');
         if (PEAR::isError($result)) {
             $p = new PEAR();
             return $p->raiseError('Failed to connect socket: ' .
