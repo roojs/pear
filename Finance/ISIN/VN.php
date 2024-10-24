74d429fe33fc5d00cfd47c15d69820e071e96e09
leon <leon@roojs.com>
2024-10-24 10:15:45 +0800

    Finance/ISIN/VN.php

--- Finance/ISIN/VN.php
+++ Finance/ISIN/VN.php
@@ -11,28 +11,14 @@ class Finance_ISIN_VN extends Finance_ISIN
         $ar = explode('.', $stockcode);
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://www.boerse-stuttgart.de/api/bsg-feature-navigation/Search/PostSearchInput');   
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 
-            http_build_query(
-                array(
-                    'searchSubmit' => $ar[0],
-                    'language' => 'en', 
-                    'datasource' => '5849b3c3-7bd3-4570-9fed-df92b0788426'
-                )
-            )
-        );     
-        curl_setopt($ch, CURLOPT_HTTPHEADER,
-            array(
-                "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                "Content-Type: application/x-www-form-urlencoded"
-            )
-        );
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_URL, "https://www.hsx.vn/Areas/Desktop/Web/Search?q={$ar[0]}&_search=false&rows=30&page=1&sidx=id&sord=desc");   
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
         $str = curl_exec($ch);
         curl_close($ch);
+
+        var_dump($str);
+        die('test');
+
 
         /*
 
