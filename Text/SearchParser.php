944816b6f6fcdaf6abb0325ddd0de5c5c7844e13
leon <leon@roojs.com>
2025-04-22 10:49:46 +0800

    Text/SearchParser.php

--- Text/SearchParser.php
+++ Text/SearchParser.php
@@ -31,7 +31,7 @@ class Text_SearchParser
         //var_dump($str);
         $x = new Text_SearchParser_Tokenizer ($str);
         $ar = $x->parse();
-        var_dump('ar');
+        var_dump('XXXXXXXXXXXXXXXXXXXXXXXXxx');
         var_dump($ar);
         // catch eq
         $l = count($ar) -1;
