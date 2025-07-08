01cf4ffd9b583471604fbb6e78b445e8b3b15e16
leon <leon@roojs.com>
2025-07-08 13:22:26 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -107,7 +107,7 @@ Available commands:
                 continue;
             }
 
-            var_dump('++++ ' . $fullePath);
+            var_dump('++++ ' . implode('/', $full_path));
             var_dump('----- ' . $d);
             $chk = $full_path;
             $chk[] = $d;
