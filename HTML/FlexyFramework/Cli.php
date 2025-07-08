047ecd14cac12888514f3b1aa8ee1166825a7cbe
leon <leon@roojs.com>
2025-07-08 13:25:20 +0800

    HTML/FlexyFramework/Cli.php

--- HTML/FlexyFramework/Cli.php
+++ HTML/FlexyFramework/Cli.php
@@ -101,7 +101,7 @@ Available commands:
         }
         //print_r("CHKDIR:    ". implode('/', $full_path)."\n");
 
-        var_dump($path);
+        var_dump('++++ ' . $path[0]);
         
         foreach(scandir(implode('/', $full_path)) as $d) {
             
