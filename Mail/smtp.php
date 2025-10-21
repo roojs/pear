5d181119c9d604efba9ec162d57b880f2bfad1bf
leon <leon@roojs.com>
2025-10-21 15:11:57 +0800

    Mail/smtp.php

--- Mail/smtp.php
+++ Mail/smtp.php
@@ -457,7 +457,6 @@ class Mail_smtp extends Mail {
 
         /* Attempt to connect to the configured SMTP server. */
         if (PEAR::isError($res = $this->_smtp->connect($this->timeout))) {
-            var_dump($res->toString());
             list($code, $error) = $this->_error('Failed to connect to ' .
                                    $this->host . ':' . $this->port,
                                    $res);
