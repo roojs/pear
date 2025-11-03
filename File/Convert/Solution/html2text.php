d01de0e11469c58e02e566f08392cd154af0fd68
leon <leon@roojs.com>
2025-11-03 10:52:12 +0800

    File/Convert/Solution/html2text.php

--- File/Convert/Solution/html2text.php
+++ File/Convert/Solution/html2text.php
@@ -53,8 +53,6 @@ class File_Convert_Solution_html2text extends File_Convert_Solution {
         }
         $cmd = "$html2text " . implode(' ', $opts)
             . "-utf8 -o " . escapeshellarg($target) . "  " . escapeshellarg($fn);
-        var_dump($cmd);
-        die('test');
 
         $this->debug( $cmd );
         
