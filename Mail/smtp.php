707542bece3a38ace78b204d5e2e6fde815f3a28
leon <leon@roojs.com>
2025-11-13 12:22:57 +0800

    Mail/smtp.php

--- Mail/smtp.php
+++ Mail/smtp.php
@@ -412,6 +412,8 @@ class Mail_smtp extends Mail {
                 $this->disconnect();
             }
 
+            var_dump("testing");
+
             return true;
         }
         die('test');
