d9be2c19dfcde9e2e5e485401b8c6e5dd713eb94
leon <leon@roojs.com>
2025-11-25 14:00:03 +0800

    Changed Mail/smtp.phpNet/Socket.php

--- Mail/smtp.php
+++ Mail/smtp.php
@@ -415,6 +415,8 @@ class Mail_smtp extends Mail {
             return true;
         }
 
+        die('test');
+
         /* Send the message's headers and the body as SMTP data. */
         $res = $this->_smtp->data($textHeaders . "\r\n\r\n" . $body);
 		list(,$args) = $this->_smtp->getResponse();
