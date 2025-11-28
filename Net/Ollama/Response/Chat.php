52a61a6cd5beb17d91fbcd9c2bedd766c9f527ae
leon <leon@roojs.com>
2025-11-28 10:40:56 +0800

    Net/Ollama/Response/Chat.php

--- Net/Ollama/Response/Chat.php
+++ Net/Ollama/Response/Chat.php
@@ -89,10 +89,7 @@ class Net_Ollama_Response_Chat extends Net_Ollama_Response {
             $this->role = $data['message']['role'];
             $this->content = $data['message']['content'];
         }
-
-        var_dump("MESSAGE START");
-        var_dump($data);
-        var_dump("MESSAGE END");
+        
         // Store messages for reply functionality
         $this->messages = !empty($data['messages']) ? $data['messages'] : array();
     }
