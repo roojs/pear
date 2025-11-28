027e28a6e2ecf51bf8850a7c00c5ad672f29d98c
leon <leon@roojs.com>
2025-11-28 11:11:35 +0800

    Net/Ollama/Response/Chat.php

--- Net/Ollama/Response/Chat.php
+++ Net/Ollama/Response/Chat.php
@@ -119,6 +119,7 @@ class Net_Ollama_Response_Chat extends Net_Ollama_Response {
     
     function reply($message)
     {
+        var_dump($this);
         die('test');
         // Start with conversation history
         $messages = isset($this->messages) ? $this->messages : array();
