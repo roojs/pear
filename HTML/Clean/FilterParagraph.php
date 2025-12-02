bc3c9a89f07e3e269a55988826bc7829770fd5ba
leon <leon@roojs.com>
2025-12-02 15:22:36 +0800

    HTML/Clean/FilterParagraph.php

--- HTML/Clean/FilterParagraph.php
+++ HTML/Clean/FilterParagraph.php
@@ -19,8 +19,9 @@ class HTML_Clean_FilterParagraph extends HTML_Clean_Filter
     function __construct($cfg)
     {
         parent::__construct($cfg);
+        var_dump($this->node->getElementsByTagName('p'));
         $pp = $this->arrayFrom($this->node->getElementsByTagName('p'));
-        var_dump($pp);
+        // var_dump($pp);
         die('test');
         while($pp->length) {
             $this->replaceIt($p);
