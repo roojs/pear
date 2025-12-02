f51a0d1fe44b2165ae26ecdb653ffed11901b081
leon <leon@roojs.com>
2025-12-02 13:53:17 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -9,9 +9,10 @@ class HTML_Clean {
     
     static function fromHTML($str, $opts = array())
     {
+        $str= self::cleanWordChars($str);
+
         var_dump($str);
         die('test');
-        $str= self::cleanWordChars($str);
         $dom = new DOMDocument('1.0', 'utf8');
         $dom->loadHTML($str);
         $opts['dom'] = $dom;
