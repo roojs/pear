549d98ba64b24b944c1554fafbc8b0f5548ff30c
leon <leon@roojs.com>
2025-12-03 16:38:38 +0800

    HTML/Clean/FilterKeepChildren.php

--- HTML/Clean/FilterKeepChildren.php
+++ HTML/Clean/FilterKeepChildren.php
@@ -27,21 +27,21 @@ class HTML_Clean_FilterKeepChildren extends HTML_Clean_Filter
  
     function replaceTag ($n)
     {
-        self::$counter++;
-        var_dump(self::$counter);
-        var_dump($n);
-        if(self::$counter == 2) {
-            die('test');
-        }
-        // $ar = $this->arrayFrom($n->childNodes);
-
-        // // remove first.. - otherwise due to our walking method - the parent will not look at them.
-        // foreach($ar as $t) {
-        //     if (!$this->isTagMatch($t)) {
-        //         continue;
-        //     }
-        //     $this->replaceTag($t); // this effetively walks all the children.
+        // self::$counter++;
+        // var_dump(self::$counter);
+        // var_dump($n);
+        // if(self::$counter == 2) {
+        //     die('test');
         // }
+        $ar = $this->arrayFrom($n->childNodes);
+
+        // remove first.. - otherwise due to our walking method - the parent will not look at them.
+        foreach($ar as $t) {
+            if (!$this->isTagMatch($t)) {
+                continue;
+            }
+            $this->replaceTag($t); // this effetively walks all the children.
+        }
 
         $this->removeNodeKeepChildren($n);
         return false; // don't walk children
