6179a1322ba80d49b6b36f437b508c7ebee46c81
leon <leon@roojs.com>
2025-12-03 16:07:54 +0800

    HTML/Clean/FilterKeepChildren.php

--- HTML/Clean/FilterKeepChildren.php
+++ HTML/Clean/FilterKeepChildren.php
@@ -28,7 +28,6 @@ class HTML_Clean_FilterKeepChildren extends HTML_Clean_Filter
  
     function replaceTag ($n)
     {
-        var_dump($n->tagName);
         $ar = $this->arrayFrom($n->childNodes);
 
         // remove first.. - otherwise due to our walking method - the parent will not look at them.
