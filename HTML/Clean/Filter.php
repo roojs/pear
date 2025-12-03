8c2aaca3f3b0569d6ba590811f971775d6536273
leon <leon@roojs.com>
2025-12-03 14:23:43 +0800

    HTML/Clean/Filter.php

--- HTML/Clean/Filter.php
+++ HTML/Clean/Filter.php
@@ -131,6 +131,15 @@ class  HTML_Clean_Filter
         return $ret;
     }
 
+    function nodeSetStyle($node, $style)
+    {
+        $str = array();
+        foreach($style as $k=>$v) {
+            $str[] = "$k:$v";
+        }
+        $node->setAttribute('style', implode(";", $str));
+    }
+
     /*
     
     function styleToObject($node, $lower = false)
@@ -150,14 +159,4 @@ class  HTML_Clean_Filter
     }
 
     */
-    
-    function nodeSetStyle($node, $style)
-    {
-        $str = array();
-        foreach($style as $k=>$v) {
-            $str[] = "$k:$v";
-        }
-        $node->setAttribute('style', implode(";", $str));
-    }
-    
 }
