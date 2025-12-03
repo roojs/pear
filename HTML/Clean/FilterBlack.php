acc41e185ba1e56dcbb77a3d6bfd2897d5935997
leon <leon@roojs.com>
2025-12-03 15:20:15 +0800

    HTML/Clean/FilterBlack.php

--- HTML/Clean/FilterBlack.php
+++ HTML/Clean/FilterBlack.php
@@ -38,8 +38,6 @@ class HTML_Clean_FilterBlack extends HTML_Clean_Filter
     function simpleReplace()
     {
         foreach(is_array($this->tag) ? $this->tag : array($this->tag) as $t) {
-            var_dump($this->node->getElementsByTagName('HEAD'));
-            die('test');
             $ar = $this->arrayFrom($this->node->getElementsByTagName($t));
             foreach($ar as $k) {
                 var_dump($k->tagName);
