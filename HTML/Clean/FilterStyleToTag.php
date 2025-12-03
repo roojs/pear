65b76d5bebf9880421e5520e79a84f3ed00424a4
leon <leon@roojs.com>
2025-12-03 11:47:54 +0800

    HTML/Clean/FilterStyleToTag.php

--- HTML/Clean/FilterStyleToTag.php
+++ HTML/Clean/FilterStyleToTag.php
@@ -42,6 +42,80 @@ class HTML_Clean_FilterStyleToTag extends HTML_Clean_Filter
     
     function replaceTag($node)
     {
+        /*
+            $style = $node->getAttribute('style');
+
+        // no attribute 'style'
+        if(empty($style)) {
+            return;
+        }
+
+        // styles to be converted to tags
+        $styles = array(
+            'B' => array(
+                'font-weight',
+                'bold'
+            ),
+            'I' => array(
+                'font-style',
+                'italic'
+            ),
+            'SUP' => array(
+                'vertical-align',
+                'super'
+            ),
+            'SUB' => array(
+                'vertical-align',
+                'sub'
+            )
+        );
+
+        $tags = array();
+
+        foreach($styles as $tag => $s) {
+            $pattern = '/' . $s[0] . '\s*:\s*' . $s[1] . '\s*;/';
+
+            $matches = array();
+
+            preg_match($pattern, $style, $matches);
+
+            if(!empty($matches)) {
+                // tags to add
+                $tags[] = $tag;
+
+                // remove styles
+                $style = preg_replace($pattern, '', $style);
+            }
+        }
+
+        if(empty($tags)){
+            return;
+        }
+
+        $node->setAttribute('style', $style);
+        
+        // copy of child nodes
+        $childNodes = array();
+
+        foreach($node->childNodes as $n) {
+            $childNodes[] = $n;
+        }
+
+        $current = $node;
+
+        // add tags
+        foreach($tags as $tag) {
+            $new = $node->ownerDocument->createElement($tag);
+            $current->append($new);
+            $current = $new;
+        }
+
+        // move children
+        foreach($childNodes as $n) {
+            $node->removeChild($n);
+            $new->append($n);
+        }
+        */
         
         
         if (!$node->hasAttribute("style")) {
