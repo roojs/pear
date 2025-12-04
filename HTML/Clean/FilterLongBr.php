9f8437d6bfb04f9faa1398f79f01bfc730f971f1
leon <leon@roojs.com>
2025-12-04 11:31:19 +0800

    HTML/Clean/FilterLongBr.php

--- HTML/Clean/FilterLongBr.php
+++ HTML/Clean/FilterLongBr.php
@@ -17,6 +17,7 @@ class HTML_Clean_FilterLongBR extends HTML_Clean_Filter
     function __construct($cfg)
     {
         parent::__construct($cfg);
+        var_dump($this->node);
         $pp = $this->arrayFrom($this->node->getElementsByTagName('br'));
         var_dump($this->node->getElementsByTagName('br'));
         die('test');
