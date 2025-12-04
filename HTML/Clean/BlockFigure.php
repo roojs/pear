bc5dee6181c19f4baeb40b609db3ca69dcfec790
leon <leon@roojs.com>
2025-12-04 16:08:50 +0800

    HTML/Clean/BlockFigure.php

--- HTML/Clean/BlockFigure.php
+++ HTML/Clean/BlockFigure.php
@@ -45,7 +45,7 @@ class  HTML_Clean_BlockFigure extends HTML_Clean_Block
     {
         $doc = new DOMDocument('1.0', 'utf8');
         
-        // plain text caption
+        // alt text for the image
         $this->caption = '<b>test caption</b>';
         $alt = '';
         if(!empty($this->caption)) {
