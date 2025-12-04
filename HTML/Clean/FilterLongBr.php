d2f475749dea005179b9be1ef1b399a2cf8c475a
leon <leon@roojs.com>
2025-12-04 11:33:43 +0800

    HTML/Clean/FilterLongBr.php

--- HTML/Clean/FilterLongBr.php
+++ HTML/Clean/FilterLongBr.php
@@ -17,7 +17,7 @@ class HTML_Clean_FilterLongBR extends HTML_Clean_Filter
     function __construct($cfg)
     {
         parent::__construct($cfg);
-        $pp = $this->arrayFrom($this->node->getElementsByTagName('br'));
+        $pp = $this->arrayFrom($this->node->getElementsByTagName('BR'));
         foreach($pp as $p) {
             if (!$p->parentNode) { // should not happen as we only walk forwards.
                 continue;
