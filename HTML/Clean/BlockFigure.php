4fb185ff22c0812888d3a6bacdf7f7935067cce8
leon <leon@roojs.com>
2025-12-04 16:07:32 +0800

    HTML/Clean/BlockFigure.php

--- HTML/Clean/BlockFigure.php
+++ HTML/Clean/BlockFigure.php
@@ -168,9 +168,7 @@ class  HTML_Clean_BlockFigure extends HTML_Clean_Block
                 )
             );
         }
-
-        var_dump($ret);
-        die('test');
+        
         return $ret;
     }
     
