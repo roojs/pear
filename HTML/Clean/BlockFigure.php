ee0ee3e24d24acde195c29abb9e558dc37d5b2e8
leon <leon@roojs.com>
2025-12-04 15:40:17 +0800

    HTML/Clean/BlockFigure.php

--- HTML/Clean/BlockFigure.php
+++ HTML/Clean/BlockFigure.php
@@ -100,7 +100,7 @@ class  HTML_Clean_BlockFigure extends HTML_Clean_Block
                 'frameborder' => 0,
                 'allowfullscreen' => true,
                 'width' => 768,  // these are for video tricks - that we replace the outer
-                'height' => (image_width && image_height) ? (Math.round(768 / image_width * image_height)) : 576,
+                'height' => (!empty($image_width) && !empty($image_height)) ? (Math.round(768 / image_width * image_height)) : 576,
                 'src' => $this->video_url,
                 'cn' => array(
                     $img
