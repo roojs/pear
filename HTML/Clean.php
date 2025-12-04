41503dedd66f0d5c3c6eeafd38eeb900834f63c2
leon <leon@roojs.com>
2025-12-04 10:58:08 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -92,22 +92,15 @@ class HTML_Clean {
         
         // should be fonts..
         $this->filter('KeepChildren',array( 'node' =>  $d, 'tag'  =>   array(   'FONT', ':' )) );  
-        // $result = '';
-        // foreach ($d->childNodes as $child) {
-        //     $result .= ($this->dom->saveHTML($child));
-        // }
-        // var_dump("RESULT");
-        // var_dump($result);
-        // die('test4');
         $this->filter('Paragraph',array( 'node' =>  $d, 'lang' => $language ));
+        $this->filter('Span',array( 'node' =>  $d ));
         $result = '';
         foreach ($d->childNodes as $child) {
             $result .= ($this->dom->saveHTML($child));
         }
         var_dump("RESULT");
         var_dump($result);
-        die('test5');
-        $this->filter('Span',array( 'node' =>  $d ));
+        die('test6');
         $this->filter('LongBr',array( 'node' =>  $d ));
 
 
