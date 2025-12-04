447c71b630ea15eff3f82e38ffa51ab95f1459d7
leon <leon@roojs.com>
2025-12-04 12:04:02 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -9,7 +9,7 @@ class HTML_Clean {
     
     static function fromHTML($str, $opts = array())
     {
-        $str = "<h1>aaa</h1><P></P><p dir='ltr'><b>Hello</b></p><span dir='rtl'>World</span>";
+        $str = "<body lang='ar'><h1>aaa</h1><P></P><p dir='ltr'><b>Hello</b></p><span dir='rtl'>World</span></body>";
         $str = "<?xml encoding='utf-8'?> <div id='tmp_dom_wrapper'>" . $str . "<div>";
         $str= self::cleanWordChars($str);
         $dom = new DOMDocument('1.0', 'utf8');
