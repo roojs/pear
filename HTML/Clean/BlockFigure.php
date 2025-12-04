7bad5abc46095dc7ef3252051d4c8fde92be7048
leon <leon@roojs.com>
2025-12-04 12:40:03 +0800

    HTML/Clean/BlockFigure.php

--- HTML/Clean/BlockFigure.php
+++ HTML/Clean/BlockFigure.php
@@ -10,6 +10,8 @@ class  HTML_Clean_BlockFigure extends HTML_Clean_Block
 {
     
     function __construct($cfg) {
+        var_dump($this);
+        die('test');
         if ($cfg['node']) {
             $this->readElement($cfg['node']);
             $this->updateElement($cfg['node']);
