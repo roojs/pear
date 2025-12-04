120aaa1fc6bdf72f2527f80aa4da3bb2c7815ecc
leon <leon@roojs.com>
2025-12-04 10:24:23 +0800

    HTML/Clean/FilterAttributes.php

--- HTML/Clean/FilterAttributes.php
+++ HTML/Clean/FilterAttributes.php
@@ -44,6 +44,9 @@ class HTML_Clean_FilterAttributes  extends HTML_Clean_Filter
 
         foreach($node->attributes as $a) {
             var_dump($a->name);
+            if($a->name == 'dir') {
+                die('test');
+            }
             if(count($this->attrib_white) && !in_array(strtolower($a->name), $this->attrib_white)) {
                 $removeAttributes[] = $a;
                 continue;
