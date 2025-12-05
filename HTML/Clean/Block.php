52ac0be0f5b8daa8cf542555c5bc5f82daabaa4c
leon <leon@roojs.com>
2025-12-05 13:24:15 +0800

    HTML/Clean/Block.php

--- HTML/Clean/Block.php
+++ HTML/Clean/Block.php
@@ -40,8 +40,8 @@ abstract class  HTML_Clean_Block extends HTML_Clean_Filter
         if ($type === false) {
             
             self::initAll($body,'table');
-            self::initAll($body,'td');
-            // self::initAll($body,'figure');
+            // self::initAll($body,'td');
+            self::initAll($body,'figure');
             return;
         }
         $ar = $body->getElementsByTagName($type);
