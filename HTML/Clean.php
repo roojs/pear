5537841d32d7a63d2665dc3ee19cc60939651040
leon <leon@roojs.com>
2025-12-05 12:07:14 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -114,9 +114,6 @@ class HTML_Clean {
         foreach ($d->childNodes as $child) {
             $result .= ($this->dom->saveHTML($child));
         }
-        var_dump("RESULT");
-        var_dump($result);
-        die('test8');
          
         
         
