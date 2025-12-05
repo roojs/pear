9c53e39b116acd8042b1d3beadecc08c0eb56fdc
leon <leon@roojs.com>
2025-12-05 12:38:21 +0800

    HTML/Clean/BlockTable.php

--- HTML/Clean/BlockTable.php
+++ HTML/Clean/BlockTable.php
@@ -117,6 +117,9 @@ class  HTML_Clean_BlockTable extends HTML_Clean_Block
             $this->rows[] = $row;     
             $this->no_col = max($this->no_col, $no_column);
         }
+
+        var_dump($this);
+        die('test');
     }
     
     function emptyCell () {
