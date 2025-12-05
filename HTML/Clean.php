0e8038b351fe14e628f0da82d07ac86329fd57cb
leon <leon@roojs.com>
2025-12-05 12:07:00 +0800

    HTML/Clean.php

--- HTML/Clean.php
+++ HTML/Clean.php
@@ -109,6 +109,14 @@ class HTML_Clean {
             ));
             $fig->updateElement($img);
         }
+
+        $result = '';
+        foreach ($d->childNodes as $child) {
+            $result .= ($this->dom->saveHTML($child));
+        }
+        var_dump("RESULT");
+        var_dump($result);
+        die('test8');
          
         
         
