b2b3de472179c3d06c2bc3c30706900156a51e44
leon <leon@roojs.com>
2025-12-05 13:02:31 +0800

    HTML/Clean/BlockTable.php

--- HTML/Clean/BlockTable.php
+++ HTML/Clean/BlockTable.php
@@ -84,7 +84,7 @@ class  HTML_Clean_BlockTable extends HTML_Clean_Block
             $row =  array();
             $this->no_row++;
             $no_column = 0;
-            foreach($node->getElementsByTagName('td') as $td) {
+            foreach($tr->getElementsByTagName('td') as $td) {
                 $add = new HTML_Clean_BlockTd( array('node' => $td ));
                 $no_column += $add->colspan;
                 $row[] =   $add;
