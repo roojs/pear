d67aac8e5ed21141925c20580e997997768b1862
leon <leon@roojs.com>
2025-12-05 13:27:29 +0800

    HTML/Clean/BlockFigure.php

--- HTML/Clean/BlockFigure.php
+++ HTML/Clean/BlockFigure.php
@@ -188,8 +188,8 @@ class  HTML_Clean_BlockFigure extends HTML_Clean_Block
 
         // backward compatible
         // it was stored in figcaption
-        if(this.caption_display == '') {
-            this.caption_display = this.getVal(node, 'figcaption', 'data-display');
+        if($this->caption_display == '') {
+            $this->caption_display = this.getVal(node, 'figcaption', 'data-display');
         }
         
         $figcaption = $this->getVal($node, 'figcaption', false);
