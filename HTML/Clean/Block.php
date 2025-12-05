e2e69f4e915a6a70a28337d80bbbba819e2b7106
leon <leon@roojs.com>
2025-12-05 11:17:06 +0800

    HTML/Clean/Block.php

--- HTML/Clean/Block.php
+++ HTML/Clean/Block.php
@@ -22,7 +22,7 @@ abstract class  HTML_Clean_Block extends HTML_Clean_Filter
         if (!$db) {
             $db = ucfirst($node->nodeName);
         }
-        var_dump($db);
+        var_dump($node);
         die('test');
         require_once 'HTML/Clean/Block'.$db . '.php';
         $cls = 'HTML_Clean_Block'. $db;
