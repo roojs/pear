1b8485663556877c5fb64def02d435342928cc54
leon <leon@roojs.com>
2025-12-05 11:11:43 +0800

    HTML/Clean/Block.php

--- HTML/Clean/Block.php
+++ HTML/Clean/Block.php
@@ -45,9 +45,6 @@ abstract class  HTML_Clean_Block extends HTML_Clean_Filter
             return;
         }
         $ar = $body->getElementsByTagName($type);
-        var_dump($ar);
-        var_dump($type);
-        die('test');
         foreach($ar as $a) {
             self::factory($a);
         }
