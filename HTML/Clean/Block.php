68c1d0bb880e2879ea87450e7395fdc19d3ae649
leon <leon@roojs.com>
2025-12-05 11:48:39 +0800

    HTML/Clean/Block.php

--- HTML/Clean/Block.php
+++ HTML/Clean/Block.php
@@ -100,9 +100,6 @@ abstract class  HTML_Clean_Block extends HTML_Clean_Filter
         }
         if ($attr == 'style') {
             $styles = $this->styleToArray($node);
-            var_dump($styles);
-            var_dump($style);
-            die('test');
             return isset($styles[strtolower($style)]) ? $styles[strtolower($style)] : '';
         }
         
