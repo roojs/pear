e72be1877287dc53fa1ba0514a78a9ee84934e1c
leon <leon@roojs.com>
2026-01-13 11:16:12 +0800

    Mail/smtp.php

--- Mail/smtp.php
+++ Mail/smtp.php
@@ -394,7 +394,7 @@ class Mail_smtp extends Mail {
         }
 
         // Don't send anything in test mode
-        // if ($this->test) {
+        if ($this->test) {
             $res = $this->_smtp->rset();
             if (is_a($res, 'PEAR_Error')) {
                 list($code, $error) = $this->_error("Failed to reset SMTP connection", $res);
@@ -413,10 +413,9 @@ class Mail_smtp extends Mail {
             if ($this->persist === false) {
                 $this->disconnect();
             }
-            die('test');
 
             return true;
-        // }
+        }
         
         /* Send the message's headers and the body as SMTP data. */
         $res = $this->_smtp->data($textHeaders . "\r\n\r\n" . $body);
