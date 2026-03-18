fa3b7840567e989b0f7d604ce8f5ad5a45a5ec37
leon <leon@roojs.com>
2026-03-18 12:33:29 +0800

    Net/Ollama/Call/Chat.php

--- Net/Ollama/Call/Chat.php
+++ Net/Ollama/Call/Chat.php
@@ -68,8 +68,6 @@ class Net_Ollama_Call_Chat extends Net_Ollama_Call {
     {
         if (!is_object($response)) {
             $response = $this->oai->response('Chat', json_decode($response, true));
-            var_dump($response);
-            die('test');
         }
         $this->response = $response;
         $this->response->call = $this;
