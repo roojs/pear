fd4e26d32228732d42f67e11e9c065545e040ad4
leon <leon@roojs.com>
2026-03-18 11:41:40 +0800

    Net/Ollama.php

--- Net/Ollama.php
+++ Net/Ollama.php
@@ -13,6 +13,9 @@ class Net_Ollama {
     {
         // Universal constructor - accepts array or object
         $options = (array)$options;
+
+        var_dump($options);
+        die('test');
         
         // Populate properties from options
         foreach ($options as $k => $v) {
