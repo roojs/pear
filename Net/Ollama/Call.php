7046154e91c50909c9cd6521dd299b818451014e
leon <leon@roojs.com>
2026-03-19 15:19:38 +0800

    Net/Ollama/Call.php

--- Net/Ollama/Call.php
+++ Net/Ollama/Call.php
@@ -3,7 +3,7 @@
 abstract class Net_Ollama_Call {
     protected $oai; // to prevent looping
     var $id;
-    protected $response;
+    var $response;
     static $excluded = array('id', 'response');
     protected $exclude = array(); // eg.'id', 'response');
     protected $_url = '';
