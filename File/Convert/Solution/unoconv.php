fe01d7ed57b9e7a5c4f1970355712316a5e3a561
leon <leon@roojs.com>
2026-04-01 12:15:24 +0800

    File/Convert/Solution/unoconv.php

--- File/Convert/Solution/unoconv.php
+++ File/Convert/Solution/unoconv.php
@@ -75,7 +75,7 @@ class File_Convert_Solution_unoconv extends File_Convert_Solution
         
         $timeout = System::which('timeout');
         // fix the home directory - as we can't normally write to www-data's home directory.
-        putenv('HOME='. ini_get('session.save_path'));
+        // putenv('HOME='. ini_get('session.save_path'));
         $libreoffice = System::which('libreoffice');
         if (empty($libreoffice)) {
             $this->debug("missing libreoffice");
