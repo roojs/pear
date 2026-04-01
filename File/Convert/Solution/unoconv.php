b6d0db7eeec4d122ab8cba86e1f5233d5de74179
leon <leon@roojs.com>
2026-04-01 12:49:11 +0800

    File/Convert/Solution/unoconv.php

--- File/Convert/Solution/unoconv.php
+++ File/Convert/Solution/unoconv.php
@@ -102,7 +102,6 @@ class File_Convert_Solution_unoconv extends File_Convert_Solution
         $loHome = rtrim(ini_get('session.save_path') ?: sys_get_temp_dir(), '/\\') . '/tmp-lo-' . str_replace('.', '', uniqid('', true));
         if (!@mkdir($loHome, 0700, true)) {
             $this->debug("Could not create LibreOffice HOME: {$loHome}");
-            @unlink($from);
             return false;
         }
         $previousHome = getenv('HOME');
