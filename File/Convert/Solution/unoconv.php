47e9b7a82a2b2200c25dd81f89e7921c13790d52
leon <leon@roojs.com>
2026-04-01 13:22:32 +0800

    File/Convert/Solution/unoconv.php

--- File/Convert/Solution/unoconv.php
+++ File/Convert/Solution/unoconv.php
@@ -87,8 +87,7 @@ class File_Convert_Solution_unoconv extends File_Convert_Solution
         $from = $this->tempName(array_pop($bits),true);
         $to = $this->tempName($ext,true);
 
-        var_dump($from);
-        var_dump($to);
+        var_dump($fn);
         
         copy($fn, $from);
         
