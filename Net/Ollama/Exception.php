51a7de24192f62776b848e755a3767a42d6d24e5
leon <leon@roojs.com>
2026-04-02 16:25:27 +0800

    Net/Ollama/Exception.php

--- Net/Ollama/Exception.php
+++ Net/Ollama/Exception.php
@@ -5,7 +5,7 @@ class Net_Ollama_Exception extends Exception
     static function factory($message, $type, $previous)
     {
         $cls = 'Net_Ollama_Exception_' . $type;
-        return new $cls($message, 0, $previous);
+        return new ('Net_Ollama_Exception_' . $type)($message, 0, $previous);
     }
 }
 
