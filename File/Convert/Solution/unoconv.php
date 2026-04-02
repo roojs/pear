608a979ad527b174f9cd9ccd1efaf5248fa93fe6
leon <leon@roojs.com>
2026-04-02 10:59:11 +0800

    File/Convert/Solution/unoconv.php

--- File/Convert/Solution/unoconv.php
+++ File/Convert/Solution/unoconv.php
@@ -140,6 +140,9 @@ class File_Convert_Solution_unoconv extends File_Convert_Solution
         
         $target =   $fn  . '.' . $ext;
         
+
+        var_dump(self::$options);
+        die('test');
         
         if ( file_exists($target)  && filesize($target) && filemtime($target) > filemtime($fn)) {
             $this->debug("UNOCONV SKIP target exists");
