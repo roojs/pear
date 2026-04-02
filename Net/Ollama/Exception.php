52072f56b13328f662a6b340d6af5326650bcf32
leon <leon@roojs.com>
2026-04-02 16:55:33 +0800

    Net/Ollama/Exception.php

--- Net/Ollama/Exception.php
+++ Net/Ollama/Exception.php
@@ -8,7 +8,3 @@ class Net_Ollama_Exception extends Exception
         return new $cls($message, 0, $previous);
     }
 }
-
-class Net_Ollama_Exception_ConnectionTimeout extends Net_Ollama_Exception {};
-class Net_Ollama_Exception_CurlError extends Net_Ollama_Exception {};
-class Net_Ollama_Exception_HttpError extends Net_Ollama_Exception {};
