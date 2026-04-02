a80c71266796b42cfb764da441386818525f2620
leon <leon@roojs.com>
2026-04-02 11:08:09 +0800

    File/Convert/Solution/unoconv.php

--- File/Convert/Solution/unoconv.php
+++ File/Convert/Solution/unoconv.php
@@ -123,6 +123,7 @@ class File_Convert_Solution_unoconv extends File_Convert_Solution
             }
             $mime = image_type_to_mime_type($imgType);
             $im->setAttribute('src', 'data:' . $mime . ';base64,' . base64_encode(file_get_contents($ifn)));
+            $this->debug("Converted image to data URL: " . $src);
             $modified = true;
         }
 
