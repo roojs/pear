7a9a6d2be54d741b06cd86b945d120fe2083beb9
leon <leon@roojs.com>
2026-04-08 11:03:39 +0800

    Net/Ollama/Call.php

--- Net/Ollama/Call.php
+++ Net/Ollama/Call.php
@@ -172,15 +172,15 @@ abstract class Net_Ollama_Call {
         // Check for timeout or other errors
         if ($curlErrno === CURLE_OPERATION_TIMEDOUT || $curlErrno === CURLE_OPERATION_TIMEOUTED) {
             require_once 'Net/Ollama/Exception/ConnectionTimeout.php';
-            throw new Net_Ollama_Exception_ConnectionTimeout($connectTimeout);;
+            throw new Net_Ollama_Exception_ConnectionTimeout(array('connectionTimeout' => $connectTimeout));
         }
         if ($curlError) {
-            require_once 'Net/Ollama/Exception/HttpError.php';
-            throw new Net_Ollama_Exception_CurlError($curlError);
+            require_once 'Net/Ollama/Exception/CurlError.php';
+            throw new Net_Ollama_Exception_CurlError(array('curlError' => $curlError));
         }
         if ($httpCode !== 200 && $httpCode !== 0) {
             require_once 'Net/Ollama/Exception/HttpError.php';
-            throw new Net_Ollama_Exception_HttpError($httpCode);
+            throw new Net_Ollama_Exception_HttpError(array('httpCode' => $httpCode));
         }
         
         $this->oai->debug("Received Response", json_decode($result, true));
