69a14faa5fb3cd3821713435ae56046a6d7bdab4
leon <leon@roojs.com>
2026-04-08 10:32:30 +0800

    Net/Ollama/Exception/ConnectionTimeout.php

--- Net/Ollama/Exception/ConnectionTimeout.php
+++ Net/Ollama/Exception/ConnectionTimeout.php
@@ -5,14 +5,8 @@ require_once 'Net/Ollama/Exception.php';
 class Net_Ollama_Exception_ConnectionTimeout extends Net_Ollama_Exception
 {
     var $connectionTimeout = 0;
-    function __construct($options = array()) 
-    {
-        $message = "Failed to connect";
-        if(!empty($options['connectionTimeout'])) {
-            $this->connectionTimeout = $options['connectionTimeout'];
-            $message .= " within {$this->connectionTimeout} seconds";
-        }
 
-        parent::__construct($message);
+    function buildMessage() {
+        return "Failed to connect within {$this->connectionTimeout} seconds";
     }
 }
