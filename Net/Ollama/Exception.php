1ddc9884ff1dc5aec88eb6f87b5a060e271836aa
leon <leon@roojs.com>
2026-04-08 10:22:48 +0800

    Net/Ollama/Exception.php

--- Net/Ollama/Exception.php
+++ Net/Ollama/Exception.php
@@ -2,4 +2,12 @@
 
 class Net_Ollama_Exception extends Exception
 {
+    function __construct($options = array()) 
+    {
+        foreach($options as $k => $v) {
+            if(property_exists($this, $k)) {
+                $this->{$k} = $v;
+            }
+        }
+    }
 }
