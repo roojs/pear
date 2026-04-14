46c24afebfe0e8f7ea46cda8baf5eeb393f3af0d
leon <leon@roojs.com>
2026-04-14 13:54:51 +0800

    DB/DataObject.php

--- DB/DataObject.php
+++ DB/DataObject.php
@@ -4176,6 +4176,9 @@ class DB_DataObject extends DB_DataObject_Overload
             }
 
             if(!empty($cfg['exclude_join']) && in_array($ocl, $cfg['exclude_join'])) {
+                var_dump($cfg['exclude_join']);
+                var_dump($ocl);
+                die('test');
                 continue;
             }
             
