90ab0bcb7d0a368e75bf6ddb60d88f356ea33c3c
leon <leon@roojs.com>
2026-05-05 12:29:59 +0800

    Document/Word/IOFactory.php

--- Document/Word/IOFactory.php
+++ Document/Word/IOFactory.php
@@ -2,6 +2,10 @@
 /**
  * Generic IO entry points for {@see Document_Word}.
  *
+ * Writer discovery matches the former {@see Document_Word_Writer_IOFactory} (search locations,
+ * IWriter loop) so custom {@see Document_Word_IOFactory::addSearchLocation} registrations keep working
+ * after {@see Document_Word_Writer_IOFactory} is removed.
+ *
  * Example (HTML export via writers under Document/Word/Writer/Writer):
  *
  * ```php
@@ -17,8 +21,57 @@
  */
 class Document_Word_IOFactory
 {
+    /**
+     * Search locations (same shape as legacy Document_Word_Writer_IOFactory).
+     *
+     * @var array
+     */
+    private static $_searchLocations = array(
+        array('type' => 'IWriter', 'path' => 'Document/Word/Writer/Writer/{0}.php', 'class' => 'Document_Word_Writer_Writer_{0}')
+    );
+
+    /**
+     * Autoresolve classes (carried over for parity with legacy IOFactory; unused by createWriter).
+     *
+     * @var array
+     */
+    private static $_autoResolveClasses = array(
+        'Serialized'
+    );
+
     private function __construct()
     {
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSearchLocations()
+    {
+        return self::$_searchLocations;
+    }
+
+    /**
+     * @param array $value
+     * @throws Exception
+     */
+    public static function setSearchLocations($value)
+    {
+        if (is_array($value)) {
+            self::$_searchLocations = $value;
+            return;
+        }
+        throw new Exception('Invalid parameter passed.');
+    }
+
+    /**
+     * @param string $type       Example: IWriter
+     * @param string $location   Example: Document/Word/Writer/Writer/{0}.php
+     * @param string $classname  Example: Document_Word_Writer_Writer_{0}
+     */
+    public static function addSearchLocation($type = '', $location = '', $classname = '')
+    {
+        self::$_searchLocations[] = array('type' => $type, 'path' => $location, 'class' => $classname);
     }
 
     /**
@@ -42,7 +95,18 @@ class Document_Word_IOFactory
     public static function createWriter(Document_Word $documentWord, $writerType = '')
     {
         require_once __DIR__ . '/Writer/Writer/' . $writerType . '.php';
-        $className = 'Document_Word_Writer_Writer_' . $writerType;
-        return new $className($documentWord);
+        $searchType = 'IWriter';
+
+        foreach (self::$_searchLocations as $searchLocation) {
+            if ($searchLocation['type'] == $searchType) {
+                $className = str_replace('{0}', $writerType, $searchLocation['class']);
+                $instance = new $className($documentWord);
+                if (!is_null($instance)) {
+                    return $instance;
+                }
+            }
+        }
+
+        throw new Exception("No $searchType found for type $writerType");
     }
 }
