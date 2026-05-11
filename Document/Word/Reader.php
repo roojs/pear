0778bb84b33bef3640f04a619985e655888b18a9
leon <leon@roojs.com>
2026-05-11 11:31:55 +0800

    Document/Word/Reader.php

--- Document/Word/Reader.php
+++ Document/Word/Reader.php
@@ -515,6 +515,10 @@ class Document_Word_Reader
             $creator = trim($n->textContent);
             break;
         }
+
+        var_dump($title);
+        var_dump($creator);
+        die('test');
         if ($title !== '') {
             $doc->getProperties()->setTitle($title);
         }
