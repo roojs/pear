ccaeb2393bc679c5a189dbf0e1874586ffc1af70
leon <leon@roojs.com>
2026-05-20 13:05:07 +0800

    DB/mysqli.php

--- DB/mysqli.php
+++ DB/mysqli.php
@@ -666,8 +666,6 @@ class DB_mysqli extends DB_common
      */
     function rollback()
     {
-        var_dump("rollback");
-        var_dump($this->transaction_opcount);
         if ($this->transaction_opcount > 0) {
             if ($this->_db) {
                 if (!@mysqli_select_db($this->connection, $this->_db)) {
