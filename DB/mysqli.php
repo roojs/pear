5b3c093018b5502212b65486461a0f96b5c508df
leon <leon@roojs.com>
2026-05-22 12:22:47 +0800

    DB/mysqli.php

--- DB/mysqli.php
+++ DB/mysqli.php
@@ -640,7 +640,6 @@ class DB_mysqli extends DB_common
      */
     function commit()
     {
-        var_dump($this->transaction_opcount);
         if ($this->transaction_opcount > 0) {
             if ($this->_db) {
                 if (!@mysqli_select_db($this->connection, $this->_db)) {
