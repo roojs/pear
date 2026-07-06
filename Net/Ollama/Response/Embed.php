6bee9e391a6e4e1a29ba52fbef92ccaeae58bf4c
leon <leon@roojs.com>
2026-07-06 10:28:39 +0800

    Net/Ollama/Response/Embed.php

--- Net/Ollama/Response/Embed.php
+++ Net/Ollama/Response/Embed.php
@@ -5,4 +5,9 @@ class_exists('Net_Ollama_Response') || require_once 'Net/Ollama/Response.php';
 class Net_Ollama_Response_Embed extends Net_Ollama_Response
 {
     var $embeddings = array();
+    var $model;
+    var $total_duration;
+    var $load_duration;
+    var $prompt_eval_count;
+    var $prompt_eval_duration;
 }
